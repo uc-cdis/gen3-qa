18b8038c0b79079a548ebca6f3aea00aaf6b038c
Reuben Pasquini <reuben@frickjack.com>
2018-04-06 14:50:03 -0500

    chore(Jenkinsfile): add Jenkins, tweak run-tests

---
+++ Jenkinsfile
@@ -0,0 +1,57 @@
+#!groovy
+
+pipeline {
+  agent any
+
+  stages {
+    stage('FetchCode') {
+      steps {
+        dir('gen3-qa') {
+          checkout scm
+        }
+        dir('cdis-manifest') {
+          git(
+            url: 'https://github.com/uc-cdis/cdis-manifest.git',
+            branch: 'QA'
+          )
+        }
+        dir('cloud-automation') {
+          git(
+            url: 'https://github.com/uc-cdis/cloud-automation.git',
+            branch: 'chore/jenkins-qa'
+          )
+        }
+      }
+    }
+    stage('K8sDeploy') {
+      steps {
+        withEnv(['GEN3_NOPROXY=true']) {
+          echo "GIT_BRANCH is $env.GIT_BRANCH"
+          echo "GIT_COMMIT is $env.GIT_COMMIT"
+          echo "WORKSPACE is $env.WORKSPACE"
+          sh "bash cloud-automation/tf_files/configs/kube-roll-qa.sh"
+        }
+      }
+    }
+    stage('RunTests') {
+      steps {
+        dir('gen3-qa') {
+          withEnv(['GEN3_NOPROXY=true']) {
+            sh "bash ./run-tests.sh"
+          }
+        }
+      }
+    }
+  }
+  post {
+    success {
+      echo "https://jenkins.planx-pla.net/ $env.JOB_NAME pipeline succeeded"
+    }
+    failure {
+      slackSend color: 'bad', message: "https://jenkins.planx-pla.net $env.JOB_NAME pipeline failed"
+    }
+    unstable {
+      slackSend color: 'bad', message: "https://jenkins.planx-pla.net $env.JOB_NAME pipeline unstable"
+    }
+  }
+}
--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,70 @@
 #!/bin/bash -e
+#
+#
 
-set -e
+_RUN_TESTS=$(dirname "${BASH_SOURCE:-$0}")  # $0 supports zsh
+
+cd "${_RUN_TESTS}"
 npm install
-export ACCESS_TOKEN=$(kubectl exec $(get_pod fence) -it -- fence-create token-create --scopes openid,user,fence,data --type access_token --exp 1800 --username cdis.autotest@gmail.com)
-qa_cred=$( kubectl get secret sheepdog-secret -ojson | jq -r '.data["wsgi.py"]' | base64 --decode | egrep "(\s+'auth':\s\('gdcapi')" | sed "s/.*\(('gdcapi', '[A-Za-z0-9]\{8,\}')\).*/\1/" )
-export INDEX_USER=gdcapi
-export INDEX_PASSWORD=$( [[ $qa_cred =~ ([A-Za-z0-9]{8,}+) ]] && echo ${BASH_REMATCH[1]} )
-npm run test
+
+namespaceList=$(kubectl get namespace -o json | jq -r '.items[].metadata.name')
+
+if [[ $? -ne 0 ]]; then
+  echo "ERROR: failed to retrieve kubernetes namespaces: $namespaceList"
+  exit 1
+fi
+
+
+get_pod() {
+  local name
+  local namespace
+  name="$1"
+  namespace="${2:-default}"
+  pod=$(kubectl --namespace $namespace get pods --output=jsonpath='{range .items[*]}{.metadata.name}  {"\n"}{end}' | grep -m 1 "$name")
+  echo $pod
+}
+
+#
+# Run a test in the specified namespace
+#
+runTest() {
+  local namespace
+  namespace="${1:-default}"
+  (
+    export KUBECTL_NAMESPACE="$namespace"
+    export HOSTNAME=$(kubectl --namespace=${namespace} get configmaps global -ojsonpath='{.data.hostname}')
+    if [[ $? -ne 0 || -z "$HOSTNAME" ]]; then
+      echo "ERROR: failed to retrive HOST_NAME for namespace $namespace"
+      return 1
+    fi
+    export ACCESS_TOKEN=$(kubectl --namespace=${namespace} exec $(get_pod fence $namespace) -- fence-create token-create --scopes openid,user,fence,data --type access_token --exp 1800 --username cdis.autotest@gmail.com)
+    if [[ $? -ne 0 || -z "$ACCESS_TOKEN" ]]; then
+      echo "ERROR: failed to retrieve ACCESS_TOKEN for namespace $namespace"
+      return 1
+    fi
+    qa_cred=$( kubectl get secret sheepdog-secret -ojson | jq -r '.data["wsgi.py"]' | base64 --decode | egrep "(\s+'auth':\s\('gdcapi')" | sed "s/.*\(('gdcapi', '[A-Za-z0-9]\{8,\}')\).*/\1/" )
+    export INDEX_USER=gdcapi
+    export INDEX_PASSWORD=$( [[ $qa_cred =~ ([A-Za-z0-9]{8,}+) ]] && echo ${BASH_REMATCH[1]} )
+    if [[ $? -ne 0 || -z "$INDEX_PASSWORD" ]]; then
+      echo "ERROR: failed to retrieve INDEX_PASSWORD for namespace $namespace"
+      return 1
+    fi
+    cat - <<EOM
+  Running test in $namespace
+  HOSTNAME=$HOSTNAME
+
+EOM
+    # Don't actually run the tests yet ... :-p
+    echo npm run test
+  )
+}
+
+exitCode=0
+for name in ${namespaceList}; do
+  if [[ "$name" == "default" || "$name" =~ ^qa- ]]; then
+    runTest "$name"
+    if [[ $? -ne 0 ]]; then exitCode=1; fi
+  fi
+done
+
+exit $exitCode
