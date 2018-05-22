849b95c4b5e48ce1e5a2454fe692009d08e339d5
avantol <avantol@uchicago.edu>
2018-05-22 15:38:05 -0500

    fix(jenkins): fix automation test in jenkins

--- apis/presignedUrlTest.js
+++ apis/presignedUrlTest.js
@@ -33,6 +33,13 @@ const files = {
     acl: ['test'],
     size: 9,
   },
+  invalid_protocol: {
+    filename: 'test_invalid_protocol',
+    link: 's2://cdis-presigned-url-test/testdata',
+    md5: '73d643ec3f4beb9020eef0beed440ad0',
+    acl: ['test'],
+    size: 9,
+  },
 };
 
 Scenario('test presigned-url', async(I) => {
@@ -52,9 +59,17 @@ Scenario('test presigned-url with file user does not have permission', async(I) 
 Scenario('test presigned-url with invalid protocol', async(I) => {
   return expect(I.seeFileContentEqual(
     '/user/data/download/',
+    files.invalid_protocol.did,
+    ['protocol=s2']
+  )).to.eventually.equal('The specified protocol s2 is not supported');
+});
+
+Scenario('test presigned-url with protocol not available in indexed document', async(I) => {
+  return expect(I.seeFileContentEqual(
+    '/user/data/download/',
     files.allowed.did,
     ['protocol=s2']
-  )).to.eventually.equal('The specified protocol is not supported');
+  )).to.eventually.equal(`File ${files.allowed.did} does not have a location with specified protocol s2.`);
 });
 
 Scenario('test presigned-url with protocol not exist for file', async(I) => {
@@ -68,8 +83,16 @@ Scenario('test presigned-url with protocol not exist for file', async(I) => {
 Scenario('test presigned-url no data', async(I) => {
   return expect(I.seeFileContentEqual(
     '/user/data/download/',
+    files.no_link.did,
+    ['protocol=s3']
+  )).to.eventually.equal(`File ${files.no_link.did} does not have a location with specified protocol s3.`);
+});
+
+Scenario('test presigned-url no requested protocol, no data', async(I) => {
+  return expect(I.seeFileContentEqual(
+    '/user/data/download/',
     files.no_link.did
-  )).to.eventually.equal('Can\'t find a location for the data');
+  )).to.eventually.equal(`Can\'t find any file locations.`);
 });
 
 BeforeSuite((I) => {
--- apis/userTokenTest.js
+++ apis/userTokenTest.js
@@ -13,12 +13,13 @@ let expired_access_token = process.env.EXPIRED_ACCESS_TOKEN;
 
 Scenario('test create APIKey success', async(I) => {
   scope = ['data', 'user'];
-  return expect(I.createAPIKey('/user/credentials/cdis/', scope, access_token)
+  return expect(I.createAPIKey('/user/credentials/api/', scope, access_token)
     .then(
-      (res) => {
-        key_id = res.body.key_id;
-        api_key = res.body.api_key;
-        return res.body;
+      (result) => {
+        console.log(result);
+        key_id = result.key_id;
+        api_key = result.api_key;
+        return result;
       }
     ))
     .to.eventually.have.property('api_key');
@@ -26,28 +27,25 @@ Scenario('test create APIKey success', async(I) => {
 
 Scenario('test create APIKey with expired access token', async(I) => {
   scope = ['data', 'user'];
-  return expect(I.createAPIKey('/user/credentials/cdis/',
-    scope, expired_access_token)
-    .then(
-      (res) => {
-        return res.body;
-      }
-    ))
-    .to.eventually.not.have.property('api_key');
+  let result = I.createAPIKey('/user/credentials/api/',
+    scope, expired_access_token);
+  return expect(result).to.eventually.not.have.property('api_key')
+    && expect(result).to.eventually.have.property('message')
+      .to.eventually.equal('Authentication Error: Signature has expired');
 });
 
 Scenario('test refresh access token with api_key', async(I) => {
-  return expect(I.getAccessToken('/user/credentials/cdis/access_token', api_key))
+  return expect(I.getAccessToken('/user/credentials/api/access_token', api_key))
     .to.eventually.have.property('access_token');
 });
 
 Scenario('test refresh access token with invalid api_key', async(I) => {
-  return expect(I.getAccessToken('/user/credentials/cdis/access_token', 'invalid'))
+  return expect(I.getAccessToken('/user/credentials/api/access_token', 'invalid'))
     .to.eventually.not.have.property('access_token');
 });
 
 Scenario('test refresh access token without api_key', async(I) => {
-  return expect(I.getAccessToken('/user/credentials/cdis/access_token', null))
+  return expect(I.getAccessToken('/user/credentials/api/access_token', null))
     .to.eventually.have.property('message')
     .to.eventually.equal('Please provide an api_key in payload');
 });
