71bd46f9c42e49268fd3f78074ffb4cd232a4c87
Reuben Pasquini <reuben@frickjack.com>
2018-07-09 20:47:37 -0500

    PXD-710 - fix(run-tests.sh): wait for Pending pods (#15)
    * fix(run-tests.sh): wait for Pending pods
    
    don't start the test until the cluster is ready
    
    * chore(mocha-junit-reporter): reports in Jenkins
    
    * fix(Jenkins): kube-wait4-pods and gen3 integration
    
    call through to gen3 scripts in Jenkins
    for interacting with kubernetes
    
    * fix(cloud-automation): Jenkins load master branch

--- run-tests.sh
+++ run-tests.sh
@@ -7,21 +7,12 @@ _RUN_TESTS=$(dirname "${BASH_SOURCE:-$0}")  # $0 supports zsh
 cd "${_RUN_TESTS}"
 npm install
 
-namespaceList="${1:-default}" #$(kubectl get namespace -o json | jq -r '.items[].metadata.name')}
+namespaceList="${1:-default}" 
 
-if [[ $? -ne 0 ]]; then
-  echo "ERROR: failed to retrieve kubernetes namespaces: $namespaceList"
-  exit 1
+if [[ -n "$GEN3_HOME" ]]; then  # load gen3 tools from cloud-automation
+  source "${GEN3_HOME}/gen3/lib/utils.sh"
+  gen3_load "gen3/gen3setup"
 fi
-
-get_pod() {
-  local name
-  local namespace
-  name="$1"
-  namespace="${2:-default}"
-  pod=$(kubectl --namespace $namespace get pods --output=json | jq -r '.items[] | select(.status.phase=="Running") | .metadata.name' | grep -m 1 "$name")
-  echo $pod
-}
 
 #
 # Run a test in the specified namespace
@@ -30,38 +21,57 @@ runTest() {
   local namespace
   namespace="${1:-default}"
   echo $namespace
+  
   (
-    export KUBECTL_NAMESPACE="$namespace"
-    export HOSTNAME=$(kubectl --namespace=${namespace} get configmaps global -ojsonpath='{.data.hostname}')
-    if [[ $? -ne 0 || -z "$HOSTNAME" ]]; then
-      echo "ERROR: failed to retrive HOST_NAME for namespace $namespace"
-      return 1
-    fi
-    export ACCESS_TOKEN=$(kubectl --namespace=${namespace} exec $(get_pod fence $namespace) -- fence-create token-create --scopes openid,user,fence,data,credentials --type access_token --exp 1800 --username cdis.autotest@gmail.com)
-    if [[ $? -ne 0 || -z "$ACCESS_TOKEN" ]]; then
-      echo "ERROR: failed to retrieve ACCESS_TOKEN for namespace $namespace"
-      return 1
-    fi
-    export EXPIRED_ACCESS_TOKEN=$(kubectl --namespace=${namespace} exec $(get_pod fence $namespace) -- fence-create token-create --scopes openid,user,fence,data,credentials --type access_token --exp 1 --username cdis.autotest@gmail.com)
-    if [[ $? -ne 0 || -z "$EXPIRED_ACCESS_TOKEN" ]]; then
-      echo "ERROR: failed to retrieve EXPIRED_ACCESS_TOKEN for namespace $namespace"
-      return 1
-    fi
-    qa_cred=$( kubectl --namespace=${namespace} get secret sheepdog-creds -o json | jq -r '.data["creds.json"]' | base64 --decode | egrep '(\s+"indexd_password":)' | sed 's/.*\("[A-Za-z0-9]\{8,\}"\).*/\1/' )
-    export INDEX_USERNAME=gdcapi
-    export INDEX_PASSWORD=$( [[ $qa_cred =~ ([A-Za-z0-9]{8,}+) ]] && echo ${BASH_REMATCH[1]} )
-    if [[ $? -ne 0 || -z "$INDEX_PASSWORD" ]]; then
-      echo "ERROR: failed to retrieve INDEX_PASSWORD for namespace $namespace"
-      return 1
+    if [[ -n "$GEN3_HOME" ]]; then
+      echo "Acquiring tokens for test authentication"
+      if [[ -n "$namespace" && "$namespace" != "default" ]]; then
+        export KUBECTL_NAMESPACE="$namespace"
+      fi
+      export HOSTNAME=$(g3kubectl get configmaps global -ojsonpath='{.data.hostname}')
+      if [[ $? -ne 0 || -z "$HOSTNAME" ]]; then
+        echo "ERROR: failed to retrive HOST_NAME for namespace $namespace"
+        return 1
+      fi
+
+      export ACCESS_TOKEN=$(g3kubectl exec $(gen3 pod fence $namespace) -- fence-create token-create --scopes openid,user,fence,data,credentials --type access_token --exp 1800 --username cdis.autotest@gmail.com)
+      if [[ $? -ne 0 || -z "$ACCESS_TOKEN" ]]; then
+        echo "ERROR: failed to retrieve ACCESS_TOKEN for namespace $namespace"
+        return 1
+      fi
+      export EXPIRED_ACCESS_TOKEN=$(g3kubectl exec $(gen3 pod fence $namespace) -- fence-create token-create --scopes openid,user,fence,data,credentials --type access_token --exp 1 --username cdis.autotest@gmail.com)
+      if [[ $? -ne 0 || -z "$EXPIRED_ACCESS_TOKEN" ]]; then
+        echo "ERROR: failed to retrieve EXPIRED_ACCESS_TOKEN for namespace $namespace"
+        return 1
+      fi
+      qa_cred=$(g3kubectl get secret sheepdog-creds -o json | jq -r '.data["creds.json"]' | base64 --decode | egrep '(\s+"indexd_password":)' | sed 's/.*\("[A-Za-z0-9]\{8,\}"\).*/\1/' )
+      export INDEX_USERNAME=gdcapi
+      export INDEX_PASSWORD=$( [[ $qa_cred =~ ([A-Za-z0-9]{8,}+) ]] && echo ${BASH_REMATCH[1]} )
+      if [[ $? -ne 0 || -z "$INDEX_PASSWORD" ]]; then
+        echo "ERROR: failed to retrieve INDEX_PASSWORD for namespace $namespace"
+        return 1
+      fi
+    else
+      cat - <<EOM
+GEN3_HOME environment not set, so gen3 tools not available -
+please manually set the environment variables necessary to
+run the integration test suite:
+  HOSTNAME
+  ACCESS_TOKEN 
+  EXPIRED_ACCESS_TOKEN
+  INDEX_USERNAME
+  INDEX_PASSWORD
+  
+EOM
     fi
     cat - <<EOM
-  Running test in $namespace
-  HOSTNAME=$HOSTNAME
+Running test in $namespace
+HOSTNAME=$HOSTNAME
 
 EOM
-    # Don't actually run the tests yet ... :-p
     npm run custom
-    npm run test
+    # see https://codecept.io/reports/
+    ./node_modules/.bin/codeceptjs run --debug --verbose --reporter mocha-junit-reporter
   )
 }
 
