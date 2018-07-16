138440de9eaa3c9aadb1fcf65539d8d84c185f8f
Ted Summer <ted.summer2@gmail.com>
2018-07-16 15:54:37 -0500

    feat(generate-data): comment out rolling pods

--- generate-data.sh
+++ generate-data.sh
@@ -8,6 +8,7 @@ echo $PWD
 _RUN_TESTS=$(dirname "${BASH_SOURCE:-$0}")  # $0 supports zsh
 
 cd "${_RUN_TESTS}"
+cd ../data-simulator
 
 echo $PWD
 
@@ -15,8 +16,8 @@ dictURL=https://s3.amazonaws.com/dictionary-artifacts/genomel-dictionary/master/
 # dictURL=$(g3kubectl get configmaps global -o json | jq '.data.dictionary_url')
 projectName=test
 nData=1
-saveDir=~/TestData/
+saveDir=../TestData/
 
 mkdir -p $saveDir
 
-Rscript ../data-simulator/GenTestDataCmd.R $dictURL $projectName $nData $saveDir
+Rscript GenTestDataCmd.R $dictURL $projectName $nData $saveDir
