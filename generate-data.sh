7159bdc9a0f50533f223b3306a2f8f4ec8eda3fd
Ted Summer <ted.summer2@gmail.com>
2018-07-16 15:56:14 -0500

    feat(generate-data): comment out rolling pods

--- generate-data.sh
+++ generate-data.sh
@@ -12,8 +12,8 @@ cd ../data-simulator
 
 echo $PWD
 
-dictURL=https://s3.amazonaws.com/dictionary-artifacts/genomel-dictionary/master/schema.json
-# dictURL=$(g3kubectl get configmaps global -o json | jq '.data.dictionary_url')
+# dictURL=https://s3.amazonaws.com/dictionary-artifacts/genomel-dictionary/master/schema.json
+dictURL=$(g3kubectl get configmaps global -o json | jq '.data.dictionary_url')
 projectName=test
 nData=1
 saveDir=../TestData/
