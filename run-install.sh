9f335594a887a4ecc7c7e8832c8c02271b0eb967
Ted Summer <ted.summer2@gmail.com>
2018-07-19 14:32:51 -0500

    fix(selenium): wait for selenium on port 4444

--- run-install.sh
+++ run-install.sh
@@ -17,3 +17,23 @@ fi
 
 npm run selenium-install
 npm run selenium-start
+
+START_COUNT=0
+WAIT_COUNT=0
+echo "Starting Selenium..."
+
+while [ -z "$(lsof -t -i tcp:4444)" ]; do
+    if [ "$WAIT_COUNT" -gt 6 ]; then
+        if [ "$START_COUNT" -gt 2 ]; then
+            echo -e "$(red_color "ERROR:") Unable to start Selenium."
+            exit 1
+        fi
+        echo -e "$(red_color "ERROR:") Selenium not started after 30 seconds, restarting"
+        let START_COUNT+=1
+        npm run selenium-start
+    fi
+    let WAIT_COUNT+=1
+    sleep 5
+done
+
+echo "Selenium running"
