9f6c9d41a545313a83f1a38ab4df43af131455f3
Ted Summer <ted.summer2@gmail.com>
2018-08-01 14:16:51 -0500

    fix(stabilize): some cleanup

--- check-pod-health.sh
+++ check-pod-health.sh
@@ -3,20 +3,22 @@
 # curl service endpoints; exit failure if any service returns status code != 200
 
 # Service Health Endpoints
-sheepdog="/api/_status"
-peregrine="/peregrine/_status"
-portal="/"
-fence="/user/jwt/keys"
-health_endpoints=( $sheepdog $peregrine $portal $fence )
+commons_url="https://${KUBECTL_NAMESPACE}.planx-pla.net"
+sheepdog="${commons_url}/api/_status"
+peregrine="${commons_url}/peregrine/_status"
+portal="${commons_url}/"
+fence="${commons_url}/user/jwt/keys"
+selenium="localhost:4444/wd/hub/sessions"
+health_endpoints=( $sheepdog $peregrine $portal $fence $selenium )
 
 exit_code=0
 
 checkHealth() {
-  status=$(curl -s -o /dev/null -w "%{http_code}" "https://${vpc_name}.planx-pla.net${1}")
+  status=$(curl -s -o /dev/null -w "%{http_code}" "${1}")
   if [[ $status != "200" ]]; then
     exit_code=1
   fi
-  printf 'Health %-20s: %s\n' "$1" "$status"
+  printf 'Health %-60s: %s\n' "${1}" "$status"
 }
 
 for health_endpoint in ${health_endpoints[@]}; do
