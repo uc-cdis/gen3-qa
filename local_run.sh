fc1863cc89bee6d5243c869ad061b6fcf0a4dbac
Ted Summer <ted.summer2@gmail.com>
2018-08-20 14:18:50 -0500

    refactor(local_run.sh): slight modification

--- local_run.sh
+++ local_run.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
 # set namespace for script, CHANGE THIS TO CHANGE NAMESPACE
-namespace="qa-kidsfirst"
+namespace="default"
+username=$namespace
+if [[ $namespace == "default" ]]; then
+    username="qaplanetv1"
+fi
+
+envs_file="local_vars.txt"
+test_dur=600 # test duration in seconds
+token_dur=1800 # access token duration
 
 # function to get environment variables
 get_envs() {
-    env_vars=$(ssh $namespace@cdistest.csoc 'bash -s' < extract_envs_for_local.sh $namespace)
+    env_vars=$(ssh ${username}@cdistest.csoc 'bash -s' < extract_envs_for_local.sh $namespace)
     if [[ $? != 0 ]]; then
         echo $env_vars
         return 1
     fi
-    echo $env_vars
-    eval $env_vars
+    echo $env_vars > $envs_file
 }
 
 exit_code=0
+
 # control block to generate new env variables only if the old ones expired
-if [[ -z $TIMESTAMP ]]; then
+if [[ ! -f $envs_file ]]; then
     echo "No environment variables found, generating new ones"
     get_envs
+    source $envs_file
     exit_code=$?
-    echo $exit_code
 else
+    source $envs_file
     timestamp="$(date +"%s")"
-    if [[ "$TIMESTAMP+1800" -lt  "$timestamp" ]]; then
-        echo "env variables expired, regenerating"
+    if [[ "$TIMESTAMP+$token_dur" -lt  "$timestamp+$test_dur" ]]; then
+        echo "Environment variables expired, regenerating"
         get_envs
+        source $envs_file
         exit_code=$?
     else
-        echo "env variables not expired, continuing"
+        echo "Environment variables not expired, continuing"
     fi
 fi
+
+source ./google_creds.sh
