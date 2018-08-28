88ca97b62850ad41addfee6f69ce11d89af8ddba
Ted Summer <ted.summer2@gmail.com>
2018-08-28 14:15:11 -0500

    fix(test_setup): add sourcing for jeknins

--- test_setup.js
+++ test_setup.js
@@ -15,7 +15,7 @@ function userFromNamespace(namespace) {
 function runCommand(cmd, namespace) {
   if (inJenkins) {
     if (process.env.GEN3_HOME) {
-      const sourceCmd = `source "${process.env.GEN3_HOME}/gen3/lib/utils.sh"`; // eslint-disable-line no-template-curly-in-string
+      const sourceCmd = `. "${process.env.GEN3_HOME}/gen3/lib/utils.sh"`; // eslint-disable-line no-template-curly-in-string
       const gen3LoadCmd = 'gen3_load "gen3/gen3setup"';
       console.log(sourceCmd);
       return execSync(`${sourceCmd}; ${gen3LoadCmd}; ${cmd};`);
