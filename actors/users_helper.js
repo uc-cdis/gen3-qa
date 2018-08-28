95bcb1e853ae99c44f3e6d3e8ada85e80d0a0446
Ted Summer <ted.summer2@gmail.com>
2018-08-28 12:55:03 -0500

    feat(setup): automate user and env var setup

---
+++ actors/users_helper.js
@@ -0,0 +1,43 @@
+const ACCESS_TOKEN_MISSING_ERROR = 'No access token was found for user ';
+const GOOGLE_CREDS_MISSING_ERROR = 'No google credentials found for user ';
+
+class User {
+  constructor({ username, envVarsName, jenkinsOnly }) {
+    this.username = username;
+    this.envTokenName = `ACCESS_TOKEN_${envVarsName}`;
+    this.envExpTokenName = `EXPIRED_ACCESS_TOKEN_${envVarsName}`;
+    this.envGoogleEmail = `GOOGLE_EMAIL_${envVarsName}`;
+    this.envGooglePassword = `GOOGLE_PASSWORD_${envVarsName}`;
+    this.jenkinsOnly = jenkinsOnly;
+  }
+
+  get accessToken() {
+    if (process.env[this.envTokenName] === '') {
+      throw Error(ACCESS_TOKEN_MISSING_ERROR + this.username);
+    }
+    return process.env[this.envTokenName];
+  }
+
+  get expiredAccessToken() {
+    if (process.env[this.envExpTokenName] === '') {
+      throw Error(ACCESS_TOKEN_MISSING_ERROR + this.username);
+    }
+    return process.env[this.envExpTokenName];
+  }
+
+  get googleCreds() {
+    if (process.env[this.envGoogleEmail] === '' || process.env[this.envGooglePassword] === '') {
+      throw Error(GOOGLE_CREDS_MISSING_ERROR + this.username);
+    }
+    return {
+      email: process.env[this.envGoogleEmail],
+      password: process.env[this.envGooglePassword],
+    };
+  }
+}
+
+module.exports = {
+  mainAcct: new User({ username: 'cdis.autotest@gmail.com', envVarsName: 'MAIN', jenkinsOnly: false }),
+  auxAcct1: new User({ username: 'dummy-one@planx-pla.net', envVarsName: 'AUX1', jenkinsOnly: true }),
+  auxAcct2: new User({ username: 'smarty-two@planx-pla.net', envVarsName: 'AUX2', jenkinsOnly: true}),
+};
--- test_setup.js
+++ test_setup.js
@@ -3,11 +3,43 @@ const { execSync } = require('child_process');
 const homedir = require('os').homedir();
 
 const commonsHelper = require('./actors/commons_helper');
+const usersHelper = require('./actors/users_helper');
+
+const DEFAULT_TOKEN_EXP = 1800;
+const inJenkins = (process.env.JENKINS_HOME !== '');
+
+function userFromNamespace(namespace) {
+  return namespace === 'default' ? 'qaplanetv1' : namespace;
+}
+
+function runCommand(cmd, namespace) {
+  if (inJenkins) {
+    return execSync(cmd);
+  }
+  const commonsUser = userFromNamespace(namespace);
+  return execSync(`ssh ${commonsUser}@cdistest.csoc 'set -i; source ~/.bashrc; ${cmd}'`);
+}
+
+function getAccessToken(namespace, username, expiration) {
+  const fenceCmd = `g3kubectl exec $(gen3 pod fence ${namespace}) -- fence-create token-create --scopes openid,user,fence,data,credentials --type access_token --exp ${expiration} --username ${username}`
+  const accessToken = runCommand(fenceCmd, namespace);
+  return accessToken.toString('utf8');
+}
+
+function getIndexPassword(namespace) {
+  /* eslint-disable */
+  const qaCredCmd = `qa_cred=$(g3kubectl get secret sheepdog-creds -o json | jq -r '.data["creds.json"]' | base64 --decode | egrep '(\\s+"indexd_password":)' | sed 's/.*\\("[A-Za-z0-9]\\{8,\\}"\\).*/\\1/' )`;
+  const passCmd = '[[ $qa_cred =~ ([A-Za-z0-9]{8,}+) ]] && echo ${BASH_REMATCH[1]}';
+  const fullCmd = `${qaCredCmd}; ${passCmd};`;
+  /* eslint-disable */
+  const indexPassword = runCommand(fullCmd, namespace);
+  return indexPassword.toString('utf8');
+}
 
 function getCommonsVars(namespace) {
   // ssh into the commons/namespace to get access token and other stuff
-  const username = namespace === 'default' ? 'qaplanetv1' : namespace;
-  const execCommonsVars = `ssh ${username}@cdistest.csoc 'bash -s' < extract_envs_for_local.sh ${namespace}`;
+  const commonsUser = userFromNamespace(namespace);
+  const execCommonsVars = `ssh ${commonsUser}@cdistest.csoc 'bash -s' < extract_envs_for_local.sh ${namespace}`;
   const commonsVars = execSync(execCommonsVars);
   return commonsVars.toString('utf8');
 }
@@ -24,6 +56,14 @@ function exportNconfVars() {
   }
 }
 
+function assertEnvVars(varNames) {
+  varNames.forEach((name) => {
+    if (process.env[name] === '') {
+      throw Error(`Missing required environment variable '${name}'`);
+    }
+  });
+}
+
 module.exports = async function (done) {
   console.log('SETUP/BOOTSTRAP OPERATIONS');
   // get some vars from the commons
@@ -37,7 +77,25 @@ module.exports = async function (done) {
     throw Error('Unable to fetch variables from commons.');
   }
 
-  // Create configuration values based on hierarchy the export them to the process
+  // Export access tokens
+  for (const user of Object.values(usersHelper)) {
+    if (inJenkins || !user.jenkinsOnly) {
+      const at = getAccessToken(process.env.NAMESPACE, user.username, DEFAULT_TOKEN_EXP);
+      process.env[user.envTokenName] = at;
+      console.log("RESULT ", at);
+    }
+  }
+
+  // Export expired access token for main acct
+  const mainAcct = usersHelper.mainAcct;
+  const expAccessToken = getAccessToken(process.env.NAMESPACE, mainAcct.username, 1);
+  process.env[mainAcct.envExpTokenName] = expAccessToken;
+
+  // Export indexd credentials
+  process.env.INDEX_USERNAME = 'gdcapi';
+  process.env.INDEX_PASSWORD = getIndexPassword(process.env.namespace);
+
+  // Create configuration values based on hierarchy then export them to the process
   nconf.overrides(commonsVarsJson);
   nconf.argv()
     .env()
@@ -48,6 +106,21 @@ module.exports = async function (done) {
     });
 
   exportNconfVars();
+
+  // Assert required env vars are defined
+  const localVars = [mainAcct.envTokenName, mainAcct.envExpTokenName, 'INDEX_USERNAME', 'INDEX_PASSWORD', 'HOSTNAME'];
+  const jenkinsVars = [
+    'GOOGLE_ACCT1_EMAIL',
+    'GOOGLE_ACCT1_PASSWORD',
+    'GOOGLE_ACCT2_EMAIL',
+    'GOOGLE_ACCT2_PASSWORD',
+    'GOOGLE_APP_CREDS_JSON',
+    'TEST_DATA_PATH',
+  ];
+  assertEnvVars(localVars);
+  if (inJenkins) {
+    assertEnvVars(jenkinsVars);
+  }
 
   // Create a program and project (does nothing if already exists)
   console.log('Creating program/project');
