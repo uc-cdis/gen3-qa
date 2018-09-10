19c70d4d232b191af300c6dbb7b5753d285b8ba2
Ted Summer <ted.summer2@gmail.com>
2018-09-10 16:07:27 -0500

    refactor(jenkins): move sim data to own bash sh

---
+++ jenkins-simulate-data.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+namespace="${1:-default}"
+echo $namespace
+
+if [[ -z "$TEST_DATA_PATH" ]]; then
+    echo "Need to set environment variable TEST_DATA_PATH"
+    exit 1
+fi
+
+_GEN_DATA="$WORKSPACE/data-simulator"
+
+cd "${_GEN_DATA}"
+
+projectName=test
+nData=1
+dictURL=$(g3kubectl get configmaps global -o json | jq -r '.data.dictionary_url')
+if [[ $? -ne 0 || -z "dictURL" ]]; then
+    echo "ERROR: failed to retrieve dictionary_url for namespace $namespace"
+    return 1
+fi
+
+mkdir -p $TEST_DATA_PATH
+
+rCMD="Rscript GenTestDataCmd.R $dictURL $projectName $nData $TEST_DATA_PATH"
+echo $rCMD
+eval $rCMD
+if [[ $? -ne 0 ]]; then
+echo "ERROR: Failed to generate test data for $namespace"
+return 1
+fi
