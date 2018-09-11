cedb29475b4dd357ebdae67b9f790e6f9490f34c
Ted Summer <ted.summer2@gmail.com>
2018-09-11 15:47:14 -0500

    fix(run-tests): don't export TEST_DATA_PATH

--- run-tests.sh
+++ run-tests.sh
@@ -2,13 +2,6 @@
 #
 #
 
-_RUN_TESTS="$PWD"
-_ROOT_DIR="$(dirname "$PWD")"
-_GEN_DATA="$_ROOT_DIR/data-simulator"
-
-export TEST_DATA_PATH="$_ROOT_DIR/TestData/"
-cd "${_RUN_TESTS}"
-
 npm install
 
 namespaceList="${1:-default}"
