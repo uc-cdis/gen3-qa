421efb2a73b671b8a5a17cba8a334714e9d2fc9e
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-01-07 11:55:13 -0600

    fix rebase

--- services/apis/dataClient/dataClientTasks.js
+++ services/apis/dataClient/dataClientTasks.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { execSync } = require('child_process');
 
 const dataClientProps = require('./dataClientProps.js');
-const usersUtil = require('../../../utils/usersUtil.js');
+const usersUtil = require('../../../utils/user.js');
 
 const I = actor();
 const homedir = require('os').homedir();
