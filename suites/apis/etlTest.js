d5ef7734a7066019f0a23ebb154febc7a4d18b6e
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-03-20 12:49:03 -0500

    fix(etl-tests): retry once if failure

--- suites/apis/etlTest.js
+++ suites/apis/etlTest.js
@@ -2,8 +2,8 @@ Feature('ETL');
 
 Scenario('run ETL first time @etl', async (I, etl) => {
   await etl.complete.runETLFirstTime();
-});
+}).retry(1);
 
 Scenario('run ETL second time @etl', async (I, etl) => {
   await etl.complete.runETLSecondTime();
-});
+}).retry(1);
