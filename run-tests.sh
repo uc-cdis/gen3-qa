da8aecbadadefe2f046c338fece18864d43b4f57
Mingfei Shao <mshao1@uchicago.edu>
2019-06-07 14:14:25 -0500

    printouts

--- run-tests.sh
+++ run-tests.sh
@@ -233,12 +233,18 @@ fi
 
 hostname="$(g3kubectl get configmaps manifest-global -o json | jq -r '.data.hostname')"
 portalConfigURL="https://${hostname}/data/config/gitops.json"
-if ! (g3kubectl get pods --no-headers -l app=jupyter-hub | grep jupyter-hub) > /dev/null 2>&1 ||
-! (g3kubectl get pods --no-headers -l app=manifestservice | grep manifestservice) > /dev/null 2>&1 ||
+echo "hostname=$hostname"
+echo "portalConfigURL=$portalConfigURL"
+g3kubectl get pods --no-headers -l app=manifestservice | grep manifestservice
+g3kubectl get pods --no-headers -l app=wts | grep wts
+g3kubectl get pods --no-headers -l app=jupyter-hub | grep jupyter-hub
+curl -s "$portalConfigURL" | jq -r 'contains({dataExplorerConfig: {buttons: [{enabled: true, type: "export-to-workspace"}]}})'
+if ! (g3kubectl get pods --no-headers -l app=manifestservice | grep manifestservice) > /dev/null 2>&1 ||
 ! (g3kubectl get pods --no-headers -l app=wts | grep wts) > /dev/null 2>&1; then
   donot '@exportToWorkspaceAPI'
 fi
-if ! (curl -s "$portalConfigURL" | jq -r 'contains({dataExplorerConfig: {buttons: [{enabled: true, type: "export-to-workspace"}]}})'); then
+if ! (g3kubectl get pods --no-headers -l app=jupyter-hub | grep jupyter-hub) > /dev/null 2>&1 ||
+! (curl -s "$portalConfigURL" | jq -r 'contains({dataExplorerConfig: {buttons: [{enabled: true, type: "export-to-workspace"}]}})'); then
   donot '@exportToWorkspacePortal'
 fi
 
