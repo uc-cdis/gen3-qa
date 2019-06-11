de6dad39d647e14dae61412d11b3a766b95430a2
Mingfei Shao <mshao1@uchicago.edu>
2019-06-11 16:39:56 -0500

    squash/ lock test

--- run-tests.sh
+++ run-tests.sh
@@ -263,7 +263,7 @@ fi
 testArgs="--reporter mocha-multi"
 
 if [[ -n "$doNotRunRegex" ]]; then
-  testArgs="${testArgs} --grep '${doNotRunRegex}' --invert"
+  testArgs="${testArgs} --grep '@exportToWorkspacePortal'"
 fi
 
 exitCode=0
