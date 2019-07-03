579ce32dfca50cc96376f8541f06b31636bc3c96
Zakir Gowani <zakirgowani@gmail.com>
2019-07-03 12:14:35 -0500

    -

--- services/apis/guppy/guppySequences.js
+++ services/apis/guppy/guppySequences.js
@@ -8,41 +8,10 @@ const guppyTasks = require('./guppyTasks.js');
 const guppyProps = require('./guppyProps.js');
 const { Gen3Response } = require('../../../utils/apiUtil');
 const user = require('../../../utils/user.js');
+const generalUtils = require('../../../utils/generalUtils.js');
 const util = require('util')
 
 const fs = require('fs');
-
-function arraysEqual(a, b) {
-  // order of elements matters
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;  
-  for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
-}
-
-function objectsAreEquivalent(a, b) {
-  // http://adripofjavascript.com/blog/drips/object-equality-in-javascript.html
-  var aProps = Object.getOwnPropertyNames(a);
-  var bProps = Object.getOwnPropertyNames(b);
-  if (aProps.length != bProps.length) {
-      console.log('22', aProps, ' ' , bProps);
-      return false;
-  }
-  for (var i = 0; i < aProps.length; i++) {
-      var propName = aProps[i];
-      if (Array.isArray(a[propName]) && !arraysEqual(a, b)) {
-        return false;
-      }
-
-      if (!Array.isArray(a[propName]) && a[propName] !== b[propName]) {
-        return false;
-      }
-  }
-  return true;
-}
 
 function recordWithSubmitterID(id, arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -77,7 +46,7 @@ function matchHistogram(actualResponse, expectedResponse) {
 
     expect(expectedHistogramList.length).to.equal(actualHistogramList.length);
     for(let k = 0; k < expectedHistogramList.length; k++) {
-      expect(objectsAreEquivalent(expectedHistogramList[k], actualHistogramList[k])).to.be.true;
+      expect(generalUtils.objectsAreEquivalent(expectedHistogramList[k], actualHistogramList[k])).to.be.true;
     }    
    }
 }
