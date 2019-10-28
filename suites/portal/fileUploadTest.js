78c7b26762bd3ac17b923f0267e6c84f7bf3907c
Marcelo Costa <marceloc@uchicago.edu>
2019-10-27 23:50:39 -0500

    add comment and escape interpolation chracters

--- suites/portal/fileUploadTest.js
+++ suites/portal/fileUploadTest.js
@@ -1,10 +1,12 @@
 Feature('Data-Portal File Upload test with gen3-client CLI - https://ctds-planx.atlassian.net/browse/PXP-4542');
 
+// To be executed with GEN3_SKIP_PROJ_SETUP=true -- No need to set up program / retrieve access token, etc.
+
 const chai = require('chai');
 const {interactive, ifInteractive} = require('../../utils/interactive.js');
 const expect = chai.expect;
 const hostname = process.env.HOSTNAME;
-const profile = process.eng.NAMESPACE
+const profile = process.env.NAMESPACE
 
 Scenario('Create a folder in $vpc_name and generate a test file @manual', ifInteractive(
     async(I) => {
@@ -30,7 +32,7 @@ Scenario('Run the gen3-client CLI utility and upload the test file to ${profile}
             1. Run the gen3-cli upload command: gen3-client upload --profile=${profile} --upload-path=test.txt
             2. Take note of the GUID that is printed as part of the output
         `);
-    expect(result.didPass, result.details).to,be.true;
+    expect(result.didPass, result.details).to.be.true;
     }
 ));
 
@@ -52,7 +54,7 @@ Scenario('Delete file from the commons system and also from local disk @manual',
             1. Login to the ${hostname} web interface and take note of the value of the "access_token" cookie
             2. Store the access token in an environment variable (export ACCESS_TOKEN="<access_token>")
             2. Run the following curl command to produce an HTTP DELETE request to remove all references of the test file from the system. Make sure you provide the same GUID correspondent to the test file that has been used in previous scenarios. The HTTP Response Code must be 2xx (not 5xx).
-               curl -L -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer ${ACCESS_TOKEN}" -X DELETE 'https://${hostname}/user/data/<GUID>'
+               curl -L -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer \$\{ACCESS_TOKEN\}" -X DELETE 'https://${hostname}/user/data/<GUID>'
             3. Delete the test file from the local disk (rm test.txt)
         `);
     expect(result.didPass, result.details).to.be.true;
