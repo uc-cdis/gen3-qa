4ee9429155e59924e6dd0ffa1d3edd180345e194
Marcelo Costa <marceloc@uchicago.edu>
2019-10-28 17:14:10 -0500

    tweak instructions and add logic to fetch access token

--- suites/portal/fileUploadTest.js
+++ suites/portal/fileUploadTest.js
@@ -2,20 +2,22 @@ Feature('Data-Portal File Upload test with gen3-client CLI - https://ctds-planx.
 
 // To be executed with GEN3_SKIP_PROJ_SETUP=true -- No need to set up program / retrieve access token, etc.
 
+// importing user utils to be able to fetch access_token from target environment
+const user = require('../../utils/user.js');
+
 const chai = require('chai');
 const {interactive, ifInteractive} = require('../../utils/interactive.js');
 const expect = chai.expect;
 const hostname = process.env.HOSTNAME;
 const profile = process.env.NAMESPACE
 
-Scenario('Create a folder in $vpc_name and generate a test file @manual', ifInteractive(
+Scenario('Create a folder and generate a test file @manual', ifInteractive(
     async(I) => {
         const result = await interactive (`
-            1. Login into $vpc_name
-            2. Create a new directory called "test_upload"
-            3. Go to directory /test_upload
-            4. Create a new file (echo "gen3 test" > test.txt)
-            5. Generate a sha256 hash of the test file and take note of the value
+            1. Create a new directory called "test_upload"
+            2. Go to directory /test_upload
+            3. Create a new file (echo "gen3 test" > test.txt)
+            4. Generate a sha256 hash of the test file and take note of the value
                (Linux: cat test.txt | sha256sum)
                (OSX: shasum -a 256 test.txt)
                e.g., % shasum -a 256 test.txt
@@ -29,7 +31,7 @@ Scenario('Create a folder in $vpc_name and generate a test file @manual', ifInte
 Scenario('Run the gen3-client CLI utility and upload the test file to ${profile} @manual', ifInteractive(
     async(I) => {
         const result = await interactive (`
-            1. Run the gen3-cli upload command: gen3-client upload --profile=${profile} --upload-path=test.txt
+            1. Run the gen3-cli upload command and store the output in a log file: gen3-client upload --profile=${profile} --upload-path=test.txt | tee upload.log
             2. Take note of the GUID that is printed as part of the output
         `);
     expect(result.didPass, result.details).to.be.true;
@@ -41,7 +43,8 @@ Scenario('Download the same test file and verify integrity @manual', ifInteracti
         const result = await interactive (`
             1. Run the gen3-cli download command: gen3-client download-single --profile=marcelo --guid=<GUI from the previous scenario>
             2. Generate a sha256 hash of the file that was downloaded (see instructions from the first scenario)
-            3. Check if both files have the same hash (i.e., confirming integrity of the file by comparing the hash values)
+            3. Run a diff command against the hashes to check if both files have the same hash (i.e., confirming integrity of the file by comparing the hash values).
+               diff uploaded-file-sha256 downloaded-file-sha256
         `);
     expect(result.didPass, result.details).to.be.true;
     }
@@ -52,7 +55,7 @@ Scenario('Delete file from the commons system and also from local disk @manual',
     async(I) => {
         const result = await interactive (`
             1. Login to the ${hostname} web interface and take note of the value of the "access_token" cookie
-            2. Store the access token in an environment variable (export ACCESS_TOKEN="<access_token>")
+            2. Store the access token in an environment variable (export ACCESS_TOKEN=${user.mainAcct.accessToken})
             2. Run the following curl command to produce an HTTP DELETE request to remove all references of the test file from the system. Make sure you provide the same GUID correspondent to the test file that has been used in previous scenarios. The HTTP Response Code must be 2xx (not 5xx).
                curl -L -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer \$\{ACCESS_TOKEN\}" -X DELETE 'https://${hostname}/user/data/<GUID>'
             3. Delete the test file from the local disk (rm test.txt)
