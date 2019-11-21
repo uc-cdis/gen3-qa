18e461fd988e1a8e1add1f3d68befe3411cecf32
Marcelo Costa <marceloc@uchicago.edu>
2019-11-21 12:20:57 -0600

    tweak lib to support svc acct registration dry run and Google monitor tests

--- services/apis/fence/fenceTasks.js
+++ services/apis/fence/fenceTasks.js
@@ -294,8 +294,9 @@ module.exports = {
    * @param {int} expires_in - requested expiration time (in seconds)
    * @returns {Promise<Gen3Response>}
    */
-  async registerGoogleServiceAccount(userAcct, googleProject, projectAccessList, expires_in=null) {
+    async registerGoogleServiceAccount(userAcct, googleProject, projectAccessList, expires_in=null, is_dry_run=false) {
     url = fenceProps.endpoints.registerGoogleServiceAccount;
+    if (is_dry_run) url += '/_dry_run';
     if (expires_in) {
       url += `?expires_in=${expires_in}`;
     }
@@ -387,6 +388,18 @@ module.exports = {
       {
         project_access: projectAccessList,
       },
+      userAcct.accessTokenHeader,
+    ).then(res => new Gen3Response(res));
+  },
+
+  /**
+   * Gets the ID of the monitor account (fence-service account)
+   * @param {User} userAcct - User to make request with
+   * @returns {Promise<Gen3Response>}
+   */
+  async getGoogleSvcAcctMonitor(userAcct) {
+    return I.sendGetRequest(
+      fenceProps.endpoints.getGoogleSvcAcctMonitor,
       userAcct.accessTokenHeader,
     ).then(res => new Gen3Response(res));
   },
