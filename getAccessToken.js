45a3a006763282b25791e132c9cf40f1c27d2eb6
Marcelo Costa <marceloc@uchicago.edu>
2019-11-23 15:41:16 -0600

    sanitizing the output and adding comment to clarify where env. variables are coming from

--- getAccessToken.js
+++ getAccessToken.js
@@ -1,5 +1,6 @@
 const { getAccessTokenFromApiKey } = require('./utils/apiUtil');
 
+// The following environment variables are set by 'getAccessToken.sh'
 TARGET_ENVIRONMENT = process.env.GEN3_COMMONS_HOSTNAME;
 // console.log('target environment: ' + TARGET_ENVIRONMENT);
 API_KEY = process.env.API_KEY;
@@ -8,7 +9,7 @@ API_KEY = process.env.API_KEY;
 // obtain access token through fence API
 getAccessTokenFromApiKey(API_KEY, TARGET_ENVIRONMENT)
     .then((ACCESS_TOKEN) => {
-	console.log('access_token: ' + ACCESS_TOKEN);
+	console.log(ACCESS_TOKEN);
     }).catch((reason) => {
 	console.log('Failed: ' + reason.status +' - ' + reason.statusText);
     });
