22a66c59b9d09b8c94ae1b574172c5f410a592a9
haraprasadj <haraprasadj@uchicago.edu>
2019-12-02 11:40:45 -0600

    task/GetAccessButtonTest_and_SynapseLoginTest

--- suites/portal/fileExplorerTest.js
+++ suites/portal/fileExplorerTest.js
@@ -1,8 +1,8 @@
-Feature('File Explorer');
+Feature('File Explorer - https://ctds-planx.atlassian.net/browse/PXP-4777');
 
 /*
 Data Setup:
-    1. Commons_1 - URL configured
+    1. Commons_1 - URL configured in gitops.json ("getAccessButtonLink": "https://dbgap.ncbi.nlm.nih.gov/")
         i) User_11 - No access to some data
         ii) User_12 - Access to all data
     2. Commons_2 - URL not configured
@@ -13,42 +13,42 @@ Data Setup:
 Scenario('Get Access button is enabled when commons has url configured and user does not have access @manual', ifInteractive(
     async (I) => {
         const result = await interactive(`
-            1. Log in to Commons_1 (url configured) as User_11 (no access)
-            2. Navigate to Files Explorer page
+            1. Log in to Commons_1 (URL configured in gitops.json as "getAccessButtonLink": "https://dbgap.ncbi.nlm.nih.gov/") as User_11 (user without access to some data)
+            2. Navigate to Files Explorer page (e.g. https://qa-brain.planx-pla.net/files)
             3. Get Access button is enabled
             4. Clicking on it takes the user to dbgap access request page
         `);
-        expect(result.didPass, result.details).to, be.true;
+        expect(result.didPass, result.details).to,be.true;
     }
 ));
 
 Scenario('Get Access button is not displayed when commons has url configured and user has full access @manual', ifInteractive(
     async (I) => {
         const result = await interactive(`
-            1. Log in to Commons_1 (url configured) as User_12 (full access)
-            2. Navigate to Files Explorer page
+            1. Log in to Commons_1 (URL configured in gitops.json as "getAccessButtonLink": "https://dbgap.ncbi.nlm.nih.gov/") as User_12 (user with full access to data)
+            2. Navigate to Files Explorer page (e.g. https://qa-brain.planx-pla.net/files)
             3. Get Access button is not displayed
         `);
-        expect(result.didPass, result.details).to, be.true;
+        expect(result.didPass, result.details).to,be.true;
     }
 ));
 
 Scenario('Get Access button is disabled when commons does not have url configured and user has no access @manual', ifInteractive(
     async (I) => {
         const result = await interactive(`
-            1. Log in to Commons_2 (url not configured) as User_21 (no access)
-            2. Navigate to Files Explorer page
+            1. Log in to Commons_2 (URL not configured in gitops.json as "getAccessButtonLink") as User_21 (user without access to some data)
+            2. Navigate to Files Explorer page (e.g. https://qa-brain.planx-pla.net/files)
             3. Get Access button is disabled
         `);
-        expect(result.didPass, result.details).to, be.true;
+        expect(result.didPass, result.details).to,be.true;
     }
 ));
 
 Scenario('Get Access button is not displayed when commons does not have url configured and user has full access @manual', ifInteractive(
     async (I) => {
         const result = await interactive(`
-            1. Log in to Commons_2 (url not configured) as User_22 (full access)
-            2. Navigate to Files Explorer page
+            1. Log in to Commons_2 (URL not configured in gitops.json as "getAccessButtonLink") as User_22 (user with full access to data)
+            2. Navigate to Files Explorer page (e.g. https://qa-brain.planx-pla.net/files)
             3. Get Access button is not displayed
         `);
         expect(result.didPass, result.details).to, be.true;
--- suites/portal/homepageTest.js
+++ suites/portal/homepageTest.js
@@ -11,10 +11,11 @@ Scenario('login @portal', (home) => {
 Data Setup:
     1. User_1 - Synapse account having access to a project in a brain commons (Commons_1)
 */
+//User story https://ctds-planx.atlassian.net/browse/PXP-4777
 Scenario('Synapse Digital ID is shown as the username in brain commons @manual', ifInteractive(
     async (I) => {
       const result = await interactive(`
-            1. Log in to Commons_1 (brain commons) as User_1 (synapse account)
+            1. Log in to Commons_1 (brain commons) (e.g. https://qa-brain.planx-pla.net) as User_1 (synapse account)
             2. Verify that the username shown on home page is the Synapse Digital ID
         `);
       expect(result.didPass, result.details).to, be.true;
