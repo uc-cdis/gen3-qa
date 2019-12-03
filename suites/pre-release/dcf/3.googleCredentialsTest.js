a506ff8b11b3b9b448167a8891039b6f06d9aac0
Marcelo Costa <marceloc@uchicago.edu>
2019-12-03 08:41:05 -0600

    adding google temp creds semi-automated scenario

--- suites/pre-release/dcf/3.googleCredentialsTest.js
+++ suites/pre-release/dcf/3.googleCredentialsTest.js
@@ -8,7 +8,7 @@ const fenceProps = require('../../../services/apis/fence/fenceProps.js');
 const user = require('../../../utils/user.js');
 const chai = require('chai');
 const {interactive, ifInteractive} = require('../../../utils/interactive.js');
-const { Gen3Response, getAccessTokenFromExecutableTest, getAccessTokenHeader } = require('../../../utils/apiUtil');
+const { Gen3Response, getAccessTokenFromExecutableTest, getAccessTokenHeader, requestUserInput } = require('../../../utils/apiUtil');
 const expect = chai.expect;
 
 // Test elaborated for nci-crdc but it can be reused in other projects
@@ -16,18 +16,28 @@ const TARGET_ENVIRONMENT = process.env.GEN3_COMMONS_HOSTNAME || 'nci-crdc-stagin
 
 BeforeSuite(async(I) => {
     console.log('Setting up dependencies...');
+    I.cache = {};
     I.TARGET_ENVIRONMENT = TARGET_ENVIRONMENT;
 });
 
-// Scenario #1 - Receive temporary access key
-Scenario(`Obtain temporary credentials (private key) from GCP @manual`, ifInteractive(
-    async(I) => {
-        
+// TODO: Consolidate some of the common scenarios across other executable tests to avoid duplicates
+// e.g., The "Temp Access Keys" check is also included in suites/apis/dataStageOIDCFlowTest.js
+
+// Scenario #1 - Retrieve temporary access keys
+Scenario(`Obtain temporary access keys (Google Credentials) @manual`, ifInteractive(
+    async(I, fence) => {
+	if (!I.cache.ACCESS_TOKEN) I.cache.ACCESS_TOKEN = await requestUserInput("Please provide your ACCESS_TOKEN: ");
+	const http_resp = await fence.do.createTempGoogleCreds(getAccessTokenHeader(I.cache.ACCESS_TOKEN));
+
 	const result = await interactive (`
-              test TBD
+            1. [Automated] Send a HTTP POST request with the NIH user's ACCESS TOKEN to register a service account:
+              HTTP POST request to: https://${TARGET_ENVIRONMENT}${fenceProps.endpoints.googleCredentials}
+            Manual verification:
+              Response status: ${http_resp.status} // Expect a HTTP 200
+              Response data: ${JSON.stringify(http_resp.body) || http_resp.parsedFenceError}
+                // Expect a JSON payload containing client information (id, email, etc.) and a private key
             `);
 	expect(result.didPass, result.details).to.be.true;
     }
 ));
 
- 
