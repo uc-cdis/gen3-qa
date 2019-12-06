c0064e5381931264a28f99d983848c5e3d883d32
Marcelo Costa <marceloc@uchicago.edu>
2019-12-06 08:58:00 -0600

    adding test for public data and controlled data

--- suites/pre-release/dcf/6.indexdTest.js
+++ suites/pre-release/dcf/6.indexdTest.js
@@ -18,19 +18,17 @@ BeforeSuite(async(I) => {
     console.log('Setting up dependencies...');
     // making this data accessible in all scenarios through the actor's memory (the "I" object)
     I.cache = {};
-
-    // Fetching public list of DIDs
-    const http_resp = await I.sendGetRequest(
-	`https://${TARGET_ENVIRONMENT}/index/index`
-    ).then(res => new Gen3Response(res));
-
-    I.cache.records = http_resp.body.records;
 });
 
-Scenario(`Get IndexD data from indexed files @manual`, ifInteractive(
+Scenario(`Get public data record @manual`, ifInteractive(
     async(I, indexd) => {
 	if (!I.cache.ACCESS_TOKEN) I.cache.ACCESS_TOKEN = await requestUserInput("Please provide your ACCESS_TOKEN: ");
-	const guid = I.cache.records[0]['did'];
+	// Fetching public list of DIDs
+	const index_http_resp = await I.sendGetRequest(
+	    `https://${TARGET_ENVIRONMENT}/index/index?acl=*`
+	).then(res => new Gen3Response(res));
+
+	const guid = index_http_resp.body.records[0]['did'];
 	// set a userAcct obj {} with an "accessTokenHeader" property to use Gen3-qa's IndexD testing API
         const http_resp = await indexd.do.getFile(
 	    { did: guid },
@@ -38,7 +36,7 @@ Scenario(`Get IndexD data from indexed files @manual`, ifInteractive(
 	
 	const result = await interactive (`
               1. [Automated] Send a HTTP GET request to retrieve the IndexD record data from GUID [${guid}].
-              HTTP GET request to: https://${TARGET_ENVIRONMENT}${indexdProps.endpoints.get}
+              HTTP GET request to: https://${TARGET_ENVIRONMENT}${indexdProps.endpoints.get}/${guid}
 
               Manual verification:
                 Response status: ${http_resp.status} // Expect a HTTP 200
@@ -49,11 +47,38 @@ Scenario(`Get IndexD data from indexed files @manual`, ifInteractive(
     }
 ));
 
-/*get S3 public data record GET /index/<public_base_id> - PASS
+Scenario(`Get controlled data record @manual`, ifInteractive(
+    async(I, indexd) => {
+	if (!I.cache.ACCESS_TOKEN) I.cache.ACCESS_TOKEN = await requestUserInput("Please provide your ACCESS_TOKEN: ");
+	// Fetching list of ACLs associated with the user
+	const user_http_resp = await I.sendGetRequest(
+	    `https://${TARGET_ENVIRONMENT}/user/user`,
+	    { Authorization: `bearer ${I.cache.ACCESS_TOKEN}` }
+	).then(res => new Gen3Response(res));
 
-get GC public data record GET /index/<public_base_id> - PASS
+	project_access_list = user_http_resp.body.project_access;
+        project_access = Object.keys(project_access_list)[0];
 
-get S3 controlled data record GET /index/<controlled_base_id> - PASS
+	// Fetching list of DIDs matching one of the ACLs from the user
+	const index_http_resp = await I.sendGetRequest(
+	    `https://${TARGET_ENVIRONMENT}/index/index?acl=${project_access}`
+	).then(res => new Gen3Response(res));
 
-get GC controlled data record GET /index/<controlled_base_id> - PASS
-*/
+	const guid = index_http_resp.body.records[0]['did'];
+	// set a userAcct obj {} with an "accessTokenHeader" property to use Gen3-qa's IndexD testing API
+        const http_resp = await indexd.do.getFile(
+	    { did: guid },
+	    { accessTokenHeader: getAccessTokenHeader(I.cache.ACCESS_TOKEN) });
+
+	const result = await interactive (`
+              1. [Automated] Send a HTTP GET request to retrieve the IndexD record data from GUID [${guid}].
+              HTTP GET request to: https://${TARGET_ENVIRONMENT}${indexdProps.endpoints.get}/${guid}
+
+              Manual verification:
+                Response status: ${http_resp.status} // Expect a HTTP 200
+                Response data: ${JSON.stringify(http_resp)}
+              // Expect response containing the index record details
+            `);
+	expect(result.didPass, result.details).to.be.true;
+    }
+));
