75dd5441a2749510c66fbfccec3dd6ff7c69f953
haraprasadj <haraprasadj@uchicago.edu>
2019-12-13 12:03:11 -0600

    adding lint-diff to gen3-qa

--- suites/portal/fileExplorerTest.js
+++ suites/portal/fileExplorerTest.js
@@ -53,7 +53,7 @@ Scenario('Get Access button is not displayed when commons has url configured, hi
             2. Navigate to Files Explorer page (e.g. https://qa-brain.planx-pla.net/files)
             3. Get Access button is not displayed
         `);
-    expect(result.didPass, result.details).to.be.true
+    expect(result.didPass, result.details).to.be.true;
   },
 ));
 
@@ -64,7 +64,7 @@ Scenario('Get Access button is disabled when commons does not have url configure
             2. Navigate to Files Explorer page (e.g. https://qa-brain.planx-pla.net/files)
             3. Get Access button is disabled with a tooltip saying "Coming Soon"
         `);
-    expect(result.didPass, result.details).to.be.true;
+    expect(result.didPass, result.details).to.be.true
   },
 ));
 
