a60fd2df94441512b9864bb86c8e09f39dc260d0
Marcelo Costa <marceloc@uchicago.edu>
2019-12-20 14:06:11 -0600

    add a request failed rate

--- load-testing/fence/presigned-url.js
+++ load-testing/fence/presigned-url.js
@@ -1,19 +1,28 @@
 const { check } = require('k6'); // eslint-disable-line import/no-unresolved
 const http = require('k6/http'); // eslint-disable-line import/no-unresolved
+const { Rate } = require('k6/metrics'); // eslint-disable-line import/no-unresolved
 
 const { GUIDS_LIST, GEN3_HOST, ACCESS_TOKEN } = __ENV; // eslint-disable-line no-undef
 
 // __ENV.GUIDS_LIST is only set when `random-guids` is triggered
 const guids = GUIDS_LIST ? GUIDS_LIST.split(',') : '00037250-f2e5-47e2-863c-5f225c7f79e8';
 
+const myFailRate = new Rate('failed requests');
+
 export const options = {
   stages: [
-    { duration: '15s', target: 50 },
-    { duration: '30s', target: 50 },
+    { duration: '30s', target: 20 },
+    { duration: '60s', target: 50 },
+    { duration: '60s', target: 50 },
+    { duration: '60s', target: 50 },
+    { duration: '60s', target: 50 },
+    { duration: '60s', target: 50 },
+    { duration: '30s', target: 20 },
     { duration: '15s', target: 0 },
   ],
   thresholds: {
-    http_req_duration: ['avg<100', 'p(95)<200'],
+    http_req_duration: ['avg<100', 'p(95)<300'],
+    'failed requests': ['rate<0.1'],
   },
   noConnectionReuse: true,
 };
@@ -27,6 +36,7 @@ export default function () {
     },
   };
   const res = http.get(url, params);
+  myFailRate.add(res.status !== 200);
   check(res, {
     'is status 200': (r) => r.status === 200,
   });
