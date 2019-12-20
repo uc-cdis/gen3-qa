7617f573f6ab2e6ac37ecd49a5699925656eeefb
haraprasadj <haraprasadj@uchicago.edu>
2019-12-20 14:14:06 -0600

    added test cases for tier access and bug fixes caught after linter run

--- suites/portal/tieredAccessTest.js
+++ suites/portal/tieredAccessTest.js
@@ -17,8 +17,8 @@ Scenario('Verify summary charts are displayed for authorized users @manual', ifI
 ));
 
 Scenario('Verify summary charts are displayed only when all values are above TIER_ACCESS_LIMIT for unauthorized users @manual', ifInteractive(
-    async () => {
-        const result = await interactive(`
+  async () => {
+    const result = await interactive(`
             1. Login to a commons (e.g. https://qa-brain.planx-pla.net) as a user with access to none of the projects
             2. Navigate to Data Explorer page (/explorer)
             3. Select a project so that all values in all charts are above the TIER_ACCESS_LIMIT
@@ -26,8 +26,8 @@ Scenario('Verify summary charts are displayed only when all values are above TIE
             5. Select a project so that some values in one of the charts are below the TIER_ACCESS_LIMIT
             6. The summary chart where the values are below the limit is not displayed
         `);
-        expect(result.didPass, result.details).to.be.true;
-    },
+    expect(result.didPass, result.details).to.be.true;
+  },
 ));
 
 /*
@@ -40,14 +40,14 @@ P2 contributes category 'Unknown' to the Race chart the value 30
 P2 contributes category 'Multiracial' to the Race chart the value 20
 */
 Scenario('Verify summary charts displayed correctly for partially authorized users @manual @regression @bugfix', ifInteractive(
-    async () => {
-        const result = await interactive(`
+  async () => {
+    const result = await interactive(`
             1. Login to a commons (e.g. https://qa-brain.planx-pla.net) as a user with access to all projects
             2. Navigate to Data Explorer page (/explorer)
             3. Select projects with and without access that contribute values to charts smaller than the TIER_ACCESS_LIMIT
             4. The summary chart is displayed with values contributed by the projects to which the user has access
             5. With the sample data described above the chart should show Unknown - 40 and not show Multiracial
         `);
-        expect(result.didPass, result.details).to.be.true;
-    },
+    expect(result.didPass, result.details).to.be.true;
+  },
 ));
