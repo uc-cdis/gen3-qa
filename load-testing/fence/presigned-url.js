cbdf8790e254e7319c9868a232ac40d5a60054e6
Marcelo Costa <marceloc@uchicago.edu>
2019-12-22 12:52:41 -0600

    improvements to grafana dashboard

--- load-testing/fence/presigned-url.js
+++ load-testing/fence/presigned-url.js
@@ -1,4 +1,4 @@
-const { check } = require('k6'); // eslint-disable-line import/no-unresolved
+const { check, group, sleep } = require('k6'); // eslint-disable-line import/no-unresolved
 const http = require('k6/http'); // eslint-disable-line import/no-unresolved
 const { Rate } = require('k6/metrics'); // eslint-disable-line import/no-unresolved
 
@@ -10,18 +10,31 @@ const guids = GUIDS_LIST ? GUIDS_LIST.split(',') : '00037250-f2e5-47e2-863c-5f22
 const myFailRate = new Rate('failed requests');
 
 export const options = {
+  rps: 90000,
   stages: [
-    { duration: '30s', target: 20 },
+    { duration: '30s', target: 1 },
+    { duration: '60s', target: 5 },
+    { duration: '60s', target: 5 },
+    { duration: '60s', target: 10 },
+    { duration: '60s', target: 10 },
+    { duration: '60s', target: 15 },
+    { duration: '60s', target: 15 },
+    { duration: '60s', target: 20 },
+    { duration: '60s', target: 20 },
+    { duration: '60s', target: 30 }, // Throughput impact starts here
+    { duration: '60s', target: 30 },
+    { duration: '60s', target: 40 },
+    { duration: '60s', target: 40 },
+    { duration: '60s', target: 50 }, // Objective
     { duration: '60s', target: 50 },
     { duration: '60s', target: 50 },
     { duration: '60s', target: 50 },
-    { duration: '60s', target: 50 },
-    { duration: '60s', target: 50 },
-    { duration: '30s', target: 20 },
-    { duration: '15s', target: 0 },
+    { duration: '60s', target: 30 },
+    { duration: '60s', target: 10 },
+    { duration: '60s', target: 0 },
   ],
   thresholds: {
-    http_req_duration: ['avg<100', 'p(95)<300'],
+    http_req_duration: ['avg<3000', 'p(95)<15000'],
     'failed requests': ['rate<0.1'],
   },
   noConnectionReuse: true,
@@ -35,9 +48,17 @@ export default function () {
       Authorization: `Bearer ${ACCESS_TOKEN}`,
     },
   };
-  const res = http.get(url, params);
-  myFailRate.add(res.status !== 200);
-  check(res, {
-    'is status 200': (r) => r.status === 200,
+  group('Sending PreSigned URL request', () => {
+    group('http get', () => {
+      const res = http.get(url, params, { tags: { name: 'PreSignedURL' } });
+      console.log(`Request performed: ${new Date()}`);
+      myFailRate.add(res.status !== 200);
+      check(res, {
+        'is status 200': (r) => r.status === 200,
+      });
+    });
+    group('wait 1 sec between requests', () => {
+      sleep(1);
+    });
   });
 }
