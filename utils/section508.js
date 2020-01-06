d70a4bc88020b9d8516a8ac273e1dbb663b6adf9
Marcelo Costa <marceloc@uchicago.edu>
2020-01-06 11:10:25 -0600

    additional logging

--- utils/section508.js
+++ utils/section508.js
@@ -1,6 +1,7 @@
 var wcag = require('wcag');
 
-console.log(`Running check against ${process.env.TARGET_GEN3_ENVIRONMENT}}...`);
+console.log(`Running check against ${process.env.TARGET_GEN3_ENVIRONMENT}...`);
+console.log(`Checking first few chars of the AChecker ID: ${process.env.CTDS_QA_ACHECKER.substring(0,3)}`);
 
 var options = {
   id: `${process.env.CTDS_QA_ACHECKER}`,
