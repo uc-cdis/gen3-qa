5bcb7d41ea2a96db368cf51708242d7aa7efcb6a
Marcelo Costa <marceloc@uchicago.edu>
2020-01-23 13:04:02 -0600

    chore(Stage): Scripts to run the biodatacatalyst section 508 compliance report

---
+++ utils/section508.js
@@ -0,0 +1,18 @@
+var wcag = require('wcag');
+
+console.log(`Running check against ${process.env.TARGET_GEN3_ENVIRONMENT}...`);
+console.log(`Checking first few chars of the AChecker ID: ${process.env.CTDS_QA_ACHECKER.substring(0,3)}`);
+
+var options = {
+  id: `${process.env.CTDS_QA_ACHECKER}`,
+  uri: `https://${process.env.TARGET_GEN3_ENVIRONMENT}`,
+  guide: '508'
+};
+
+wcag(options, function (error, data) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
