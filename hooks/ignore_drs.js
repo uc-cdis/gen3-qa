6b7013f8b5ce470f76c7f5dbeeb96ddb60ac6b69
haraprasadj <haraprasadj@uchicago.edu>
2020-03-19 20:08:56 -0500

    fix formatting in the ignore_drs hook

--- hooks/ignore_drs.js
+++ hooks/ignore_drs.js
@@ -2,7 +2,7 @@ const { event } = require('codeceptjs');
 
 module.exports = function () {
   event.dispatcher.on(event.test.before, (test) => {
-    if(test.title.includes('@drs')){
+    if (test.title.includes('@drs')) {
       test.run = () => console.log(`Ignoring test - ${test.title}`);
     }
   });
