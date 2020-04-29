272e2699622c6475f486f31f121c56f06db2bd67
Marcelo Costa <marceloc@uchicago.edu>
2020-04-28 20:06:07 -0500

    change title of the feature

--- suites/portal/indexingPageTest.js
+++ suites/portal/indexingPageTest.js
@@ -1,11 +1,12 @@
 /*
+ Indexing GUI & Indexing/Manifest Sower Jobs (PXP-5786)
  This test plan has a few pre-requisites:
  1. Sower must be deployed and
  2. The environment's manifest must have the indexing jobs declared
     within the sower config block (manifest-indexing & indexd-manifest)
  3. The Indexing GUI is only available in data-portal >= 2.24.9
 */
-Feature('Indexing page - PXP-5786');
+Feature('Indexing GUI');
 
 const { expect } = require('chai');
 const { sleepMS, Gen3Response } = require('../../utils/apiUtil.js');
