6284a157a05fd937fcc5bdd9638243de814cbb84
Marcelo Costa <marceloc@uchicago.edu>
2020-05-15 10:24:07 -0500

    define function

--- scripts/self_service_etl.js
+++ scripts/self_service_etl.js
@@ -2,7 +2,7 @@ const { Bash } = require('./utils/bash');
 const inJenkins = (process.env.JENKINS_HOME !== '' && process.env.JENKINS_HOME !== undefined);
 const bash = new Bash();
 
-async runFrigginETL() {
+async function runFrigginETL() {
   const result = await bash.runCommand('gen3 job run etl');
   console.log(`result: ${result}`);
 }
