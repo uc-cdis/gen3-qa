68c5ae3728831b5d4eae6adc177a280c855e8979
Marcelo Costa <marceloc@uchicago.edu>
2020-05-15 10:59:05 -0500

    remove unused variable

--- scripts/self_service_etl.js
+++ scripts/self_service_etl.js
@@ -1,6 +1,4 @@
 const { Bash } = require('../utils/bash');
-
-const inJenkins = (process.env.JENKINS_HOME !== '' && process.env.JENKINS_HOME !== undefined);
 const bash = new Bash();
 
 async function runFrigginETL() {
