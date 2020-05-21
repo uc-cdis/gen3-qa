6a2c7df6769402ce1afae8577d2b32cbaef7e55e
Marcelo Costa <marceloc@uchicago.edu>
2020-05-21 10:04:36 -0500

    fix command call

--- scripts/self_service_etl.js
+++ scripts/self_service_etl.js
@@ -2,7 +2,7 @@ const { Bash } = require('../utils/bash');
 const bash = new Bash();
 
 async function runFrigginETL() {
-  const cmd1 = await bash.runCommand('gen3 kube-setup-secret');
+  const cmd1 = await bash.runCommand('gen3 kube-setup-secrets');
   console.log(`result: ${cmd1}`);
   const cmd2 = await bash.runCommand('gen3 job run etl');
   console.log(`result: ${cmd2}`);
