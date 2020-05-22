ba5abb93ef3084640bf967b7a22548a581007828
Marcelo Costa <marceloc@uchicago.edu>
2020-05-22 11:19:28 -0500

    set env variables

--- scripts/self_service_gen3_roll.js
+++ scripts/self_service_gen3_roll.js
@@ -3,6 +3,8 @@ const { Bash } = require('../utils/bash');
 const bash = new Bash();
 
 async function runGen3Roll(serviceName) {
+  process.env['NODE_ENV'] = process.env.TARGET_ENVIRONMENT;
+  process.env['GEN3_HOME'] = `${require('path').resolve(__dirname, '..')}/cloud-automation`;
   const cmd1 = await  bash.runCommand(`ssh ${process.env.TARGET_ENVIRONMENT}@cdistest_dev.csoc 'set -i; source ~/.bashrc; cd ~/cdis-manifest && git pull && gen3 roll ${serviceName}'`);
   console.log(`result: ${cmd1}`);
 }
