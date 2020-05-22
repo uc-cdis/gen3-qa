526d74435346e3d3daeb99c5cd1afbfdf11a73f9
Marcelo Costa <marceloc@uchicago.edu>
2020-05-22 10:05:12 -0500

    ssh to target namespace and perform gen3 roll against a service

--- scripts/self_service_gen3_roll.js
+++ scripts/self_service_gen3_roll.js
@@ -3,7 +3,7 @@ const { Bash } = require('../utils/bash');
 const bash = new Bash();
 
 async function runGen3Roll(serviceName) {
-  const cmd1 = await bash.runCommand(`gen3 roll ${serviceName}`);
+  const cmd1 = await  bash.runCommand(`ssh ${(process.env.TARGET_ENVIRONMENT}@cdistest_dev.csoc 'set -i; source ~/.bashrc; cd ~/cdis-manifest && git pull && gen3 roll ${serviceName}`);
   console.log(`result: ${cmd1}`);
 }
 
