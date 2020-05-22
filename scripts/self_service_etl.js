58d878992bf44e18be6eb399105d773c1cf95c20
Marcelo Costa <marceloc@uchicago.edu>
2020-05-22 14:10:49 -0500

    tweaks for jenkins2

--- scripts/self_service_etl.js
+++ scripts/self_service_etl.js
@@ -2,12 +2,10 @@ const { Bash } = require('../utils/bash');
 const bash = new Bash();
 
 async function runFrigginETL() {
-  const cmd1 = await bash.runCommand('gen3 kube-setup-secrets');
+  process.env['KUBECTL_NAMESPACE'] = process.env.TARGET_ENVIRONMENT;
+  process.env['GEN3_HOME'] = `${require('path').resolve(__dirname, '../..')}/cloud-automation`;
+  const cmd1 = await  bash.runCommand(`export KUBECTL_NAMESPACE=${process.env.TARGET_ENVIRONMENT} && gen3 kube-setup-secrets && gen3 job run etl && gen3 roll guppy`);
   console.log(`result: ${cmd1}`);
-  const cmd2 = await bash.runCommand('gen3 job run etl');
-  console.log(`result: ${cmd2}`);
-  const cmd3 = await bash.runCommand('gen3 roll guppy');
-  console.log(`result: ${cmd3}`);
 }
 
 runFrigginETL();
