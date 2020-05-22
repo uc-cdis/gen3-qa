df3a0c63eb3692152e00064bf766128d1e190b88
Marcelo Costa <marceloc@uchicago.edu>
2020-05-22 11:36:51 -0500

    debugging1

--- scripts/self_service_gen3_roll.js
+++ scripts/self_service_gen3_roll.js
@@ -5,6 +5,8 @@ const bash = new Bash();
 async function runGen3Roll(serviceName) {
   process.env['KUBECTL_NAMESPACE'] = process.env.TARGET_ENVIRONMENT;
   process.env['GEN3_HOME'] = `${require('path').resolve(__dirname, '..')}/cloud-automation`;
+  const debug1 = await  bash.runCommand('ls -ilha && pwd && ls -ilha && pwd && ls -ilha');
+  console.log(`result: ${debug1}`);
   const cmd1 = await  bash.runCommand(`gen3 kube-setup-secrets && gen3 roll ${serviceName}`);
   console.log(`result: ${cmd1}`);
 }
