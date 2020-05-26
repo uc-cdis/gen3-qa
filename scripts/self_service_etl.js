ec17c582824c6d91b28bcc07ea9f04f7b4bbde01
Marcelo Costa <marceloc@uchicago.edu>
2020-05-26 11:55:48 -0500

    remove guppy roll as we need to wait until etl is finished

--- scripts/self_service_etl.js
+++ scripts/self_service_etl.js
@@ -4,7 +4,7 @@ const bash = new Bash();
 async function runFrigginETL() {
   process.env['KUBECTL_NAMESPACE'] = process.env.TARGET_ENVIRONMENT;
   process.env['GEN3_HOME'] = `${require('path').resolve(__dirname, '../..')}/cloud-automation`;
-  const cmd1 = await  bash.runCommand(`export KUBECTL_NAMESPACE=${process.env.TARGET_ENVIRONMENT} && gen3 kube-setup-secrets && gen3 job run etl && gen3 roll guppy`);
+  const cmd1 = await  bash.runCommand(`export KUBECTL_NAMESPACE=${process.env.TARGET_ENVIRONMENT} && gen3 kube-setup-secrets && gen3 job run etl`);
   console.log(`result: ${cmd1}`);
 }
 
