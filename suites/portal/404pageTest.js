c848e55e67514507f6061fb1aeb71586b6ec3894
haraprasadj <haraprasadj@uchicago.edu>
2020-06-04 10:09:47 -0500

    adding negative tests

--- suites/portal/404pageTest.js
+++ suites/portal/404pageTest.js
@@ -4,6 +4,18 @@ const { expect } = require('chai');
 
 Feature('Redirect invalid urls to 404 page');
 
+Scenario('Valid urls are not redirected to 404 page @manual', ifInteractive(
+  async () => {
+    const result = interactive(`
+              1. Navigate to a valid url like https://qa-brain.planx-pla.net/profile
+              2. User is not redirected to the 404 page
+              3. Navigate to a valid url like https://qa-brain.planx-pla.net/DEV-test
+              4. User is redirected to the 404 page
+          `);
+    expect(result.didPass, result.details).to.be.true;
+  },
+));
+
 Scenario('Invalid urls get redirected to 404 page @manual', ifInteractive(
   async () => {
     const result = interactive(`
