ab3c94b175bd90b622fc48b752cd9b9dfea10bba
Marcelo Costa <marceloc@uchicago.edu>
2020-06-15 10:38:19 -0500

    solve issues pointed out by the linter

--- scripts/self_service_gen3_roll.js
+++ scripts/self_service_gen3_roll.js
@@ -1,13 +1,14 @@
+const path = require('path');
 const { Bash } = require('../utils/bash');
 
 const bash = new Bash();
 
 async function runGen3Roll(serviceName) {
-  process.env['KUBECTL_NAMESPACE'] = process.env.TARGET_ENVIRONMENT;
-  process.env['GEN3_HOME'] = `${require('path').resolve(__dirname, '../..')}/cloud-automation`;
-  const debug1 = await  bash.runCommand('ls && pwd && cd .. && ls -ilha && pwd && ls && cd .. && ls');
+  process.env.KUBECTL_NAMESPACE = process.env.TARGET_ENVIRONMENT;
+  process.env.GEN3_HOME = `${path.resolve(__dirname, '../..')}/cloud-automation`;
+  const debug1 = await bash.runCommand('ls && pwd && cd .. && ls -ilha && pwd && ls && cd .. && ls');
   console.log(`result: ${debug1}`);
-  const cmd1 = await  bash.runCommand(`export KUBECTL_NAMESPACE=${process.env.TARGET_ENVIRONMENT} && gen3 kube-setup-secrets && gen3 roll ${serviceName}`);
+  const cmd1 = await bash.runCommand(`export KUBECTL_NAMESPACE=${process.env.TARGET_ENVIRONMENT} && gen3 kube-setup-secrets && gen3 roll ${serviceName}`);
   console.log(`result: ${cmd1}`);
 }
 
