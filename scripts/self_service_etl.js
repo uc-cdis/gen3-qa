81e73fa5274baa0507fcd4b8a536100e529a1e5e
Marcelo Costa <marceloc@uchicago.edu>
2020-06-15 10:28:28 -0500

    fix issues pointed out by linter

--- scripts/self_service_etl.js
+++ scripts/self_service_etl.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const { Bash } = require('../utils/bash');
+
 const bash = new Bash();
 
 async function runFrigginETL() {
-  process.env['KUBECTL_NAMESPACE'] = process.env.TARGET_ENVIRONMENT;
-  process.env['GEN3_HOME'] = `${require('path').resolve(__dirname, '../..')}/cloud-automation`;
-  const cmd1 = await  bash.runCommand(`export KUBECTL_NAMESPACE=${process.env.TARGET_ENVIRONMENT} && gen3 kube-setup-secrets && gen3 job run etl`);
+  process.env.KUBECTL_NAMESPACE = process.env.TARGET_ENVIRONMENT;
+  process.env.GEN3_HOME = `${path.resolve(__dirname, '../..')}/cloud-automation`;
+  const cmd1 = await bash.runCommand(`export KUBECTL_NAMESPACE=${process.env.TARGET_ENVIRONMENT} && gen3 kube-setup-secrets && gen3 job run etl`);
   console.log(`result: ${cmd1}`);
 }
 
