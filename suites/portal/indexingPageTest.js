6f2e9da330fb9b0a554cd3f178d0b0d58e919f68
Marcelo R Costa <themarcelor@gmail.com>
2020-07-01 18:26:49 -0500

    Revert "chore(qa): Refactor apiUtils (#388)"
    This reverts commit 26e5788718e5f5031f8ef483b3354af8dc725e30.

--- suites/portal/indexingPageTest.js
+++ suites/portal/indexingPageTest.js
@@ -60,7 +60,7 @@ Scenario('Navigate to the indexing page and upload a test manifest @indexing', a
   indexing.do.goToIndexingPage();
   I.waitForElement({ css: '.indexing-page' }, 10);
   I.click('.index-flow-form'); // after clicking open window file upload dialog
-  I.attachFile('input[type=\'file\']', `manifest_${I.cache.UNIQUE_NUM}.tsv`);
+  await I.attachFile('input[type=\'file\']', `manifest_${I.cache.UNIQUE_NUM}.tsv`);
   I.click({ xpath: 'xpath: //button[contains(text(), \'Index Files\')]' });
 
   await checkPod('manifest-indexing');
@@ -126,10 +126,10 @@ Scenario('Navigate to the indexing page and upload an invalid manifest @indexing
   indexing.do.goToIndexingPage();
   I.waitForElement({ css: '.indexing-page' }, 10);
   I.click('.index-flow-form'); // after clicking open window file upload dialog
-  I.attachFile('input[type=\'file\']', `invalid_manifest_${I.cache.UNIQUE_NUM}.tsv`);
+  await I.attachFile('input[type=\'file\']', `invalid_manifest_${I.cache.UNIQUE_NUM}.tsv`);
   I.click({ xpath: 'xpath: //button[contains(text(), \'Index Files\')]' });
 
-  await checkPod('manifest-indexing', { nAttempts: 5, ignoreFailure: true });
+  await checkPod('manifest-indexing');
 
   const nAttempts = 12;
   for (let i = 0; i < nAttempts; i += 1) {
