a43bd28615bd8dbe2d9bdb1dad556e9c43dd18e4
Mingfei Shao <mshao1@uchicago.edu>
2020-07-14 15:13:26 -0500

    create

--- jenkins-simulate-data.sh
+++ jenkins-simulate-data.sh
@@ -67,7 +67,7 @@ which python3
 /usr/bin/curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 sed -i '1 s/\<python\>/python3/' $HOME/.poetry/bin/poetry
 which poetry
-$HOME/.poetry/bin/poetry config virtualenvs.create false
+# $HOME/.poetry/bin/poetry config virtualenvs.create false
 $HOME/.poetry/bin/poetry install -vv
 #python setup.py develop --user
 
