44ebaf70414944851075108f03b0c88d96326b29
Marcelo Costa <marceloc@uchicago.edu>
2020-07-21 22:39:22 -0500

    more refactoring

--- suites/batch/bucketManifestGenerationTest.js
+++ suites/batch/bucketManifestGenerationTest.js
@@ -36,7 +36,7 @@ const expectedMetadataForAssertions = {
   },
 };
 
-BeforeSuite(async (I, files) => {
+function deleteLingeringInfra() {
   console.log('deleting manifest_bucket-manifest-ci-test*.tsv files...');
   /* eslint-disable no-useless-escape */
   bash.runCommand(`
@@ -49,15 +49,24 @@ BeforeSuite(async (I, files) => {
 
   console.log('deleting infra from previous runs that might\'ve been interrupted...');
   const lingeringInfra = bash.runCommand(`
-    gen3 bucket-manifest --list
+    gen3 bucket-manifest --list | xargs -i echo "{} "
   `);
-  const jobIdsFromPreviousRuns = lingeringInfra.split('\n');
-  console.log(`Found old jobs in this namespace:\n  ${jobIdsFromPreviousRuns}`);
-  jobIdsFromPreviousRuns.forEach((jobId) => {
-    bash.runCommand(`
-      echo yes | gen3 bucket-manifest --cleanup --job-id ${jobId}
-    `);
-  });
+
+  if (lingeringInfra.length > 0) {
+    console.log(`Found jobs in this namespace:\n  ${lingeringInfra}`);
+    const jobIdsFromPreviousRuns = lingeringInfra.trim().split(' ');
+
+    jobIdsFromPreviousRuns.forEach((jobId) => {
+      console.log(`Tearing down infrastructure from job ${jobId}`);
+      bash.runCommand(`
+        echo yes | gen3 bucket-manifest --cleanup --job-id ${jobId}
+      `);
+    });
+  }
+}
+
+BeforeSuite(async (I, files) => {
+  deleteLingeringInfra();
 
   console.log('Setting up dependencies...');
   I.cache = {};
@@ -68,20 +77,8 @@ BeforeSuite(async (I, files) => {
 });
 
 AfterSuite(async (I) => {
-  console.log('deleting temporary bucket...');
-  const deletingTempBucket = bash.runCommand(`
-    echo yes | gen3 bucket-manifest --cleanup --job-id ${I.cache.jobId} | tail -n5
-  `);
-  console.log(`deletingTempBucket: ${deletingTempBucket}`);
-
-  console.log('deleting manifest_bucket-manifest-ci-test*.tsv files...');
-  bash.runCommand(`
-    find . -name "manifest_bucket-manifest-ci-test*.tsv" -exec rm {} \\\; -exec echo "deleted {}" \\\;
-  `);
-  console.log('deleting authz_mapping_*.tsv files...');
-  bash.runCommand(`
-    find . -name "authz_mapping_*.tsv" -exec rm {} \\; -exec echo "deleted {}" \\;
-  `);
+  console.log(`I.cache: ${JSON.stringify(I.cache)}`);
+  deleteLingeringInfra();
 });
 
 // Scenario #1 - Generate indexd manifest out of an s3 bucket
@@ -93,21 +90,26 @@ Scenario('Generate bucket manifest from s3 bucket @bucketManifest', async (I) =>
   console.log('gen3 bucket-manifest process initiated. Waiting for infrastructure provisioning...');
 
   await sleepMS(20000);
-  await checkPod('aws-bucket-manifest', 'gen3job', params = { nAttempts: 30, ignoreFailure: false }); // eslint-disable-line no-undef
+  await checkPod('aws-bucket-manifest', 'gen3job', params = { nAttempts: 40, ignoreFailure: false }); // eslint-disable-line no-undef
 
   const bucketManifestList = bash.runCommand(`
-    gen3 bucket-manifest --list
+    gen3 bucket-manifest --list | xargs -i echo "{} "
   `);
   console.log(`bucketManifestList: ${bucketManifestList}`);
 
-  if (bucketManifestList.split('\n').length > 1) {
+  if (bucketManifestList.trim().split(' ').length > 1) {
     throw new Error(`ERROR: Found more than one jobId on namespace ${process.env.KUBECTL_NAMESPACE}.`);
   }
 
   const bucketManifestJobStatus = bash.runCommand(`
-    gen3 bucket-manifest --status --job-id ${bucketManifestList.trim()} | tail -n4
+    gen3 bucket-manifest --status --job-id ${bucketManifestList.trim()} | tail -n10
   `);
   console.log(`bucketManifestJobStatus: ${bucketManifestJobStatus}`);
+
+  const contentsOfTheFolder = bash.runCommand(`
+    ls -ilha
+  `);
+  console.log(`contentsOfTheFolder: ${contentsOfTheFolder}`);
 
   const bucketManifestJobDataRaw = fs.readFileSync('paramFile.json', 'utf8');
   const bucketManifestJobData = JSON.parse(bucketManifestJobDataRaw);
@@ -149,4 +151,4 @@ Scenario('Generate bucket manifest from s3 bucket @bucketManifest', async (I) =>
       ).to.be.equal(expectedMetadataForAssertions[typeOfFile][assertionKey]);
     });
   });
-}).retry(1);
+});
