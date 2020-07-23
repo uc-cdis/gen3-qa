5fe601d451a49b07c88c4f15f370b301df8635be
haraprasadj <haraprasadj@uchicago.edu>
2020-07-23 07:53:29 -0500

    fixed uuidv4 function

--- load-testing/sheepdog/import-clinical-metadata.js
+++ load-testing/sheepdog/import-clinical-metadata.js
@@ -1,7 +1,6 @@
-/* eslint-disable space-infix-ops */
 /* eslint-disable no-mixed-operators */
 /* eslint-disable no-bitwise */
-
+/* eslint-disable one-var */
 const {
   check,
   group,
@@ -32,7 +31,11 @@ export const options = {
 
 export default function () {
   function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, (c) => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 
   const program = 'DEV';
