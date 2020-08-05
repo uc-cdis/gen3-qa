25a5f7e80942c5cd5fc1f8e76c5931dcc9f1264e
Marcelo Costa <marceloc@uchicago.edu>
2020-08-04 23:48:14 -0500

    check for presence of test user 2 creds

--- suites/apis/rasIntegrationTest.js
+++ suites/apis/rasIntegrationTest.js
@@ -35,7 +35,13 @@ BeforeSuite(async (I) => {
   } else if (process.env.RAS_TEST_USER_1_PASSWORD === undefined) {
     throw new Error('ERROR: There is no password defined for RAS Test User 1. Please declare the "RAS_TEST_USER_1_PASSWORD" environment variable and try again. Aborting test...');
   }
-  // If running this locally, uncomment the lines below
+  if (process.env.RAS_TEST_USER_2_USERNAME === undefined) {
+    throw new Error('ERROR: There is no username defined for RAS Test User 2. Please declare the "RAS_TEST_USER_2_USERNAME" environment variable and try again. Aborting test...');
+  } else if (process.env.RAS_TEST_USER_2_PASSWORD === undefined) {
+    throw new Error('ERROR: There is no password defined for RAS Test User 2. Please declare the "RAS_TEST_USER_2_PASSWORD" environment variable and try again. Aborting test...');
+  }
+
+  // Deleting registered clients for idempotent local runs
   const deleteClientCmd1 = 'fence-create --arborist http://arborist-service/ client-delete --client ras-user1-test-client';
   const deleteClientForRASUser1 = bash.runCommand(deleteClientCmd1, 'fence', takeLastLine);
   console.log(`deleteClientForRASUser1: ${deleteClientForRASUser1}`);
