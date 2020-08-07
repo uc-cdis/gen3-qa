f73bc8bf44b094ed7beac54b33de710cdc1cc76d
Marcelo Costa <marceloc@uchicago.edu>
2020-08-07 18:06:47 -0500

    test everything

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,6 @@
 #!groovy
 
-@Library('cdis-jenkins-lib@chore/improve_notifications_suggesting_labels') _
+@Library('cdis-jenkins-lib@master') _
 
 testPipeline {
   // tell the pipeline to not checkout `gen3-qa:master`
--- suites/portal/homepageTest.js
+++ suites/portal/homepageTest.js
@@ -9,7 +9,7 @@ Scenario('login @portal', (home) => {
 
 // To be merged with the above once all Commons move to portal version
 // with top bar login button
-Scenario('login @portal @topBarLogin', () => {
+Scenario('login @portal @topBarLogin', (home) => {
   home.do.goToHomepage();
   home.complete.topBarLogin();
   home.ask.seeDetails();
