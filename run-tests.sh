1fc5a218e924486f0ed01fdf1e4231415e7e0b71
Marcelo Costa <marceloc@uchicago.edu>
2020-08-07 17:32:44 -0500

    add test run condition

--- run-tests.sh
+++ run-tests.sh
@@ -223,6 +223,7 @@ runTestsIfServiceVersion "@indexRecordConsentCodes" "sheepdog" "1.1.13"
 runTestsIfServiceVersion "@coreMetadataPage" "portal" "2.20.8"
 runTestsIfServiceVersion "@indexing" "portal" "2.26.0" "2020.05"
 runTestsIfServiceVersion "@rasAuthz" "fence" "4.22.1"
+runTestsIfServiceVersion "@cleverSafe" "fence" "4.22.4" "2020.09"
 
 # environments that use DCF features
 # we only run Google Data Access tests for cdis-manifest PRs to these
