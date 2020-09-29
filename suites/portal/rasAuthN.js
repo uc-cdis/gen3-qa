ea7b2f80bea49034ca1a2d70c72553eb1de60de2
haraprasadj <haraprasadj@uchicago.edu>
2020-09-28 23:44:18 -0500

    refactoring

--- suites/portal/rasAuthN.js
+++ suites/portal/rasAuthN.js
@@ -10,6 +10,7 @@
               "client_id": "****",
               "client_secret": "****",
               "redirect_url": "{{BASE_URL}}/login/ras/callback"
+              "discovery_url": "<DISCOVERY_URL"
           }
  3. Environment variables containing the NIH USERNAME & PASSWORD of test users must be
     declared either locally or through Jenkins credentials and Groovy (WithCredentials())
@@ -22,9 +23,29 @@ Feature('RASAuthN - Negative Tests');
 const { expect } = require('chai');
 const { sleepMS } = require('../../utils/apiUtil.js');
 const { Bash, takeLastLine } = require('../../utils/bash.js');
+const { registerRasClient } = require('../../utils/rasAuthN');
 
 const bash = new Bash();
 const I = actor();
+
+const rasAuthLogin = async (username, password) => {
+  const { clientID } = registerRasClient(username);
+  const authUrl = '/user/oauth2/authorize?'
+    + 'response_type=code'
+    + `&client_id=${clientID}`
+    + `&redirect_uri=https://${process.env.HOSTNAME}/user`
+    + '&scope=openid+user+data+google_credentials+ga4gh_passport_v1'
+    + '&idp=ras';
+  await sleepMS(3000);
+  I.amOnPage(authUrl);
+  I.saveScreenshot('NIH_Login_1.png');
+
+  I.fillField('USER', username);
+  I.fillField('PASSWORD', password);
+  I.click({ xpath: 'xpath: //button[contains(text(), \'Sign in\')]' });
+  await sleepMS(3000);
+  I.saveScreenshot('NIH_Login_2.png');
+};
 
 Before(async () => {
   console.log('Setting up dependencies...');
@@ -42,79 +63,24 @@ Before(async () => {
   console.log(`deleteClientForRASUser1: ${deleteClientForRASUser1}`);
 });
 
-const registerRasClient = (username) => {
-  const registerClientCmd = 'fence-create'
-    + ' --arborist http://arborist-service/ client-create'
-    + ' --client ras-user1-test-client'
-    + ` --user ${username}`
-    + ` --urls https://${process.env.HOSTNAME}/user`
-    + ' --policies programs.QA-admin programs.test-admin programs.DEV-admin programs.jnkns-admin'
-    + ' --allowed-scopes openid user data google_credentials ga4gh_passport_v1';
-
-  const registerClientForRASUser1 = bash.runCommand(registerClientCmd, 'fence', takeLastLine);
-  console.log(`registerClientForRASUser1: ${registerClientForRASUser1}`);
-
-  const re = /\('(.*)',\s'(.*)'\)/;
-  const clientID = registerClientForRASUser1.match(re)[1];
-  const secretID = registerClientForRASUser1.match(re)[2];
-
-  return {
-    clientID,
-    secretID,
-  };
-};
-
-const rasAuthLogin = async (url, username, password) => {
-  I.amOnPage(url);
-  await sleepMS(3000);
-  I.saveScreenshot('NIH_Login_1.png');
-
-  I.fillField('USER', username);
-  I.fillField('PASSWORD', password);
-  I.click({ xpath: 'xpath: //button[contains(text(), \'Sign in\')]' });
-  await sleepMS(3000);
-  I.saveScreenshot('NIH_Login_2.png');
-};
-
 Scenario('Provide invalid credentials in NIH Login page @rasAuthN @negativeTest', async () => {
   // Using RAS Test User 1
-  const rasClient = registerRasClient(process.env.RAS_TEST_USER_1_USERNAME);
-
-  const authUrl = `/user/oauth2/authorize
-  ?response_type=code&client_id=${rasClient.clientID}
-  &redirect_uri=https://${process.env.HOSTNAME}/user
-  &scope=openid+user+data+google_credentials+ga4gh_passport_v1&idp=ras`;
-
-  await rasAuthLogin(authUrl, process.env.RAS_TEST_USER_1_USERNAME, 'THIS_IS_AN_INVALID_PASSWORD_FOR_USER_1');
-
+  await rasAuthLogin(process.env.RAS_TEST_USER_1_USERNAME, 'THIS_IS_AN_INVALID_PASSWORD_FOR_USER_1');
   I.seeTextEquals('Login Failed', 'h1');
 });
 
 Scenario('Click on Deny button in RAS Authorization page @rashAuthN @negativeTest', async () => {
   // Using RAS Test User 1
-  const rasClient = registerRasClient(process.env.RAS_TEST_USER_1_USERNAME);
-  console.log(rasClient);
-
-  const authUrl = `/user/oauth2/authorize
-  ?response_type=code&client_id=${rasClient.clientID}
-  &redirect_uri=https://${process.env.HOSTNAME}/user
-  &scope=openid+user+data+google_credentials+ga4gh_passport_v1&idp=ras`;
-
-  await rasAuthLogin(authUrl, process.env.RAS_TEST_USER_1_USERNAME,
-    process.env.RAS_TEST_USER_1_PASSWORD);
+  await rasAuthLogin(process.env.RAS_TEST_USER_1_USERNAME, process.env.RAS_TEST_USER_1_PASSWORD);
 
   const postNIHLoginURL = await I.grabCurrentUrl();
   if (postNIHLoginURL === 'https://stsstg.nih.gov/auth/oauth/v2/authorize/consent') {
     I.click({ xpath: 'xpath: //input[@value=\'Deny\']' });
   }
   I.saveScreenshot('NIH_Login_3.png');
-  // await sleepMS(3000);
-  let urlWithCode = await I.grabCurrentUrl();
+  await sleepMS(3000);
+  const urlWithCode = await I.grabCurrentUrl();
   console.log(`URL With Code - ${urlWithCode}`);
   expect(urlWithCode).to.contain('error_description=The+resource_owner+denied+access+to+resources',
     'The error message is not as expected');
-  await sleepMS(3000);
-  I.saveScreenshot('NIH_Login_4.png');
-  urlWithCode = await I.grabCurrentUrl();
-  console.log(`URL With Code - ${urlWithCode}`);
 });
