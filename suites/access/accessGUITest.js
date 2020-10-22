54e86c285f0129d88e1a2b757f4bb03d41f7551b
Marcelo Costa <marceloc@uchicago.edu>
2020-10-22 18:13:28 -0500

    tweak comments

--- suites/access/accessGUITest.js
+++ suites/access/accessGUITest.js
@@ -9,7 +9,7 @@
     access-backend.env  dbcreds.json  user.yaml
  3. Test Credentials:
     - SUPER_ADMIN: Declared in the "access-backend.env" file.
-      e.g., export SUPER_ADMINS='cdis.autotest@gmail.com'
+      e.g., export SUPER_ADMINS='admin.userX@uchicago.edu'
     - ADMIN: Added through the Access GUI.
 */
 Feature('Access GUI');
