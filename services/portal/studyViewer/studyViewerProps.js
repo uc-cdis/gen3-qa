9b06ccc667978e8c51b3448dc0463c755cf3eb2f
Atharva Rane <atharvar@uchicago.edu>
2020-11-13 10:38:09 -0500

    fixing lint

--- services/portal/studyViewer/studyViewerProps.js
+++ services/portal/studyViewer/studyViewerProps.js
@@ -1,28 +1,22 @@
-/**
-*Explorer Page Property
-*/
-
 const userEndPoint = '/requestor/request/user';
 const requestEndPoint = '/requestor/request';
 
 module.exports = {
-    endpoint: {
-        userEndPoint,
-        requestEndPoint,
-    },
-    path: '/study-viewer/clinical_trials',
-    datasetPath: '/study-viewer/clinical_trials/ACTT',
+  endpoint: {
+    userEndPoint,
+    requestEndPoint,
+  },
+  path: '/study-viewer/clinical_trials',
+  datasetPath: '/study-viewer/clinical_trials/ACTT',
+  studyViewerDivClass: '.study-viewer',
+  datasetDivClass: '.study-viewer__title',
+  detailedButtonXPath: '.ant-collapse-header',
 
-    studyViewerDivClass: '.study-viewer',
-    datasetDivClass: '.study-viewer__title',
-    detailedButtonXPath: '.ant-collapse-header',
+  learnMoreButtonXPath: '//button[contains(text(), \'Learn More\')]',
+  loginRAButtonXPath: '//button[contains(text(), \'Login to Request Access\')]',
+  requestAccessButtonXPath: '//button[contains(text(), \'Request Access\')]',
+  downloadButtonXPath: '//button[contains(text(), \'Download\')]',
 
-    learnMoreButtonXPath: '//button[contains(text(), \'Learn More\')]',
-    loginRAButtonXPath: '//button[contains(text(), \'Login to Request Access\')]',
-    requestAccessButtonXPath: '//button[contains(text(), \'Request Access\')]',
-    downloadButtonXPath: '//button[contains(text(), \'Download\')]',
-
-    inactiveDivClass: '.ant-collapse-content-inactive',
-    activeDivClass: '.ant-collapse-content-active',
+  inactiveDivClass: '.ant-collapse-content-inactive',
+  activeDivClass: '.ant-collapse-content-active',
 };
-
--- services/portal/studyViewer/studyViewerTasks.js
+++ services/portal/studyViewer/studyViewerTasks.js
@@ -1,78 +1,76 @@
-const studyViewerProps =  require('./studyViewerProps.js');
-const users = require("../../../utils/user");
-const stringify = require('json-stringify-safe');
+const studyViewerProps = require('./studyViewerProps.js');
+const users = require('../../../utils/user');
 
 const I = actor();
 
 module.exports = {
 
-    goToStudyViewerPage() {
-        I.amOnPage(studyViewerProps.path); ///study-viewer/clinical_trials
-        I.waitForVisible(studyViewerProps.studyViewerDivClass, 5); //.study-viewer
-        I.saveScreenshot(`study_viewer_page.png`);
-    },
+  goToStudyViewerPage() {
+    I.amOnPage(studyViewerProps.path); /// study-viewer/clinical_trials
+    I.waitForVisible(studyViewerProps.studyViewerDivClass, 5); // .study-viewer
+    I.saveScreenshot('study_viewer_page.png');
+  },
 
-    goToStudyPage() {
-        // /study-viewer/clinical_trials/ACTT
-        I.amOnPage(studyViewerProps.datasetPath);
-        I.waitForVisible(studyViewerProps.datasetDivClass, 5);
-        I.saveScreenshot('dataset_page.png');
-    },
+  goToStudyPage() {
+    // /study-viewer/clinical_trials/ACTT
+    I.amOnPage(studyViewerProps.datasetPath);
+    I.waitForVisible(studyViewerProps.datasetDivClass, 5);
+    I.saveScreenshot('dataset_page.png');
+  },
 
-    async clickRequestAccess() {
-        await I.seeElement(studyViewerProps.requestAccessButtonXPath);
-        I.click(studyViewerProps.requestAccessButtonXPath);
-    },
+  async clickRequestAccess() {
+    await I.seeElement(studyViewerProps.requestAccessButtonXPath);
+    I.click(studyViewerProps.requestAccessButtonXPath);
+  },
 
-    async loginToRequestAccess() {
-        await I.seeElement(studyViewerProps.loginRAButtonXPath);
-        I.click(studyViewerProps.loginRAButtonXPath);
-    },
+  async loginToRequestAccess() {
+    await I.seeElement(studyViewerProps.loginRAButtonXPath);
+    I.click(studyViewerProps.loginRAButtonXPath);
+  },
 
-    async clickDownload() {
-        await I.seeElement(studyViewerProps.downloadButtonXPath);
-        I.click(studyViewerProps.downloadButtonXPath);
-    },
+  async clickDownload() {
+    await I.seeElement(studyViewerProps.downloadButtonXPath);
+    I.click(studyViewerProps.downloadButtonXPath);
+  },
 
-    async learnMoreButton() {
-        await I.seeElement(studyViewerProps.detailedButtonXPath);
-        I.click(studyViewerProps.detailedButtonXPath);
-        await I.seeElement(studyViewerProps.activeDivClass);
-        I.saveScreenshot('.png');
-        await I.seeElement(studyViewerProps.learnMoreButtonXPath);
-        I.click(studyViewerProps.learnMoreButtonXPath);
-        await I.seeElement(studyViewerProps.studyViewerDivClass);
-    },
+  async learnMoreButton() {
+    await I.seeElement(studyViewerProps.detailedButtonXPath);
+    I.click(studyViewerProps.detailedButtonXPath);
+    await I.seeElement(studyViewerProps.activeDivClass);
+    I.saveScreenshot('.png');
+    await I.seeElement(studyViewerProps.learnMoreButtonXPath);
+    I.click(studyViewerProps.learnMoreButtonXPath);
+    await I.seeElement(studyViewerProps.studyViewerDivClass);
+  },
 
-    async getRequestId() {
-        const getResponse = await I.sendGetRequest(
-            `${studyViewerProps.endpoint.userEndPoint}`,
-            users.mainAcct.accessTokenHeader,
-        );
-        const responseData = getResponse.data;
-        const req_id = responseData[0].request_id;
-        console.log(`### request id: ${req_id}`);
-        return req_id;
-    },
+  async getRequestId() {
+    const getResponse = await I.sendGetRequest(
+      `${studyViewerProps.endpoint.userEndPoint}`,
+      users.mainAcct.accessTokenHeader,
+    );
+    const responseData = getResponse.data;
+    const reqID = responseData[0].request_id;
+    console.log(`### request id: ${reqID}`);
+    return reqID;
+  },
 
-    async putRequest() {
-        let req_id_put = await this.getRequestId();
-        console.log(`### put request id: ${req_id_put}`);
-        //sending PUT request /requestor/request/${req_id} endpoint
-        await I.sendPutRequest(
-            `${studyViewerProps.endpoint.requestEndPoint}/${req_id_put}`,
-            { "status": "APPROVED" },
-            users.mainAcct.accessTokenHeader,
-        );
-    },
+  async putRequest() {
+    const reqIDPut = await this.getRequestId();
+    console.log(`### put request id: ${reqIDPut}`);
+    // sending PUT request /requestor/request/${req_id} endpoint
+    await I.sendPutRequest(
+      `${studyViewerProps.endpoint.requestEndPoint}/${reqIDPut}`,
+      { status: 'APPROVED' },
+      users.mainAcct.accessTokenHeader,
+    );
+  },
 
-    async deleteRequest() {
-        let req_id_del = await this.getRequestId();
-        console.log(`### delete request id: ${req_id_del}`);
-        await I.sendDeleteRequest(
-            `${studyViewerProps.endpoint.requestEndPoint}/${req_id_del}`,
-            users.mainAcct.accessTokenHeader,
-        );
-    }
+  async deleteRequest() {
+    const reqIDDel = await this.getRequestId();
+    console.log(`### delete request id: ${reqIDDel}`);
+    await I.sendDeleteRequest(
+      `${studyViewerProps.endpoint.requestEndPoint}/${reqIDDel}`,
+      users.mainAcct.accessTokenHeader,
+    );
+  },
 };
-
