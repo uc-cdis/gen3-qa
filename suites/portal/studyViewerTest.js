3f74b57ec07b60962a4fce4fde09886684d83689
Atharva Rane <atharvar@uchicago.edu>
2020-11-13 09:43:58 -0500

    resolving eslint issue

--- suites/portal/studyViewerTest.js
+++ suites/portal/studyViewerTest.js
@@ -3,46 +3,43 @@ Feature('Study Viewer');
 const studyViewerTasks = require('../../services/portal/studyViewer/studyViewerTasks.js');
 const studyViewerProps = require('../../services/portal/studyViewer/studyViewerProps.js');
 
-/* Setup after the test*/
-After(async (home)=> {
-   home.complete.logout();
-   //TBC - delete the access for the mainAcct user from arborist and requestor db
+/* Setup after the test */
+After(async (home) => {
+  home.complete.logout();
+  // TBC - delete the access for the mainAcct user from arborist and requestor db
 });
 
 // User does not log in and has no access. User see 'Login to Request access' button
 Scenario('User logs in and requests the access @studyViewer', async (I, home, users, login) => {
-   home.do.goToHomepage();
-   login.complete.login(users.mainAcct);
-   studyViewerTasks.goToStudyPage();
-   await studyViewerTasks.clickRequestAccess();
-   //request id from requestor db
-   await studyViewerTasks.getRequestId();
-   await studyViewerTasks.putRequest();
-   I.refreshPage();
-   I.wait(5);
-   // await studyViewerTasks.clickDownload();
-   await studyViewerTasks.deleteRequest();
-   },
-);
+  home.do.goToHomepage();
+  login.complete.login(users.mainAcct);
+  studyViewerTasks.goToStudyPage();
+  await studyViewerTasks.clickRequestAccess();
+  // request id from requestor db
+  await studyViewerTasks.getRequestId();
+  await studyViewerTasks.putRequest();
+  I.refreshPage();
+  I.wait(5);
+  // await studyViewerTasks.clickDownload();
+  await studyViewerTasks.deleteRequest();
+});
 
 // User doesnot login and want to request access
-Scenario('User doesnot login and requests the access', async(I, users, login) => {
-    studyViewerTasks.goToStudyPage();
-    await studyViewerTasks.loginToRequestAccess();
-    login.ask.isCurrentPage();
-    login.complete.login(users.mainAcct);
-    studyViewerTasks.goToStudyPage();
-    await I.waitForElement(studyViewerProps.requestAccessButtonXPath, 5);
-    },
-);
+Scenario('User doesnot login and requests the access', async (I, users, login) => {
+  studyViewerTasks.goToStudyPage();
+  await studyViewerTasks.loginToRequestAccess();
+  login.ask.isCurrentPage();
+  login.complete.login(users.mainAcct);
+  studyViewerTasks.goToStudyPage();
+  await I.waitForElement(studyViewerProps.requestAccessButtonXPath, 5);
+});
 
 Scenario('Navigation to the detailed dataset page', async (I, home, users, login) => {
-    home.do.goToHomepage();
-    login.complete.login(users.mainAcct);
-    studyViewerTasks.goToStudyViewerPage();
-    await studyViewerTasks.learnMoreButton();
-  },
-);
+  home.do.goToHomepage();
+  login.complete.login(users.mainAcct);
+  studyViewerTasks.goToStudyViewerPage();
+  await studyViewerTasks.learnMoreButton();
+});
 
 // For download feature
 // /* user with access and can download the dataset*/
@@ -54,4 +51,3 @@ Scenario('Navigation to the detailed dataset page', async (I, home, users, login
 //     await studyViewer.clickDownload();
 //     },
 // );
-
