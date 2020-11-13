d0b167ee269644f9c6a095d629c7b3ab3bd56c63
Marcelo Costa <marceloc@uchicago.edu>
2020-11-13 15:17:53 -0600

    resolve issues pointed out by eslint

--- suites/portal/landingPageButtonsTest.js
+++ suites/portal/landingPageButtonsTest.js
@@ -1,6 +1,6 @@
 /*
  Automated tests for Landing Page buttons (PXP-6216)
- This test plan assumes the presence of one or more default 
+ This test plan assumes the presence of one or more default
  landing page with the following cards:
   1. Define Data Field (redirects user to #hostname/DD)
   2. Explore Data (redirects user to #hostname/explorer)
@@ -13,40 +13,41 @@ const { expect } = require('chai');
 const { sleepMS } = require('../../utils/apiUtil.js');
 
 // Login and navigate to the landing page to instrument buttons and assert they are working
-Scenario('Navigate to the landing page and click on buttons @landing', async ({ I, home, users }) => {
+Scenario('Navigate to the landing page and click on buttons @landing', async ({ I, home }) => {
   home.do.goToHomepage();
   home.complete.login();
 
   const buttons = [
     {
       text: 'Learn more',
-      expectedUrl: 'DD'
+      expectedUrl: 'DD',
     },
     {
       text: 'Explore data',
-      expectedUrl: 'explorer'
+      expectedUrl: 'explorer',
     },
     {
       text: 'Query data',
-      expectedUrl: 'query'
+      expectedUrl: 'query',
     },
     {
       text: 'Run analysis',
-      expectedUrl: 'workspace'
-    }
+      expectedUrl: 'workspace',
+    },
   ];
 
   I.scrollPageToBottom();
   I.saveScreenshot('Landing_page_cards_and_buttons.png');
 
   for (const [key, button] of Object.entries(buttons)) {
+    console.log(`checking: ${key}`);
     I.waitForElement({ css: '.index-button-bar' }, 10);
-    btExists = await tryTo(() => I.seeElement({"xpath": `//button[contains(text(), \'${button.text}\')]`}) );
+    const btExists = await tryTo(() => I.seeElement({ xpath: `//button[contains(text(), '${button.text}')]` })); // eslint-disable-line no-undef
     if (btExists) {
-      assertionFailureMsg = `button ${button.text} did not redirect user to expected url ${button.expectedUrl}.`;
-      I.click({"xpath": `//button[contains(text(), \'${button.text}\')]`});
+      const assertionFailureMsg = `button ${button.text} did not redirect user to expected url ${button.expectedUrl}.`;
+      I.click({ xpath: `//button[contains(text(), '${button.text}')]` });
       await sleepMS(500);
-      theUrl = await I.grabCurrentUrl();
+      const theUrl = await I.grabCurrentUrl();
       expect(
         theUrl,
         assertionFailureMsg,
@@ -56,5 +57,5 @@ Scenario('Navigate to the landing page and click on buttons @landing', async ({ 
     }
     // Go back to the landing page and try to find / click on the buttons again
     I.amOnPage('/');
-  };
+  }
 });
