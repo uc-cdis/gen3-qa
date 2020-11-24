a1cf8d946e1b31a67de14bb0b022d7f998ab241b
Marcelo Costa <marceloc@uchicago.edu>
2020-11-24 16:08:49 -0600

    add semicolons as per eslint

--- suites/apis/etlTest.js
+++ suites/apis/etlTest.js
@@ -1,13 +1,13 @@
 Feature('ETL');
 
 Scenario('run ETL first time @etl', async ({ etl }) => {
-  console.log(`${Date.now()}: Before run ETL first time`)
+  console.log(`${Date.now()}: Before run ETL first time`);
   await etl.complete.runETLFirstTime();
-  console.log(`${Date.now()}: After run ETL first time`)
+  console.log(`${Date.now()}: After run ETL first time`);
 }).retry(1);
 
 Scenario('run ETL second time @etl', async ({ etl }) => {
-  console.log(`${Date.now()}: Before run ETL second time`)
+  console.log(`${Date.now()}: Before run ETL second time`);
   await etl.complete.runETLSecondTime();
-  console.log(`${Date.now()}: After run ETL second time`)
+  console.log(`${Date.now()}: After run ETL second time`);
 }).retry(1);
