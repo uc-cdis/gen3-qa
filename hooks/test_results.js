b0d8ded60383ca8e78e9f1dbf79e40a7d4ce4594
Marcelo Costa <marceloc@uchicago.edu>
2020-11-25 19:56:08 -0600

    solve issues pointed out by eslint

--- hooks/test_results.js
+++ hooks/test_results.js
@@ -9,78 +9,74 @@ const influx = new Influx.InfluxDB({
   database: 'ci_metrics',
 });
 
-async function writeMetrics(measurement, test) {
-    // test metrics
-    const suiteName = test.parent.title.split(' ').join('_');
-    const testName = test.title.split(' ').join('_');
-    const ciEnvironment = process.env.KUBECTL_NAMESPACE;
-    const duration = test.duration / 1000;
-    // eslint-disable-next-line no-underscore-dangle
-    const retries = test._retries;
-    // eslint-disable-next-line no-underscore-dangle
-    const currentRetry = test._currentRetry;
+async function writeMetrics(measurement, test, currentRetry) {
+  // test metrics
+  const suiteName = test.parent.title.split(' ').join('_');
+  const testName = test.title.split(' ').join('_');
+  const ciEnvironment = process.env.KUBECTL_NAMESPACE;
+  const duration = test.duration / 1000;
 
-    // github metrics
-    let prName = '';
-    let repoName = '';
-    try {
-      prName = process.env.BRANCH_NAME.split('-')[1]; // eslint-disable-line prefer-destructuring
-      repoName = process.env.JOB_NAME.split('/')[1]; // eslint-disable-line prefer-destructuring
-    } catch {
-      prName = 'UNDEFINED';
-      repoName = 'UNDEFINED';
-    }
+  // github metrics
+  let prName = '';
+  let repoName = '';
+  try {
+    prName = process.env.BRANCH_NAME.split('-')[1]; // eslint-disable-line prefer-destructuring
+    repoName = process.env.JOB_NAME.split('/')[1]; // eslint-disable-line prefer-destructuring
+  } catch {
+    prName = 'UNDEFINED';
+    repoName = 'UNDEFINED';
+  }
 
-    // selenium metrics
-    const resp = await fetch('http://selenium-hub:4444/status');
-    const respJson = await resp.json();
-    let sessionCount = 0;
-    const { nodes } = respJson.value;
-    if (nodes.length > 0) {
-      nodes.forEach((node) => {
-        node.slots.forEach((slot) => {
-          if (slot.session) {
-            sessionCount += 1;
-          }
-        });
+  // selenium metrics
+  const resp = await fetch('http://selenium-hub:4444/status');
+  const respJson = await resp.json();
+  let sessionCount = 0;
+  const { nodes } = respJson.value;
+  if (nodes.length > 0) {
+    nodes.forEach((node) => {
+      node.slots.forEach((slot) => {
+        if (slot.session) {
+          sessionCount += 1;
+        }
       });
-    }
+    });
+  }
 
-    // logs
-    console.log('********');
-    console.log(`TEST: ${testName}`);
-    console.log(`RESULT: ${test.state}`);
-    console.log(`CURRENT RETRY - ${currentRetry}`);
-    console.log(`TIMESTAMP: ${new Date()}`);
-    console.log(`GRID_SESSION_COUNT: ${sessionCount}`);
-    console.log(`TEST_DURATION: ${duration}s`);
-    console.log('********');
+  // logs
+  console.log('********');
+  console.log(`TEST: ${testName}`);
+  console.log(`RESULT: ${test.state}`);
+  console.log(`CURRENT RETRY - ${currentRetry}`);
+  console.log(`TIMESTAMP: ${new Date()}`);
+  console.log(`GRID_SESSION_COUNT: ${sessionCount}`);
+  console.log(`TEST_DURATION: ${duration}s`);
+  console.log('********');
 
-    // define information to write into time-series db
-    const field_info = measurement === 'run_time' ? duration : 1;
+  // define information to write into time-series db
+  const fieldInfo = measurement === 'run_time' ? duration : 1;
 
-    tsData = {};
-    tsData[measurement] = field_info;
+  const tsData = {};
+  tsData[measurement] = fieldInfo;
 
-    // writing metrics
-    await influx.writePoints(
-      [{
-        measurement: measurement,
-        tags: {
-          repo_name: repoName,
-          pr_num: prName,
-          suite_name: suiteName,
-          test_name: testName,
-          ci_environment: ciEnvironment,
-          selenium_grid_sessions: sessionCount,
-          run_time: duration,
-        },
-        fields: tsData,
-      }],
-      { precision: 's' }
-   ).catch((err) => {
-     console.error(`Error saving data to InfluxDB! ${err}`);
-   });
+  // writing metrics
+  await influx.writePoints(
+    [{
+      measurement,
+      tags: {
+        repo_name: repoName,
+        pr_num: prName,
+        suite_name: suiteName,
+        test_name: testName,
+        ci_environment: ciEnvironment,
+        selenium_grid_sessions: sessionCount,
+        run_time: duration,
+      },
+      fields: tsData,
+    }],
+    { precision: 's' },
+  ).catch((err) => {
+    console.error(`Error saving data to InfluxDB! ${err}`);
+  });
 }
 
 module.exports = function () {
@@ -92,15 +88,15 @@ module.exports = function () {
     // eslint-disable-next-line no-underscore-dangle
     const currentRetry = test._currentRetry;
     if (testResult === 'failed' && retries <= currentRetry) {
-      await writeMetrics('fail_count', test)
+      await writeMetrics('fail_count', test, currentRetry);
     }
     if (testResult === 'passed') {
-      await writeMetrics('pass_count', test)
+      await writeMetrics('pass_count', test, currentRetry);
     }
     if (currentRetry > 0 && (testResult === 'passed' || retries === currentRetry)) {
-      await writeMetrics('retry_count', test)
+      await writeMetrics('retry_count', test, currentRetry);
     }
-    await writeMetrics('run_time', test)
+    await writeMetrics('run_time', test, currentRetry);
   });
 
   event.dispatcher.on(event.suite.before, (suite) => {
