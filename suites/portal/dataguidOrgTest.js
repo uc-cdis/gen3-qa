0a9badb89fa74fb5a5f1ea971d93ccff57a98615
jingh8 <jingh8@uchicago.edu>
2020-12-10 12:37:23 -0600

    fix DRS url

--- suites/portal/dataguidOrgTest.js
+++ suites/portal/dataguidOrgTest.js
@@ -42,14 +42,12 @@ Data(nonexistentGuids).Scenario('Negativetest resolving for non-exitent guids @d
 
 // Test DRS endpoint
 Data(correctGuids).Scenario('Test if DRSendpoint resolve the guids correctly @dataguids', ({ I, current }) => {
-  I.amOnPage(testURL);
-  I.amOnPage(`/index/ga4gh/drs/v1/objects/${current.guids}`);
+  I.amOnPage(testURL + `/index/ga4gh/drs/v1/objects/${current.guids}`);
   I.see(`${current.guids}`);
 });
 
 // Nagative DRS endpoint test
 Data(nonexistentGuids).Scenario('Negativetest DRSendpoint with non-existent guids @dataguids', ({ I, current }) => {
-  I.amOnPage(testURL);
-  I.amOnPage(`/index/ga4gh/drs/v1/objects/${current.guids}`);
+  I.amOnPage(testURL + `/index/ga4gh/drs/v1/objects/${current.nguids}`);
   I.see('"error":"no record found"');
 });
