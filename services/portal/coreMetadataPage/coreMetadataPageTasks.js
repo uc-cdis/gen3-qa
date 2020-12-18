dd375b663200a3ca71e63273e7f22c1d6f123dd8
Atharva Rane <atharvar@uchicago.edu>
2020-12-18 10:34:29 -0600

    fix

--- services/portal/coreMetadataPage/coreMetadataPageTasks.js
+++ services/portal/coreMetadataPage/coreMetadataPageTasks.js
@@ -9,7 +9,8 @@ module.exports = {
   goToCoreMetadataPage(guid) {
     const coreMetadataPagePath = `${coreMetadataPageProps.filesPagePath}/${guid}`;
     I.amOnPage(coreMetadataPagePath);
-    I.waitForVisible(coreMetadataPageProps.coreMetadataPageClass, 10);
     I.saveScreenshot('coremetadata_page.png');
+    I.waitForVisible(coreMetadataPageProps.coreMetadataPageClass, 10);
+
   },
 };
