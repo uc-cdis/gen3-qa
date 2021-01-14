52e0b7909c03095f3d83c6330f76fced53e48869
Marcelo Costa <marceloc@uchicago.edu>
2021-01-13 18:25:49 -0600

    chore(ci): Add retries to data upload test

--- suites/portal/dataUploadTest.js
+++ suites/portal/dataUploadTest.js
@@ -94,7 +94,7 @@ Scenario('Map uploaded files in windmill submission page @dataUpload @portal', a
 
   // user1 should see 0 files now because all files are mapped.
   portalDataUpload.complete.checkUnmappedFilesAreInSubmissionPage(I, []);
-});
+}).retry(2);
 
 Scenario('Cannot see files uploaded by other users @dataUpload @portal', async ({
   I, sheepdog, nodes, files, fence, users, indexd, portalDataUpload, dataUpload,
