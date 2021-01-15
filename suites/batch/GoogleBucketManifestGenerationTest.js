65093be6585d6dbd8bee927364a8d8ac7176bd25
Marcelo Costa <marceloc@uchicago.edu>
2021-01-15 16:12:58 -0600

    fix issues pointed out by eslint

--- suites/batch/GoogleBucketManifestGenerationTest.js
+++ suites/batch/GoogleBucketManifestGenerationTest.js
@@ -145,7 +145,7 @@ Scenario('Generate bucket manifest from s3 bucket @googleStorage @batch @bucketM
   // Final assertions
   const files = ['test_file', 'humongous_file'];
   for (let i = 0; i < files.length; i++) { // eslint-disable-line no-plusplus
-    Object.keys(expectedMetadataForAssertions[files[i]]).forEach(( assertionKey ) => {
+    Object.keys(expectedMetadataForAssertions[files[i]]).forEach((assertionKey) => {
       console.log(
         `Running assertion for ${files[i]} (index: ${i}) - TSV header: ${assertionKey}...`,
       );
