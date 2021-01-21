491f8d6cfb0e61276363bba602f066e2c7c36468
Marcelo Costa <marceloc@uchicago.edu>
2021-01-20 22:25:34 -0600

    only run additional check inside jenkins

--- suites/portal/dataUploadTest.js
+++ suites/portal/dataUploadTest.js
@@ -10,6 +10,7 @@ const createdFileNames = [];
 let submitterID;
 
 const bash = new Bash();
+const inJenkins = (process.env.JENKINS_HOME !== '' && process.env.JENKINS_HOME !== undefined);
 
 const generateFileAndGetUrlFromFence = async function (files, fence, accessTokenHeader) {
   // generate a file unique to this session
@@ -40,35 +41,34 @@ const uploadFile = async function (I, dataUpload, indexd, sheepdog, nodes, fileO
   // upload the file to the S3 bucket using the presigned URL
   await dataUpload.uploadFileToS3(presignedUrl, filePath, fileSize);
 
-  // make sure the file shows up in S3
-  let fileFound = false;
-  const nAttempts = 6;
-  let bucketName = '';
-  if (process.env.NAMESPACE !== '' && process.env.NAMESPACE !== undefined) {
-    bucketName = 'qaplanetv1-data-bucket';
-  } else {
-    bucketName = `${process.env.NAMESPACE}-databucket-gen3`;
-  }
-  for (let i = 1; i < nAttempts; i += 1) {
-    try {
-      console.log(`waiting for file [${fileName}] with guid [${fileGuid}] to show up on ${bucketName}... - attempt ${i}`);
-      await sleepMS(10000);
+  // additional scrutiny for file upload only when running inside Jenkins
+  if(inJenkins) {
+    let fileFound = false;
+    const nAttempts = 6;
+    const jenkinsNamespace = process.env.HOSTNAME.replace(".planx-pla.net", "")
+    let bucketName = `${jenkinsNamespace}-databucket-gen3`;
+  
+    for (let i = 1; i < nAttempts; i += 1) {
+      try {
+        console.log(`waiting for file [${fileName}] with guid [${fileGuid}] to show up on ${bucketName}... - attempt ${i}`);
+        await sleepMS(10000);
 
-      const contentsOfTheBucket = await bash.runCommand(`aws s3 ls s3://${bucketName}/${fileGuid}`);
-      console.log(`contentsOfTheBucket: ${contentsOfTheBucket}`);
-      if (!fileFound) {
-        if (contentsOfTheBucket.includes(fileName)) {
-          console.log(`the file ${fileName} was found! Proceed with the rest of the test...`);
-          fileFound = true;
+        const contentsOfTheBucket = await bash.runCommand(`aws s3 ls s3://${bucketName}/${fileGuid}`);
+        console.log(`contentsOfTheBucket: ${contentsOfTheBucket}`);
+        if (!fileFound) {
+          if (contentsOfTheBucket.includes(fileName)) {
+            console.log(`the file ${fileName} was found! Proceed with the rest of the test...`);
+            fileFound = true;
+          }
+        } else {
+          console.log('The file did now show up in the bucket yet...');
+          if (i === nAttempts) {
+            throw new Error(`Max number of attempts reached: ${i}`);
+          }
         }
-      } else {
-        console.log('The file did now show up in the bucket yet...');
-        if (i === nAttempts) {
-          throw new Error(`Max number of attempts reached: ${i}`);
-        }
+      } catch (e) {
+        throw new Error(`Failed to upload the file ${fileObj} to bucket ${bucketName} on attempt ${i}: ${e.message}`);
       }
-    } catch (e) {
-      throw new Error(`Failed to upload the file ${fileObj} to bucket ${bucketName} on attempt ${i}: ${e.message}`);
     }
   }
 
