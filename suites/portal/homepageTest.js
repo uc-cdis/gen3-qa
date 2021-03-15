e56324a322d5bf7354de5ee56f2bc20eb4aafb5d
Jing Huang <71466688+jingh8@users.noreply.github.com>
2021-03-15 18:31:33 -0500

    test async homepagetest

--- suites/portal/homepageTest.js
+++ suites/portal/homepageTest.js
@@ -1,6 +1,6 @@
 Feature('Homepage').retry(2);
 
-Scenario('login @portal', ({ I, home }) => {
+Scenario('login @portal', async ({ I, home }) => {
   home.do.goToHomepage();
   home.complete.login();
   home.ask.seeDetails();
@@ -9,7 +9,7 @@ Scenario('login @portal', ({ I, home }) => {
 
 // To be merged with the above once all Commons move to portal version
 // with top bar login button
-Scenario('login @portal @topBarLogin', ({ home }) => {
+Scenario('login @portal @topBarLogin', async ({ home }) => {
   home.do.goToHomepage();
   home.complete.topBarLogin();
   home.ask.seeDetails();
