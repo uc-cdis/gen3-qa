46eb567413c66e37e511bd0fc6421c3c9eb7d36a
Jing Huang <71466688+jingh8@users.noreply.github.com>
2021-03-19 14:30:08 -0500

    fix travis error

--- services/portal/home/homeTasks.js
+++ services/portal/home/homeTasks.js
@@ -13,7 +13,7 @@ module.exports = {
     if (process.env.testedEnv.includes('covid19') || process.env.testedEnv.includes('pandemicresponsecommons') || process.env.testedEnv.includes('midrc')) {
       I.refreshPage();
     }
-    //wait 2 sec to let the page fully loaded
+    // wait 2 sec to let the page fully loaded
     I.wait(2);
     I.saveScreenshot('Home_page_for_debugging.png');
     portal.seeProp(homeProps.ready_cue, 60);
