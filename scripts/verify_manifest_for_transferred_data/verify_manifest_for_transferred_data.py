5dc560deaf2cbaa2fb40b2411f3713342c2dc8af
jingh8 <jingh8@uchicago.edu>
2021-06-03 10:10:59 -0500

    modify variables according to true manifest

--- scripts/verify_manifest_for_transferred_data/verify_manifest_for_transferred_data.py
+++ scripts/verify_manifest_for_transferred_data/verify_manifest_for_transferred_data.py
@@ -17,47 +17,50 @@ def verify_manifest(manifest_file, manifest_file_delimiter, datacommons):
     with open(manifest_file) as manifest:
         reader = csv.DictReader(manifest, delimiter=manifest_file_delimiter)
         for row in reader:
-            row['size'] = row.pop('file_size')
-            url = row['urls']
-            requesturl = 'https://{}/index/index?url={}'
-            r = requests.get(requesturl.format(datacommons, url))
+            row['size'] = row.pop('*file_size')
+            row['md5'] = row.pop('*md5sum')
+            file_name = row['*file_name']
+            requesturl = 'https://{}/index/index?file_name={}'
+            r = requests.get(requesturl.format(datacommons, file_name))
             manifest_to_verify = json.loads(r.text)
             for key in row:
                 try:
                     if key == 'md5':
                         item_to_verify = manifest_to_verify['records'][0]['hashes'][key]
-                    elif key == 'acl' or 'urls':
-                        # TODO : verify acl in future if needed
-                        continue
+                    elif key == 'size':
+                        item_to_verify = str(manifest_to_verify['records'][0][key])
+                        row[key] = row[key].replace(',',"")
                     else:
-                        item_to_verify = manifest_to_verify['records'][0][key]
+                        # TODO : verify other columns in future if needed
+                        continue
                 except KeyError:
                     errormsg = 'For {}, {} not found!\n'
-                    errorLog.write(errormsg.format(url,key))
-                    print(errormsg.format(url,key))
+                    errorLog.write(errormsg.format(file_name,key))
+                    print(errormsg.format(file_name,key))
                     continue
                 except IndexError:
                     # records is empty
                     errormsg = 'For {}, no record found!\n'
-                    errorLog.write(errormsg.format(url))
-                    print(errormsg.format(url))
+                    errorLog.write(errormsg.format(file_name))
+                    print(errormsg.format(file_name))
                     break
-
+                
                 if item_to_verify != row[key]:
-                    errormsg = 'For {}, the true value of {} is {}, but get {}\n'
-                    errorLog.write(errormsg.format(url,key,row[key],item_to_verify))
-                    print(errormsg.format(url,key,row[key],item_to_verify))
+                    errormsg = 'For file {}, the true value of {} is {}, but from the indexd record it is {}\n'
+                    errorLog.write(errormsg.format(file_name,key,row[key],item_to_verify))
+                    print(errormsg.format(file_name,key,row[key],item_to_verify))
                 else:
-                    print('{} is succefully copied'.format(url))
+                    print('{} is succefully copied'.format(file_name))
 
         errorLog.close()
 
 
 
 # TODO: Get these parameters from jenkins
-manifest_file = "example_manifest.tsv"
+# manifest_file = "example_manifest.tsv"
+manifest_file = "midrc_sample_image_manifest_v2.txt"
 manifest_file_delimiter = "\t"
-datacommons = 'qa-brain.planx-pla.net'
+datacommons = 'staging.midrc.org'
 
 verify_manifest(manifest_file, manifest_file_delimiter, datacommons) 
 
