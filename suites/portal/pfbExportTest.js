679e14f47412336d60b6e38d1a28623212d9d093
Marcelo Costa <themarcelor@gmail.com>
2021-07-16 11:01:05 -0500

    activate venv again before parsing pfb

--- suites/portal/pfbExportTest.js
+++ suites/portal/pfbExportTest.js
@@ -346,11 +346,11 @@ Scenario('Install the latest pypfb CLI version and make sure we can parse the av
   // It always downloads the 0.5.0 instead :/
   // that is because 0.5.0 has the follow py version criteria = Requires: Python >=3.6, <4.0
   // whereas 0.5.14 = Requires: Python >=3.6.1, <3.8
-  const pyPfbInstallationOutput = await bash.runCommand('python3 -m venv pfb_test && source pfb_test/bin/activate && pip install pypfb --user');
+  const pyPfbInstallationOutput = await bash.runCommand('python3 -m venv pfb_test && source pfb_test/bin/activate && pip install pypfb && ${process.env.WORKSPACE}/gen3-qa/pfb_test/bin/pfb');
   console.log(`${new Date()}: pyPfbInstallationOutput = ${pyPfbInstallationOutput}`);
 
   // await bash.runCommand(`cat ./test_export_${I.cache.UNIQUE_NUM}.avro`);
-  const pfbParsingResult = await bash.runCommand(`${process.env.WORKSPACE}/gen3-qa/pfb_test/bin/pfb show -i ./test_export_${I.cache.UNIQUE_NUM}.avro | jq .`);
+  const pfbParsingResult = await bash.runCommand(`source pfb_test/bin/activate && ${process.env.WORKSPACE}/gen3-qa/pfb_test/bin/pfb show -i ./test_export_${I.cache.UNIQUE_NUM}.avro | jq .`);
   // console.log(`${new Date()}: pfbParsingResult = ${pfbParsingResult}`);
   const pfbConvertedToJSON = JSON.parse(`[${pfbParsingResult.replace(/\}\{/g, '},{')}]`);
   // console.log(`${new Date()}: pfbConvertedToJSON = ${JSON.stringify(pfbConvertedToJSON)}`);
