22cb54f7a35fadd1caca69054e1ef2e9ee3ba776
Atharva Rane <atharvar@uchicago.edu>
2021-07-20 13:55:15 -0500

    fix1

--- load-testing/audit-service/sendLoginMessages.sh
+++ load-testing/audit-service/sendLoginMessages.sh
@@ -27,5 +27,5 @@ message='
 # send 100 dummy logs to audit-service sqs
 for i in {1..100};
     do
-        aws sqs send-message --queue-url "$awsURL/$sqsURL" --message-body "$message"
+        aws sqs send-message --queue-url "$awsURL$sqsURL" --message-body "$message"
     done
