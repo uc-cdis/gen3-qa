64b93884cf6f61422e5eae4b1be0e4652269277c
John McCann <johnmccann@uchicago.edu>
2021-08-09 13:55:17 -0700

    chore(homepageTest): save screenshot after login

--- suites/portal/homepageTest.js
+++ suites/portal/homepageTest.js
@@ -1,10 +1,13 @@
 const fetch = require('node-fetch');
 
+const I = actor();
+
 Feature('Homepage').retry(2);
 
 Scenario('login @portal', async ({ home }) => {
   home.do.goToHomepage();
   home.complete.login();
+  I.saveScreenshot('Home_page_after_login_for_debugging.png');
   home.ask.seeDetails();
   home.complete.logout();
 });
