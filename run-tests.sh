c876e6b69e3cab5c23da92d1d6a75cc216087e76
Marcelo Costa <themarcelor@gmail.com>
2021-08-10 16:59:59 -0500

    tweak java_home, path and create selenium.log

--- run-tests.sh
+++ run-tests.sh
@@ -497,16 +497,21 @@ if [[ "$testedEnv" == "ci-env-1.planx-pla.net" ]]; then
 fi
 export testedEnv="$testedEnv"
 
+
+#### Gen3 QA in a BOX ############################################################################
+
 # Tweak JAVA_HOME to use jdk11
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 export PATH="$JAVA_HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 # Start selenium process within the ephemeral jenkins pod.
 npx selenium-standalone install --version=4.0.0-alpha-7
-timeout $seleniumTimeout npx selenium-standalone start --version=4.0.0-alpha-7 &
+timeout $seleniumTimeout npx selenium-standalone start --version=4.0.0-alpha-7 &> selenium.log &
 
 # gen3-qa-in-a-box requires a couple of changes to its webdriver config
 mv gen3.qa.in.a.box.codecept.conf.js codecept.conf.js
+
+##################################################################################################
 
 if [ "$selectedTest" == "all" ]; then
     # no interactive tests
