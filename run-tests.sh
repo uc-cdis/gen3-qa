5d56c6e4ef906dfce03ebbddf771db4b01842255
Marcelo Costa <themarcelor@gmail.com>
2021-08-11 21:58:20 -0500

    set correct selenium-standalone localhost endpoint for codeceptjs hooks script that fetches metrics

--- run-tests.sh
+++ run-tests.sh
@@ -510,6 +510,9 @@ if [[ "$(hostname)" == *"cdis-github-org"* ]]; then
   set +e
   mv gen3.qa.in.a.box.codecept.conf.js codecept.conf.js
   set -e
+
+  # selenium-standalone is in localhost, do not fetch metrics from the selenium-hub
+  sed -i 's/selenium-hub/localhost/' hooks/test_results.js
 else
   echo "NOT inside an ephemeral gen3-qa-in-a-box pod..."
 fi
