98af3c10e0168f980b7f7230848861fabc11f801
Atharva Rane <atharvar@uchicago.edu>
2021-08-16 10:58:34 -0400

    adding comments

--- suites/portal/studyViewerTest.js
+++ suites/portal/studyViewerTest.js
@@ -1,3 +1,21 @@
+
+/*
+Study Viewer integration tests 
+This test has pre-requisities:
+1. studyViewerConfig to <envs>/portal/gitops.json
+2. studyViewer link under the `button` section
+        {
+          "icon": "query",
+          "link": "/study-viewer/clinical_trials",
+          "color": "#a2a2a2",
+          "name": "Study Viewer"
+        }
+3. set useArboristUI to true
+4. add `jenkins_cmc_alias` inside manifest.json guppyConfig block
+5. in manifest.json global block, add "tier_access_level": "libre"
+6. add requestor service to the manifest.json block
+*/
+
 Feature('Study Viewer');
 
 const { expect } = require('chai');
@@ -18,12 +36,13 @@ BeforeSuite(async ({ I, users }) => {
   // polling logic to capture new indicess
   const nAttempts = 24;
   let guppyStatusCheckResp = '';
-  for (let i = 0; i < nAttempts; i += 1) {
+  for (let i = 0; i < nAttempts; i+=1 ){
     console.log(`waiting for the new guppy pod with the expected jenkins cmc permanent index - Attempt #${i}...`);
     guppyStatusCheckResp = await I.sendGetRequest(
       `https://${process.env.NAMESPACE}.planx-pla.net/guppy/_status`,
       users.mainAcct.accessTokenHeader,
     );
+    // `jenkins_cmc_permanent_alias` is always pointing to `jenkins_cmc_alias_213`
     if (guppyStatusCheckResp.status === 200
       && (Object.prototype.hasOwnProperty.call(guppyStatusCheckResp.data.indices, 'jenkins_cmc_alias_213'))) {
       console.log(`${new Date()}: all good, proceed with the test...`);
@@ -39,9 +58,8 @@ BeforeSuite(async ({ I, users }) => {
       }
     }
   }
-  // wait a bit for any old pod replicas to go away
+  // wait for guppy pods to be up which points towards jenkins_cmc_permanent index
   await sleepMS(30000);
-  // Other arborist policy revoking stuff
 });
 
 // revokes your arborist access
@@ -65,8 +83,8 @@ AfterSuite(async () => {
 });
 
 // User does not log in and has no access. User see 'Login to Request access' button
-Scenario('User doesnot login and requests the access @studyViewer', async ({
-  I, users, login,
+Scenario('User doesnot login and requests the access @studyViewer', async ({ 
+  I, users, login 
 }) => {
   studyViewerTasks.goToStudyViewerPage();
   await studyViewerTasks.learnMoreButton();
@@ -134,8 +152,8 @@ Scenario('User has access to download @studyViewer', async ({
 });
 
 // checking the details of the dataset
-Scenario('Navigation to the detailed dataset page @studyViewer', async ({
-  home, users, login,
+Scenario('Navigation to the detailed dataset page @studyViewer', async ({ 
+  home, users, login 
 }) => {
   home.do.goToHomepage();
   login.complete.login(users.user0);
