db545b5870c237ac352b71a5d76a3bb8946965d4
haraprasadj <haraprasadj@uchicago.edu>
2021-08-25 21:08:37 -0500

    fix run logic for agg mds

--- run-tests.sh
+++ run-tests.sh
@@ -474,9 +474,10 @@ runTestsIfServiceVersion "@audit" "fence" "5.1.0" "2021.07"
 
 #
 # Run Agg MDS tests only if the feature is enabled
+# and service is one of metadata-service, cdis-manifest, gitops-qa, gitops-dev and gen3-qa
 #
 usingAggMDS=$(g3kubectl get cm manifest-metadata -o yaml | yq .data.USE_AGG_MDS)
-if ! [[ $usingAggMDS == \"true\" || "$service" =~ ^(cdis-manifest|gitops-qa|gitops-dev|gen3-qa|metadata-service) ]]; then
+if ! [[ $usingAggMDS == \"true\" && "$service" =~ ^(cdis-manifest|gitops-qa|gitops-dev|gen3-qa|metadata-service) ]]; then
 	donot '@aggMDS'
 fi
 
