400d22256b8054d94a8e98d148b65e171be83147
haraprasadj <haraprasadj@uchicago.edu>
2021-10-31 19:29:12 -0500

    fix

--- services/portal/exportToWorkspace/exportToWorkspaceSequences.js
+++ services/portal/exportToWorkspace/exportToWorkspaceSequences.js
@@ -28,9 +28,9 @@ module.exports = {
   },
 
   /* The 'Click Workspace tab when logged out and logged in' test sequence */
-  checkClickWorkspaceTabWithLogoutAndLogin() {
+  async checkClickWorkspaceTabWithLogoutAndLogin() {
     // exportToWorkspaceTasks.logoutAndGetToWorkspace(home);
-    // exportToWorkspaceTasks.loginAndGetToWorkspace(home);
+    // await exportToWorkspaceTasks.loginAndGetToWorkspace(home);
   },
 
   /* The 'Check export to workspace success message toaster' test sequence */
