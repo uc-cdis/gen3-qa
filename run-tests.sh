67233c10b0f10b91df1dd7f4bb6f971001239958
haraprasadj <haraprasadj@uchicago.edu>
2021-12-16 20:48:56 -0800

    test

--- run-tests.sh
+++ run-tests.sh
@@ -552,10 +552,9 @@ if [[ "$(hostname)" == *"cdis-github-org"* ]] || [[ "$(hostname)" == *"planx-ci-
   echo "inside an ephemeral gen3-qa-in-a-box pod..."
   
   # Start selenium process within the ephemeral jenkins pod.
-  npx selenium-standalone install --version=4.1.0 --drivers.chrome.version=96.0.4664.45 --drivers.chrome.baseURL=https://chromedriver.storage.googleapis.com
-  timeout $seleniumTimeout npx selenium-standalone start --singleDriverStart=chrome --version=4.1.0 --drivers.chrome.version=96.0.4664.45 &> selenium.log &
+  npx selenium-standalone install --drivers.chrome.version=96.0.4664.45
+  timeout $seleniumTimeout npx selenium-standalone start --drivers.chrome.version=96.0.4664.45 &> selenium.log &
  
-
   # gen3-qa-in-a-box requires a couple of changes to its webdriver config
   set +e
   mv gen3.qa.in.a.box.codecept.conf.js codecept.conf.js
@@ -565,6 +564,8 @@ else
   echo "NOT inside an ephemeral gen3-qa-in-a-box pod..."
 fi
 ##################################################################################################
+
+curl http://localhost:4444/status
 
 if [ "$selectedTest" == "all" ]; then
     # no interactive tests
