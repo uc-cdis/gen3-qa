61d13b0dd9d42ddadca0b6c686fdf05f0b00b0c3
Sai Shanmukha <nss10@outlook.com>
2021-12-21 15:57:57 -0600

    Merge access and revoke request scenarios

--- suites/apis/requestorTest.js
+++ suites/apis/requestorTest.js
@@ -16,66 +16,69 @@ Feature('RequestorAPI');
 const { expect } = require('chai');
 const requestorTasks = require('../../services/apis/requestor/requestorTasks.js');
 
-Before(async ({
-  home, users, login,
+// Create an access request for a policy that doesn’t exist in Arborist => 400 error
+Scenario('User requests access for a policy that does not exist in Arborist @requestor', async ({
+  home, login, users,
 }) => {
   home.do.goToHomepage();
   login.complete.login(users.user0);
-});
-
-// Create an access request for a policy that doesn’t exist in Arborist => 400 error
-Scenario('User requests access for a policy that does not exist in Arborist @requestor', async ({
-  users,
-}) => {
   const response = await requestorTasks.createRequestFromPolicyID(users.user0, 'random-policy');
   expect(response).to.have.property('status_code', 400);
 });
 
-Scenario('User does not have access request for a policy and requests for access. @requestor', async ({
-  users, fence,
+Scenario('User does not have access request for a policy and requests for access followed by sending a Revoke request @requestor', async ({
+  home, login, users, fence,
 }) => {
-  // Check that the user does not have access to policy 'programs.jnkns-read-storage'.
+  home.do.goToHomepage();
+  login.complete.login(users.user0);
+
+  // Check that the user does not have access to policy 'requestor_integration_test'.
   let userInfo = await fence.do.getUserInfo(users.user0.accessToken);
-  expect(userInfo.data.authz).to.have.property('/programs/jnkns');
-  expect(userInfo.data.authz['/programs/jnkns']).to.deep.to.not.include({ method: 'read-storage', service: '*' });
+  expect(userInfo.data.authz).to.have.property('/requestor-integration-test');
+  expect(userInfo.data.authz['/requestor-integration-test']).to.deep.to.not.include({ method: 'read', service: 'integration' });
 
   // Create a request for a policy that is present in Arborist
-  const response = await requestorTasks.createRequestFromPolicyID(users.user0, 'programs.jnkns-read-storage');
-  expect(response).to.have.property('status_code', 201);
+  const createResponse = await requestorTasks.createRequestFromPolicyID(users.user0, 'requestor_integration_test');
+  expect(createResponse).to.have.property('status_code', 201);
 
   // Check if the access is created -- It shouldn't since it is not signed yet
   userInfo = await fence.do.getUserInfo(users.user0.accessToken);
-  expect(userInfo.data.authz['/programs/jnkns']).to.deep.to.not.include({ method: 'read-storage', service: '*' });
+  expect(userInfo.data.authz['/requestor-integration-test']).to.deep.to.not.include({ method: 'read', service: 'integration' });
 
   // Create a PUT request to sign the previous request using an authenticated user
-  await requestorTasks.signedRequest(response.request_id);
+  await requestorTasks.signedRequest(createResponse.request_id);
 
   // Verify if access is given to the user -- There should be
   userInfo = await fence.do.getUserInfo(users.user0.accessToken);
-  expect(userInfo.data.authz['/programs/jnkns']).to.deep.to.include({ method: 'read-storage', service: '*' });
-});
-
-Scenario('User has access to a policy and tries to revoke access. @requestor', async ({
-  users, fence,
-}) => {
-  // Check that the user has access to policy 'programs.jnkns-read-storage'.
-  let userInfo = await fence.do.getUserInfo(users.user0.accessToken);
-  expect(userInfo.data.authz).to.have.property('/programs/jnkns');
-  expect(userInfo.data.authz['/programs/jnkns']).to.deep.to.include({ method: 'read-storage', service: '*' });
+  expect(userInfo.data.authz['/requestor-integration-test']).to.deep.to.include({ method: 'read', service: 'integration' });
 
   // Create a request to 'revoke' a policy that is present in Arborist
-  const response = await requestorTasks.createRequestFromPolicyID(users.user0, 'programs.jnkns-read-storage', true);
+  const revokeResponse = await requestorTasks.createRequestFromPolicyID(users.user0, 'requestor_integration_test', true);
 
   // Check if the access is revoked -- It shouldn't since it is not signed yet
   userInfo = await fence.do.getUserInfo(users.user0.accessToken);
-  expect(userInfo.data.authz).to.have.property('/programs/jnkns');
-  expect(userInfo.data.authz['/programs/jnkns']).to.deep.to.include({ method: 'read-storage', service: '*' });
+  expect(userInfo.data.authz['/requestor-integration-test']).to.deep.to.include({ method: 'read', service: 'integration' });
 
   // Create a PUT request to sign the previous request using an authenticated user
-  await requestorTasks.signedRequest(response.request_id);
+  await requestorTasks.signedRequest(revokeResponse.request_id);
 
-  // Verify if access is given to the user -- There should be
+  // Verify if access is revoked from the user
   userInfo = await fence.do.getUserInfo(users.user0.accessToken);
-  expect(userInfo.data.authz).to.have.property('/programs/jnkns');
-  expect(userInfo.data.authz['/programs/jnkns']).to.deep.to.not.include({ method: 'read-storage', service: '*' });
+  expect(userInfo.data.authz['/requestor-integration-test']).to.deep.to.not.include({ method: 'read', service: 'integration' });
+});
+
+// Create a revoke request for a policy that exists in Arborist
+// but user does not have access to=> 400 error
+Scenario('User sends a revoke request to a property he does not have access to @requestor', async ({
+  home, login, users, fence,
+}) => {
+  home.do.goToHomepage();
+  login.complete.login(users.user1);
+  // Check that the user does not have access to policy 'requestor_integration_test'.
+  const userInfo = await fence.do.getUserInfo(users.user1.accessToken);
+  expect(userInfo.data.authz['/requestor-integration-test']).to.deep.to.not.include({ method: 'read', service: 'integration' });
+
+  // Create a request to 'revoke' a policy to which user does not have access to
+  const revokeResponse = await requestorTasks.createRequestFromPolicyID(users.user1, 'requestor_integration_test', true);
+  expect(revokeResponse).to.have.property('status_code', 400);
 });
