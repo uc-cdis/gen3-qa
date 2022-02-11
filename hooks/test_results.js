1c62a532b87a973ce4428982761eea3099d66227
haraprasadj <haraprasadj@uchicago.edu>
2022-02-11 10:53:22 -0800

    testing datadog connection

--- hooks/test_results.js
+++ hooks/test_results.js
@@ -18,7 +18,7 @@ let ddClient;
 if (process.env.JENKINS_HOME && process.env.RUNNING_LOCAL !== 'true') {
   console.log('### ## Initializing DataDog Client...');
   ddClient = new StatsD({
-    host: 'datadog-cluster-agent.datadog',
+    host: 'datadog-cluster-agent',
     port: 8125,
     globalTags: { env: testEnvironment },
     errorHandler(error) {
