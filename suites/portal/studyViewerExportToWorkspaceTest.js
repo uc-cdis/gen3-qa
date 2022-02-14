44944c2c898ad6fb24fce097145ef0e4ba6cabc0
Pauline <4224001+paulineribeyre@users.noreply.github.com>
2022-02-14 11:59:11 -0600

    clarify user; fewer scenarios

--- suites/portal/studyViewerExportToWorkspaceTest.js
+++ suites/portal/studyViewerExportToWorkspaceTest.js
@@ -2,50 +2,26 @@
 Pre-requisites for executing the tests:
  1. Latest code of data-portal, pelican, manifestservice, gen3-fuse
  2. Users
-    User1 has access to workspace
+    dummy-one@planx-pla.net has access to workspace
+    dummy-one@planx-pla.net has access to a study (NIAID-ACTT in QA-NCT)
 */
-Feature('Export To Workspace From Study Viewer @requires-portal');
+Feature('Export to Workspace from Study Viewer @requires-portal');
 
 const { expect } = require('chai');
 const { interactive, ifInteractive } = require('../../utils/interactive.js');
 
-Scenario('User1 can export study to workspace', ifInteractive(
+Scenario('User can export study to workspace', ifInteractive(
   async () => {
     const result = await interactive(`
-            1. Navigate to '/study-viewer' page
-            2. Click 'Show details' under a study
-            3. Verify that the 'Export to Workspace' button is enabled
-            4. Click on 'Export to Workspace'
-            5. Verify that a confirmation message is shown at the bottom of the page with a button to redirect to workspace
-            6. Click on 'Go to Workspace' button
-            7. The '/workspace' page is loaded
-       `);
-    expect(result.didPass, result.details).to.be.true;
-  },
-)).tag('@manual');
-
-Scenario('On initiating export to workspace, a pelican-export job is initiated', ifInteractive(
-  async () => {
-    const result = await interactive(`
-            1. Navigate to '/study-viewer' page
-            2. Click 'Show details' under a study
-            3. Verify that the 'Export to Workspace' button is enabled
-            4. Click on 'Export to Workspace'
-            5. Verify that kubernetes job pelican-export is triggered
-       `);
-    expect(result.didPass, result.details).to.be.true;
-  },
-)).tag('@manual');
-
-Scenario('On initiating export to workspace, a hatchery pod is created', ifInteractive(
-  async () => {
-    const result = await interactive(`
-            1. Navigate to '/study-viewer' page
-            2. Click 'Show details' under a study
-            3. Verify that the 'Export to Workspace' button is enabled
-            4. Click on 'Export to Workspace'
-            5. Verify that kubernetes pod hatchery is created
-       `);
+      1. Login as dummy-one@planx-pla.net
+      2. Navigate to the study viewer page ('/study-viewer/clinical_trials' in QA-NCT)
+      3. Click 'Show details' under a study you have access to (ACTT1 in QA-NCT)
+      4. Verify that the 'Export to Workspace' button is enabled
+      5. Click on 'Export to Workspace'
+      6. Verify that a confirmation message is shown at the bottom of the page; wait to see a 'Go to Workspace' button
+      7. Click on 'Go to Workspace' button
+      8. Verify that you are redirected to the '/workspace' page
+    `);
     expect(result.didPass, result.details).to.be.true;
   },
 )).tag('@manual');
@@ -53,16 +29,13 @@ Scenario('On initiating export to workspace, a hatchery pod is created', ifInter
 Scenario('The exported study is available in the jupyter notebook', ifInteractive(
   async () => {
     const result = await interactive(`
-            1. Navigate to '/study-viewer' page
-            2. Click 'Show details' under a study
-            3. Verify that the 'Export to Workspace' button is enabled
-            4. Click on 'Export to Workspace'
-            5. Verify that a confirmation message is shown at the bottom of the page with a button to redirect to workspace
-            6. Click on 'Go to Workspace' button
-            7. The '/workspace' page is loaded
-            8. Launch a workspace
-            9. Navigate to pd/data/ and verify that a new PFB and mount are present, labeled cohort-<GUID>.avro and manifest-<GUID>
-       `);
+      Follow-up to previous test:
+      1. On the workspace page, launch a workspace; wait for it to load
+      2. Navigate to the 'pd/data/<hostname>' directory
+      3. Verify that there is a PFB file at 'cohort-<GUID>.avro'
+      4. Verify that there is a file at 'manifest-<GUID>/by-guid/<GUID>'
+      5. Verify that you can open the file at 'manifest-<GUID>/by-guid/<GUID>'
+    `);
     expect(result.didPass, result.details).to.be.true;
   },
 )).tag('@manual');
