3c531b559c877fd6ff36e8ea0a0aa22041d451a3
John McCann <johnmccann@uchicago.edu>
2022-03-31 22:32:34 -0500

    chore(etlTest.js): cleanUpIndices in AfterSuite

--- suites/apis/etlTest.js
+++ suites/apis/etlTest.js
@@ -6,24 +6,12 @@ const { Bash } = require('../../utils/bash.js');
 const { checkPod } = require('../../utils/apiUtil.js');
 
 const bash = new Bash();
-const aliases = [];
 
 BeforeSuite(async ({ etl }) => {
-  const etlMappingNames = bash.runCommand('g3kubectl get cm etl-mapping -o jsonpath=\'{.data.etlMapping\\.yaml}\' | yq \'.mappings[].name\' | xargs').split(' ');
-
-  etlMappingNames.forEach((etlMappingName) => {
-    aliases.push(etlMappingName, `${etlMappingName}-array-config`);
-  });
-
-  aliases.forEach((alias) => {
-    if (etl.do.existAlias(alias)) {
-      console.warn(`WARNING: alias ${alias} exists prior to running ETL tests`);
-      const index = etl.do.getIndexFromAlias(alias);
-      console.log(`deleting all index versions associated with alias ${alias}`);
-      etl.do.deleteAllIndexVersions(index);
-      expect(etl.do.existAlias(alias), 'Fails to delete alias').to.equal(false);
-    }
-  });
+  // this shouldn't normally be necessary, but just in case any indices didn't
+  // get properly cleaned up in a prior run (e.g. es proxy pod or pod running
+  // integration tests crashed)
+  etl.do.cleanUpIndices();
 });
 
 Scenario('run ETL first time @etl', async ({ I }) => {
@@ -33,23 +21,27 @@ Scenario('run ETL first time @etl', async ({ I }) => {
   console.log(`${new Date()}: After run ETL first time`);
 });
 
-Scenario('run ETL second time @etl', async ({ I, sheepdog }) => {
+Scenario('run ETL second time @etl', async ({ I, sheepdog, etl }) => {
   console.log(`${new Date()}: Before run ETL second time`);
   await sheepdog.do.runGenTestData(1);
   await bash.runJob('etl', '', false);
   await checkPod(I, 'etl', 'gen3job,job-name=etl', { nAttempts: 80, ignoreFailure: false, keepSessionAlive: false });
   console.log(`${new Date()}: After run ETL second time`);
+
+  const etlMappingNames = bash.runCommand('g3kubectl get cm etl-mapping -o jsonpath=\'{.data.etlMapping\\.yaml}\' | yq \'.mappings[].name\' | xargs').split(' ');
+  const aliases = [];
+  etlMappingNames.forEach((etlMappingName) => {
+    aliases.push(etlMappingName, `${etlMappingName}-array-config`);
+  });
+  aliases.forEach((alias) => {
+    if (etl.do.existAlias(alias)) {
+      const index = etl.do.getIndexFromAlias(alias);
+      etl.ask.hasVersionIncreased(index, 0);
+    }
+  });
 });
 
 AfterSuite(async ({ etl }) => {
   console.log('cleaning up indices created by ETL runs');
-  aliases.forEach((alias) => {
-    if (etl.do.existAlias(alias)) {
-      console.log(`deleting all index versions associated with alias ${alias}`);
-      const index = etl.do.getIndexFromAlias(alias);
-      etl.ask.hasVersionIncreased(index, 0);
-      etl.do.deleteAllIndexVersions(index);
-      expect(etl.do.existAlias(alias), 'Fails to delete alias').to.equal(false);
-    }
-  });
+  etl.do.cleanUpIndices();
 });
