045d0df33115b46041762d17d2ef5041aeb72200
Atharva Rane <atharvar@uchicago.edu>
2022-04-07 11:20:47 -0400

    fixing lint issues

--- suites/apis/rasDRSTest.js
+++ suites/apis/rasDRSTest.js
@@ -4,8 +4,8 @@ Feature('DRS RAS');
 const { expect } = require('chai');
 // const { URL } = require('url');
 // const { Bash } = require('../../utils/bash');
+const queryString = require('query-string');
 const { sleepMS } = require('../../utils/apiUtil.js');
-const queryString  = require('query-string');
 
 // const bash = new Bash();
 
@@ -15,28 +15,29 @@ const TARGET_ENVIRONMENT = `${process.env.NAMESPACE}.planx-pla.net`;
 const ga4ghURL = `${TARGET_ENVIRONMENT}/ga4gh/drs/v1/objects/`;
 // Ras Server URL
 const rasServerURL = 'stsstg.nih.gov';
-const scope = 'openid profile email ga4gh_passport_v1'
+const scope = 'openid profile email ga4gh_passport_v1';
 // hardcoded for testing purposes
 // TODO - make the test more generic bases on the access
 // upload a dummy indexd record with correct authz resource path
 const authZ = '/programs/phs000298.c1';
 
+// TODO working on the post a indexd record before the suite
 const indexdFile = {
-    fileToUpload: {
-        "acl": [],
-        "authz": [ '/programs/phs000298.c1' ],
-        "filename": 'ras_test_file',
-        "hashes": { md5: '587efb5d96f695710a8df9c0dbb96eb0' }, // pragma: allowlist secret
-        "size": 15,
-        "form": null,
-        "urls": [ 's3://cdis-presigned-url-test/testdata', 'gs://cdis-presigned-url-test/testdata'],
-    },
+  fileToUpload: {
+    acl: [],
+    authz: ['/programs/phs000298.c1'],
+    filename: 'ras_test_file',
+    hashes: { md5: '587efb5d96f695710a8df9c0dbb96eb0' }, // pragma: allowlist secret
+    size: 15,
+    form: null,
+    urls: ['s3://cdis-presigned-url-test/testdata', 'gs://cdis-presigned-url-test/testdata'],
+  },
 };
 
 BeforeSuite(async ({ I }) => {
   console.log('Setting up .. ');
   I.cache = {};
-  console.log("In BeforeSuite ....");
+  console.log('In BeforeSuite ....');
 
   // check if the RAS user and password is defined as the env variable or in Jenkins creds
   if (process.env.RAS_TEST_USER_1_USERNAME === undefined) {
@@ -45,7 +46,7 @@ BeforeSuite(async ({ I }) => {
     throw new Error('ERROR: There is no password defined for RAS Test User 1. Please declare the "RAS_TEST_USER_1_PASSWORD" environment variable and try again. Aborting test...');
   }
 
-  // TODO : post new indexdFile before 
+  // TODO : post new indexdFile before
 
   // if the authz resource path is hardcoded, you run this to fetch the did of file with hardcoded authz path
   // get indexd record for presignedurl request
@@ -94,14 +95,14 @@ Scenario('Obtain passport for RAS User', async ({ I }) => {
   // POST /auth/oauth/v2/token to get RAS token
   const rasAuthURL = `https://${rasServerURL}/auth/oauth/v2/token`;
   const data = queryString.stringify({
-    "grant_type": "authorization_code",
-    "code": `${I.cache.rasAuthCode}`,
-    "client_id": `${I.cache.clientID}`,
-    "client_secret": `${I.cache.clientSecret}`,
-    "scope": `${scope}`,
-    "redirect_uri": "https://qa-dcp.planx-pla.net/user/login/ras/callback"
+    grant_type: 'authorization_code',
+    code: `${I.cache.rasAuthCode}`,
+    client_id: `${I.cache.clientID}`,
+    client_secret: `${I.cache.clientSecret}`,
+    scope: `${scope}`,
+    redirect_uri: 'https://qa-dcp.planx-pla.net/user/login/ras/callback',
   });
-     
+
   const getRASToken = await I.sendPostRequest(
     rasAuthURL,
     data,
@@ -131,7 +132,7 @@ Scenario('Send DRS request - Single Passport Single VISA', async ({ I }) => {
   const drsAccessReq = await I.sendPostRequest(
     `https://${ga4ghURL}/${I.cache.indexdRecord}/access/s3`,
     {
-      "passports": [`${I.cache.passportBody}`]
+      passports: [`${I.cache.passportBody}`],
     },
     {
       Accept: 'application/json',
@@ -150,19 +151,19 @@ Scenario('Send DRS request - Single Passport Single VISA', async ({ I }) => {
   expect(preSignedURLReq).to.not.be.empty;
 });
 
-Scenario('Get Access Token from Refresh Token', async({ I }) => {
+Scenario('Get Access Token from Refresh Token', async ({ I }) => {
   // get new access token from the refresh token
   // and make /userinfo call with the new access tokens
   const rasAuthURL = `https://${rasServerURL}/auth/oauth/v2/token`;
   const refreshData = queryString.stringify({
-    "grant_type": "refresh_token",
-    "refresh_token": `${I.cache.refresh_token}`,
-    "scope": `${scope}`,
-    "client_id": `${I.cache.clientID}`,
-    "client_secret": `${I.cache.clientSecret}`,
+    grant_type: 'refresh_token',
+    refresh_token: `${I.cache.refresh_token}`,
+    scope: `${scope}`,
+    client_id: `${I.cache.clientID}`,
+    client_secret: `${I.cache.clientSecret}`,
   });
 
-  const tokenFromRefresh =  await I.sendPostRequest(
+  const tokenFromRefresh = await I.sendPostRequest(
     rasAuthURL,
     refreshData,
     {
@@ -171,9 +172,8 @@ Scenario('Get Access Token from Refresh Token', async({ I }) => {
   );
   console.log(`${JSON.stringify(tokenFromRefresh.data.access_token)}`);
   // and send subsequent /userinfo call and also a presigned url call with the passport
-
 });
 
 // Scenario('Consent Management', async ({ I }) => {
-    // TODO
+// TODO
 // });
