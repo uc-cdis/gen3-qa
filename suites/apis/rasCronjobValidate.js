d11a3410c52f71cb242450bffc6fcdf23edcf17b
Atharva Rane <atharvar@uchicago.edu>
2022-04-21 17:24:45 -0400

    adding more steps to the tests

--- suites/apis/rasCronjobValidate.js
+++ suites/apis/rasCronjobValidate.js
@@ -8,11 +8,16 @@ const TARGET_ENVIRONMENT = process.env.GEN3_COMMONS_HOSTNAME;
 Scenario('Validate fence-visa-update job @manual', ifInteractive(
   async () => {
     const result = await interactive(`
-            1. Get a new passport from /userinfo endpoint
-            2. Log into ${TARGET_ENVIRONMENT} with RAS Login (RAs user and password)
-            3. run `gen3 job run fence-visa-update`
-            4. run `gen3 psql fence`
-            5. check ga4gh_visa_v1 table for the new entry
+            1. Get a new passport from RAS /userinfo endpoint
+            2. Log into ${TARGET_ENVIRONMENT} with RAS Login (RAS user and password)
+            3. Check ga4gh_visa_v1 table by command 'gen3 psql fence'
+                - number of visas for the user
+                - check the JTI and expiration on the visa
+            4. Run 'gen3 job run fence-visa-update'
+            5. Run 'gen3 psql fence'
+            6. Again check ga4gh_visa_v1 table for the new entry
+                - number of visa(s) for the user (the number should be more)
+                - the JTI(s) and expiration(s) on the new visa(s) should be different 
         `);
     expect(result.didPass, result.details).to.be.true;
   },
