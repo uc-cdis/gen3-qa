654d3dd2bba2316d932ab2da738a571cb249996b
Atharva Rane <atharvar@uchicago.edu>
2022-04-26 16:28:28 -0400

    update the SQL query in test

--- suites/apis/rasCronjobValidate.js
+++ suites/apis/rasCronjobValidate.js
@@ -13,11 +13,13 @@ Scenario('Validate fence-visa-update job @manual', ifInteractive(
             3. Check ga4gh_visa_v1 table by command 'gen3 psql fence'
                 - number of visas for the user
                 - check the JTI and expiration on the visa
+            (SQL QUERY - 'SELECT user_id, ga4gh_visa, expires FROM ga4gh_visa_v1 WHERE user_id=<RAS user id> ORDER BY expires DESC;' )
             4. Run 'gen3 job run fence-visa-update'
             5. Run 'gen3 psql fence'
             6. Again check ga4gh_visa_v1 table for the new entry
                 - number of visa(s) for the user (the number should be more)
-                - the JTI(s) and expiration(s) on the new visa(s) should be different 
+                - the JTI(s) and expiration(s) on the new visa(s) should be different
+            (SQL QUERY - 'SELECT user_id, ga4gh_visa, expires FROM ga4gh_visa_v1 WHERE user_id=<RAS user id> ORDER BY expires DESC;' ) 
         `);
     expect(result.didPass, result.details).to.be.true;
   },
