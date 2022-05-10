c4223d8461e0697ea76e2437fcb45978ef456ce6
jingh8 <jingh8@uchicago.edu>
2022-05-10 09:59:55 -0500

    refactor gwasquestion

--- services/portal/GWAS/GWASQuestions.js
+++ services/portal/GWAS/GWASQuestions.js
@@ -2,60 +2,37 @@
 const I = actor();
 
 const GWASTasks = require('./GWASTasks.js');
-const GWASProps = require('./GWASProps.js');
+
+async function checkElement(element) {
+  let refreshed = 0; // keep track of how many refreshes
+  while (refreshed < 3) {
+    if (refreshed > 0) {
+      I.refreshPage();
+      await GWASTasks.CheckJobStatus();
+    }
+    let InProgressSpan = await tryTo(() => I.waitForElement(element, 2)); // eslint-disable-line
+    if (!InProgressSpan) {
+      refreshed += 1;
+    } else {
+      break;
+    }
+  }
+  I.seeElement(element);
+}
 
 module.exports = {
   async isJobStart(jobId) {
-    let refreshed = 0; // keep track of how many refreshes
     const InProgressxpath = `//li[.//h4[contains(text(),"${jobId}")]]//span[text()="In Progress"]`;
-    while (refreshed < 3) {
-      if (refreshed > 0) {
-        I.refreshPage();
-        await GWASTasks.CheckJobStatus();
-      }
-      let InProgressSpan = await tryTo(() => I.waitForElement(InProgressxpath, 2)); // eslint-disable-line
-      if (!InProgressSpan) {
-        refreshed += 1;
-      } else {
-        break;
-      }
-    }
-    I.seeElement(InProgressxpath);
+    await checkElement(InProgressxpath);
   },
 
   async isJobComplete(jobId) {
-    let refreshed = 0; // keep track of how many refreshes
     const Completexpath = `//li[.//h4[contains(text(),"${jobId}")]]//span[text()="Completed"]`;
-    while (refreshed < 3) {
-      if (refreshed > 0) {
-        I.refreshPage();
-        await GWASTasks.CheckJobStatus();
-      }
-      let completeSpan = await tryTo(() => I.waitForElement(Completexpath, 2)); // eslint-disable-line
-      if (!completeSpan) {
-        refreshed += 1;
-      } else {
-        break;
-      }
-    }
-    I.seeElement(GWASProps.Completexpath);
+    await checkElement(Completexpath);
   },
 
   async isJobCancel(jobId) {
-    let refreshed = 0; // keep track of how many refreshes
     const Cancelxpath = `//li[.//h4[contains(text(),"${jobId}")]]//span[text()="Cancelled"]`;
-    while (refreshed < 3) {
-      if (refreshed > 0) {
-        I.refreshPage();
-        await GWASTasks.CheckJobStatus();
-      }
-      let completeSpan = await tryTo(() => I.waitForElement(Cancelxpath, 2)); // eslint-disable-line
-      if (!completeSpan) {
-        refreshed += 1;
-      } else {
-        break;
-      }
-    }
-    I.seeElement(GWASProps.Cancelxpath);
+    await checkElement(Cancelxpath);
   },
 };
