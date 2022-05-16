946f0f7a78915989cb3b901ee983deab0cedbf65
haraprasadj <haraprasadj@uchicago.edu>
2022-05-16 12:12:59 -0700

    remove unneeded logging

--- load-testing/dicom-viewer/study.js
+++ load-testing/dicom-viewer/study.js
@@ -38,8 +38,6 @@ export function setup() {
     },
   };
   const resp = http.get(`${DICOM_SERVER_URL}/studies`, params);
-  console.log(resp);
-  console.log(JSON.stringify(resp.body));
   const studies = JSON.parse(resp.body);
   // console.log(studies);
   let studyUrl = '';
