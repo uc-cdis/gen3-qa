58657635d203cbe826ff2ae54662a8d3daa70fa6
Ramu Nerella <ramunerella@Ramus-MBP.attlocal.net>
2022-06-09 11:42:11 -0700

    updated load test runner

--- load-testing/loadTestRunnerV2.js
+++ load-testing/loadTestRunnerV2.js
@@ -1,6 +1,6 @@
 const { spawnSync } = require('child_process');
 const fs = require('fs');
-const { getJWTData, parseJwt, getAccessTokenFromApiKey } = require('../utils/apiUtil.js');
+// const { getJWTData, parseJwt, getAccessTokenFromApiKey } = require('../utils/apiUtil.js');
 
 const args = process.argv.slice(2);
 
@@ -9,7 +9,7 @@ if (args.length < 2) {
   process.exit(1);
 }
 
-const pathToCredentialsJson = args[0];
+// const pathToCredentialsJson = args[0];
 const testDescriptorFile = args[1];
 
 async function runLoadTestScenario() {
@@ -19,20 +19,21 @@ async function runLoadTestScenario() {
 
   const targetService = testDescriptorData.service;
   const loadTestScenario = testDescriptorData.load_test_scenario;
-  const jwtData = await getJWTData(pathToCredentialsJson);
-  const apiKey = jwtData[Object.keys(jwtData)[0]];
+ //  const jwtData = await getJWTData(pathToCredentialsJson);
+  /* const apiKey = jwtData[Object.keys(jwtData)[0]];
   let targetEnvironment = jwtData[Object.keys(jwtData)[1]];
 
   let token = '';
   if (Object.prototype.hasOwnProperty.call(testDescriptorData, 'override_access_token') && testDescriptorData.override_access_token !== 'test') {
     token = testDescriptorData.override_access_token;
     console.log(`Override token: ${token}`);
-    const overrideJwtData = parseJwt(testDescriptorData.override_access_token);
-    targetEnvironment = overrideJwtData.iss;
+    const overrideJwtData = parseJwt(testDescriptorData.override_access_token); */
+    let targetEnvironment = targetEnvironment = args[0];
     console.log(`Target environment from override token: ${targetEnvironment}`);
-    targetEnvironment = targetEnvironment.replace(/(^\w+:|^)\/\//, '').replace('/user', '');
+   // targetEnvironment = targetEnvironment.replace(/(^\w+:|^)\/\//, '').replace('/user', '');
     console.log(`Sanitized target environment from override token: ${targetEnvironment}`);
-  } else {
+   // } 
+   /* else {
     token = await getAccessTokenFromApiKey(apiKey, targetEnvironment)
       .then((ACCESS_TOKEN) => {
         console.log(ACCESS_TOKEN);
@@ -40,8 +41,8 @@ async function runLoadTestScenario() {
       }).catch((reason) => {
         console.log(`Failed to get access token from API Key in ${targetEnvironment}. Response: ${reason.status} - ${reason.statusText}`);
         process.exit(1);
-      });
-  }
+      }); 
+  }*/
 
   let k6output = 'json';
   if (process.env.USE_DATADOG === 'true') {
@@ -51,7 +52,7 @@ async function runLoadTestScenario() {
   // Set fixed list of args for the load test run
   const loadTestArgs = ['-e', `TARGET_ENVIRONMENT=${process.env.TARGET_ENVIRONMENT}`,
     '-e', `RELEASE_VERSION=${process.env.RELEASE_VERSION}`, '-e', `GEN3_HOST=${targetEnvironment}`,
-    '-e', `ACCESS_TOKEN=${token}`, '-e', `VU_COUNT=${process.env.VU_COUNT}`, '-e', `DURATION=${process.env.DURATION}`,
+    '-e', `VU_COUNT=${process.env.VU_COUNT}`, '-e', `DURATION=${process.env.DURATION}`,
     '--out', `${k6output}`, '--summary-export=result.json',
     `load-testing/${targetService}/${loadTestScenario}.js`];
 
@@ -69,7 +70,7 @@ async function runLoadTestScenario() {
   );
 
   // Always make the apiKey available for long-running tests
-  loadTestArgs.unshift(`API_KEY="${apiKey}"`);
+ // loadTestArgs.unshift(`API_KEY="${apiKey}"`);
   loadTestArgs.unshift('-e');
 
   // The first arg should always be 'run'
