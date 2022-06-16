bbec7cd4330fcda7e3ca2501cf2f0d03f3c42461
jingh8 <jingh8@uchicago.edu>
2022-06-16 10:21:30 -0500

    fix eslint

--- suites/dicomViewer/dicomViewerTest.js
+++ suites/dicomViewer/dicomViewerTest.js
@@ -1,6 +1,6 @@
 Feature('Dicom Viewer @requires-dicom-viewer @requires-dicom-server @requires-portal');
 
-const fs = require("fs");
+const fs = require('fs');
 const chai = require('chai');
 const { inJenkins } = require('../../utils/commons.js');
 
@@ -12,7 +12,7 @@ const { smartWait } = require('../../utils/apiUtil');
 
 async function waitForETL() {
   const isComplete = async function () {
-    const res = await bash.runCommand(`kc get pods | grep etl`);
+    const res = await bash.runCommand('kc get pods | grep etl');
     if (process.env.DEBUG === 'true') {
       console.log('############');
       console.log(res);
@@ -25,7 +25,7 @@ async function waitForETL() {
       console.error(res);
     }
     if (!complete) {
-      console.log(`etl not complete`);
+      console.log('etl not complete');
       return false;
     }
     return true;
@@ -42,13 +42,12 @@ async function waitForETL() {
   );
 }
 
-
 // Submit a dicom file and run etl
-BeforeSuite(async ({ I }) => {
+BeforeSuite(async ({ I, users }) => {
   // read dicom file
-  filePath = "files/testFile.dcm";
+  const filePath = 'files/testFile.dcm';
   if (!fs.existsSync(filePath)) {
-    const msg = `Did not find test dicom file`;
+    const msg = 'Did not find test dicom file';
     if (inJenkins) {
       throw Error(msg);
     }
@@ -56,34 +55,33 @@ BeforeSuite(async ({ I }) => {
   const content = fs.readFileSync(filePath);
 
   // submit dicom file to dicom server
-  URL = `https://${process.env.HOSTNAME}/dicom-server/instances/`;
-  const res_server = await I.sendPostRequest(URL, content,
+  const URL = `https://${process.env.HOSTNAME}/dicom-server/instances/`;
+  const resServer = await I.sendPostRequest(URL, content,
     {
       'Content-Type': 'application/dicom',
       Authorization: `bearer ${users.mainAcct.accessToken}`,
     });
-  console.log(res_server);
-  expect(res_server.status).to.equal(200);
-  const studyInstance = res_server.data.ParentStudy;
-  const res_study = await I.sendGetRequest(`dicom-server/studies/${studyInstance}`, users.mainAcct.accessTokenHeader);
-  const studyId = res_study.data.StudyInstanceUID;
-  
+  console.log(resServer);
+  expect(resServer.status).to.equal(200);
+  const studyInstance = resServer.data.ParentStudy;
+  const resStudy = await I.sendGetRequest(`dicom-server/studies/${studyInstance}`, users.mainAcct.accessTokenHeader);
+  const studyId = resStudy.data.StudyInstanceUID;
+
   // sumbit the file to the graph
-  const program = "";
-  const project = "";
-  const caseid = "";
-  const casesubmitterid = "";
-  if (process.env.RUNNING_LOCAL === 'true' && process.env.NAMESPACE.includes('qa-midrc')){
-    program = "DEV";
-    project = "DICOM_test";
-    caseid = "1265e15f-d867-42d5-8533-6bce6e97bb4d";
-    casesubmitterid = "case1";
-  }
-  else{
-    program = "jnkns";
-    project = "jenkins";
-    caseid = "3ff6714264"
-    casesubmitterid = "core_metadata_collection_9f06ab0e9b";
+  let program = '';
+  let project = '';
+  let caseid = '';
+  let casesubmitterid = '';
+  if (process.env.RUNNING_LOCAL === 'true' && process.env.NAMESPACE.includes('qa-midrc')) {
+    program = 'DEV';
+    project = 'DICOM_test';
+    caseid = '1265e15f-d867-42d5-8533-6bce6e97bb4d';
+    casesubmitterid = 'case1';
+  } else {
+    program = 'jnkns';
+    project = 'jenkins';
+    caseid = '3ff6714264';
+    casesubmitterid = 'core_metadata_collection_9f06ab0e9b';
     // TODO: find proper parameters in jenkins
   }
   const submitData = `{
@@ -95,17 +93,17 @@ BeforeSuite(async ({ I }) => {
       }
     ],
     "submitter_id": "${studyId}"
-  }`
+  }`;
   const endpoint = `/api/v0/submission/${program}/${project}/`;
-  const res_node = await I.sendPutRequest(
+  const resNode = await I.sendPutRequest(
     endpoint,
     submitData,
-    user.mainAcct.accessTokenHeader,
+    users.mainAcct.accessTokenHeader,
   );
-  console.log(res_node);
-  expect(res_node.status).to.equal(200);
-  
-  //run etl
+  console.log(resNode);
+  expect(resNode.status).to.equal(200);
+
+  // run etl
   bash.runCommand('gen3 job run etl');
   await waitForETL();
 });
