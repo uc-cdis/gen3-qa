f9d1d6761b7a19e73210dfdfbe167d2346182873
jingh8 <jingh8@uchicago.edu>
2022-08-12 10:47:13 -0500

    fix skip feature

--- hooks/test_conditions.js
+++ hooks/test_conditions.js
@@ -71,7 +71,7 @@ module.exports = async function () {
       const pfbButton2 = bash.runCommand('gen3 secrets decode portal-config gitops.json | jq \'contains({explorerConfig: {buttons: [{enabled: true, type: "export-to-pfb"}]}})\'');
       if (pfbButton1 !== 'true' && pfbButton2 !== 'true') {
         console.log('Skipping pfb export tests since the required configuration is not in fence-config.yaml');
-        suite.feature.skip();
+        xFeature(suite.title);
       }
     }
   });
