2c4af24605970d40b2ecb79225a11568b4dc47c7
Atharva Rane <41084525+atharvar28@users.noreply.github.com>
2022-10-03 15:44:56 -0400

    update test condition for jenkins (#856)

--- hooks/test_conditions.js
+++ hooks/test_conditions.js
@@ -52,7 +52,7 @@ module.exports = async function () {
     }
 
     if (suite.title === 'nonDBGap Project Usersync @requires-fence @requires-indexd') {
-      const dbGaPConfig = bash.runCommand('gen3 secrets decode fence-config fence-config.yaml | yq -r \'.dbGaP[] | select(."info".username=="qa-dcf") | ."allow_non_dbgap_whitelist"\'');
+      const dbGaPConfig = bash.runCommand('gen3 secrets decode fence-config fence-config.yaml | yq -r \'.dbGaP[] | ."allow_non_dbgap_whitelist"\'');
       if (!dbGaPConfig) {
         console.log('Skipping nonDBGap project usersync tests since the required configuration is not in fence-config.yaml');
         console.dir(suite.tests);
