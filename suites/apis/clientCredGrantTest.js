9e7903a2531916f9d4d201595627aab2d48745d1
Atharva Rane <atharvar@uchicago.edu>
2022-11-03 15:57:41 -0400

    fix:the policy

--- suites/apis/clientCredGrantTest.js
+++ suites/apis/clientCredGrantTest.js
@@ -69,8 +69,7 @@ AfterSuite(async ({ I, fence }) => {
   // revoking the arborist policy for the user
   console.log('Revoking the arborist policy for the user0 ...');
   await bash.runCommand(`
-      gen3 devterm curl -X DELETE arborist-service/user/dcf-integration-test-0@planx-pla.net/policy/requestor_integration_test
-  `);
+      gen3 devterm curl -X DELETE arborist-service/user/dcf-integration-test-0@planx-pla.net/policy/requestor_client_credentials_test`);
 });
 
 Scenario('Client Credentials Grant Type interaction with Requestor @clientCreds', async ({ I, users, fence }) => {
@@ -95,7 +94,7 @@ Scenario('Client Credentials Grant Type interaction with Requestor @clientCreds'
   const { username } = users.user0;
   const data = {
     username,
-    policy_id: 'requestor_integration_test',
+    policy_id: 'requestor_client_credentials_test',
   };
 
   //  create a access request in requestor DB with the clientAccessToken
