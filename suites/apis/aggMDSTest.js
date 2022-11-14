5b6434d9c99c34baac6d12a6983cb54346c01c29
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2022-11-14 07:49:07 -0800

    Add test data to aggregate mds tests

--- suites/apis/aggMDSTest.js
+++ suites/apis/aggMDSTest.js
@@ -3,60 +3,37 @@ Feature('Aggregate Metadata Service @aggMDS @requires-metadata');
 const uuid = require('uuid');
 const { expect } = require('chai');
 const { output } = require('codeceptjs');
+const { Bash } = require('../../utils/bash.js');
+const fs = require('fs');
 
+const bash = new Bash();
 const I = actor();
 I.cache = {};
 
-// After(async ({ users, mds }) => {
-//  if ('studyId' in I.cache) {
-//    await mds.do.deleteMetadataRecord(users.mainAcct.accessTokenHeader, I.cache.studyId);
-//    await mds.do.reSyncAggregateMetadata();
-//  }
-// });
+After(async ({ users, mds, I }) => {
+ if ('studyId' in I.cache) {
+   await mds.do.deleteMetadataRecord(users.mainAcct.accessTokenHeader, I.cache.studyId);
+   await mds.do.reSyncAggregateMetadata();
+ }
+});
 
-Scenario('Create, edit and delete aggregate metadata record', async ({ mds, users }) => {
+let testDataFiles = new DataTable(['studyFilePath']);
+testDataFiles.add(['files/testData/aggMDSTest/study1.json']);
+testDataFiles.add(['files/testData/aggMDSTest/study2.json']);
+testDataFiles.add(['files/testData/aggMDSTest/study3.json']);
+
+Data(testDataFiles).Scenario('Create, edit and delete aggregate metadata record @wip', async ({ mds, users, I, current }) => {
   I.cache.studyId = uuid.v4();
-  // eslint-disable-next-line prefer-const
-  let studyMetaData = {
-    _guid_type: 'discovery_metadata',
-    gen3_discovery: {
-      tags: [
-        {
-          category: 'Other',
-          name: 'AUTOTEST Tag',
-        },
-      ],
-      authz: [],
-      sites: 3,
-      summary: '[AUTOTEST Summary] The BACPAC Research Program, Data Integration, Algorithm Development, and Operations Management Center (DAC) will bring cohesion to research performed by the participating Mechanistic Research Centers, Technology Research Sites, and Phase 2 Clinical Trials Centers. DAC Investigators will share their vision and provide scientific leadership and organizational support to the BACPAC Consortium. The research plan consists of supporting design and conduct of clinical trials with precision interventions that focus on identifying the best treatments for individual patients. The DAC will enhance collaboration and research progress with experienced leadership, innovative design and analysis methodologies, comprehensive research operations support, a state-of-the-art data management and integration system, and superior administrative support. This integrated structure will set the stage for technology assessments, solicitation of patient input and utilities, and the evaluation of high-impact interventions through the innovative design and sound execution of clinical trials, leading to effective personalized treatment approaches for patients with chronic lower back pain.',
-      location: 'Chapel Hill, Nc',
-      subjects: 150,
-      __manifest: [],
-      study_name: 'BACPAC Research Consortium',
-      study_type: 'Other',
-      institutions: 'University Of North Carolina Chapel Hill',
-      year_awarded: 2019,
-      investigators: 'Lavange, Lisa',
-      project_title: '[AUTOTEST Title] Testing Aggregate Metadata Service',
-      protocol_name: 'BACPAC Minimum Dataset Example',
-      project_number: `${I.cache.studyId}`,
-      administering_ic: 'NIAMS',
-      advSearchFilters: [
-        {
-          key: 'Research Focus Area',
-          value: 'AUTOTEST Filter',
-        },
-      ],
-      research_program: 'Back Pain Consortium Research Program',
-      research_question: 'To inform a precision medicine approach to cLBP.',
-      study_description: 'Observational',
-      research_focus_area: 'Clinical Research in Pain Management',
-    },
-  };
+  // dynamically get UID field name from portal config
+  const UIDFieldName = bash.runCommand('gen3 secrets decode portal-config gitops.json | jq \'.discoveryConfig.minimalFieldMapping.uid\'').replace(/^"(.*)"$/, '$1');
+  expect(UIDFieldName).to.be.a('string').that.is.not.empty;
+  let studyMetadata = JSON.parse(fs.readFileSync(current.studyFilePath));
+  studyMetadata['gen3_discovery'][UIDFieldName] = I.cache.studyId;
+  const project_title = studyMetadata['gen3_discovery']['project_title'];
   // CREATE
   output.print('Creating Metadata Record');
   await mds.do.createMetadataRecord(
-    users.mainAcct.accessTokenHeader, I.cache.studyId, studyMetaData,
+    users.mainAcct.accessTokenHeader, I.cache.studyId, studyMetadata,
   );
   output.print('Re-sync Aggregate Metadata');
   await mds.do.reSyncAggregateMetadata();
@@ -64,19 +41,19 @@ Scenario('Create, edit and delete aggregate metadata record', async ({ mds, user
     users.mainAcct.accessTokenHeader, I.cache.studyId,
   );
   expect(record.commons_name).to.equal('HEAL');
-  expect(record.project_title).to.equal('[AUTOTEST Title] Testing Aggregate Metadata Service');
+  expect(record.project_title).to.equal(studyMetadata['gen3_discovery']['project_title']);
   // EDIT
   output.print('Updating Metadata Record');
-  studyMetaData.gen3_discovery.project_title = '[AUTOTEST Title] Testing Aggregate Metadata Service - Modified';
+  studyMetadata.gen3_discovery.project_title = `${project_title} - Modified`;
   await mds.do.editMetadataRecord(
-    users.mainAcct.accessTokenHeader, I.cache.studyId, studyMetaData,
+    users.mainAcct.accessTokenHeader, I.cache.studyId, studyMetadata,
   );
   output.print('Re-sync Aggregate Metadata');
   await mds.do.reSyncAggregateMetadata();
   const updatedRecord = await mds.do.readAggMetadataRecord(
     users.mainAcct.accessTokenHeader, I.cache.studyId,
   );
-  expect(updatedRecord.project_title).to.equal('[AUTOTEST Title] Testing Aggregate Metadata Service - Modified');
+  expect(updatedRecord.project_title).to.equal(`${project_title} - Modified`);
   // DELETE
   // output.print('Deleting Metadata Record');
   // await mds.do.deleteMetadataRecord(
