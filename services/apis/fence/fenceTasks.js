c63f65ffbf4b601f81d891c082897ae74a246dd1
Atharva Rane <41084525+atharvar28@users.noreply.github.com>
2023-01-23 13:57:51 -0500

    Update fenceTasks.js

--- services/apis/fence/fenceTasks.js
+++ services/apis/fence/fenceTasks.js
@@ -320,6 +320,8 @@ module.exports = {
   async forceLinkGoogleAcct(userAcct, googleEmail) {
     // hit link endpoint to ensure a proxy group is created for user
     await I.sendGetRequest(fenceProps.endpoints.linkGoogle, userAcct.accessTokenHeader);
+    console.log(`########### UserAcct : ${userAcct}`);
+    console.log(`########### UserAcct Header : ${userAcct.accessTokenHeader}`);
 
     // run fence-create command to circumvent google and add user link to fence
     const cmd = `fence-create force-link-google --username ${userAcct.username} --google-email ${googleEmail}`;
