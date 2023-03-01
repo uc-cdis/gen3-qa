ce4cd7f06b91a712b6fefdb4dd9b8b94fa6e4f86
Hara Prasad <haraprasadj@uchicago.edu>
2023-03-01 10:46:25 -0800

    use http in REST helper

--- gen3.qa.in.a.box.codecept.conf.js
+++ gen3.qa.in.a.box.codecept.conf.js
@@ -36,7 +36,7 @@ exports.config = {
       port: 4444,
     },
     REST: {
-      endpoint: `https://${process.env.HOSTNAME}`,
+      endpoint: `http://${process.env.HOSTNAME}`,
       timeout: 300000,
       defaultHeaders: {
         common: {
