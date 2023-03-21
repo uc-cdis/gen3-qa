718f459b6bc0b6cba155d16db795f1f31159ee48
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-03-21 09:16:39 -0700

    top div for qa-covid19 as well

--- utils/apiUtil.js
+++ utils/apiUtil.js
@@ -474,7 +474,7 @@ module.exports = {
       },
       covid19: {
         summary: {
-          css: '.covid19-dashboard_panel',
+          css: '.covid19-dashboard',
         },
         cards: {
           css: '.covid19-dashboard_counts',
