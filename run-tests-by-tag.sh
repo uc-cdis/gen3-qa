dcec34be2b3bd6d73182b52027b291bb88bb0942
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-04-07 11:08:23 -0700

    fix grep

--- run-tests-by-tag.sh
+++ run-tests-by-tag.sh
@@ -136,7 +136,7 @@ fi
 
 ##################################################################################################
 
-DEBUG=$debug npm test -- --reporter mocha-multi --verbose --grep '(?=.*\\$selectedTag)^(?!.*@manual)'
+DEBUG=$debug npm test -- --reporter mocha-multi --verbose --grep "@manual" --invert --grep "$selectedTag"
 
 ##################################################################################################
 
