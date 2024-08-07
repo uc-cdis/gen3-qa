42329af88ed8353c773745603af64ca662b7cc5c
Hara Prasad <haraprasadj@uchicago.edu>
2024-08-07 10:25:04 -0700

    Fix/recreate automated copy img (#1050)
    * set github username and token when cloning the repo
    
    * fail jenkins job if any of the steps go wrong
    
    * git pull fast forward only
    
    * merge only
    
    ---------
    
    Co-authored-by: Marcelo Costa <marceloc@uchicago.edu>

--- files/jenkins-job-create-testing-image-for-external-contributors.sh
+++ files/jenkins-job-create-testing-image-for-external-contributors.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -x
+set -e
+
 # This automation is required so Gen3 Engineering can produce a copy of a branch from a forked-repo
 # and trigger a Quay image build that is utilized in our CI Pipeline.
 
 git config --global user.email "cdis@uchicago.edu"
 git config --global user.name "$GITHUB_USERNAME"
 
-OUR_REMOTE_URL="https://github.com/uc-cdis/${OUR_GEN3_SERVICE_REPO_NAME}"
+URL_PREFIX="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/"
+OUR_REMOTE_URL="${URL_PREFIX}${OUR_GEN3_SERVICE_REPO_NAME}"
 
 echo "cloning $OUR_REMOTE_URL..."
 git clone $OUR_REMOTE_URL
@@ -18,15 +22,19 @@ cd $OUR_GEN3_SERVICE_REPO_NAME
 
 ls -ilha
 
+set +e
 # delete branch if it already exists
 branch_exists=$(git ls-remote --heads ${OUR_REMOTE_URL}.git automatedCopy-$NAME_OF_THE_BRANCH)
+set -e
 
 if [[ -z $branch_exists ]]; then
   echo "git ls-remote output empty. The branch does not exist."
 else
   echo "WARN: git ls-remote output is NOT empty."
   echo " Deleting the existing automatedCopy branch to create a new copy based on new changes from the forked-repo branch..."
+  set +e
   git branch -D automatedCopy-$NAME_OF_THE_BRANCH
+  set -e
   git push origin --delete automatedCopy-$NAME_OF_THE_BRANCH
 fi
 
@@ -37,10 +45,9 @@ echo "changing origin to pull changes from external repo: https://${EXTERNAL_REP
 git remote set-url origin https://${EXTERNAL_REPO_REMOTE_URL}
 
 echo "pulling changes from external branch $NAME_OF_THE_BRANCH"
-git pull origin $NAME_OF_THE_BRANCH
+git pull origin $NAME_OF_THE_BRANCH --no-rebase
 
 echo "restore original origin $OUR_REMOTE_URL"
-URL_PREFIX="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/"
 git remote set-url origin ${URL_PREFIX}${OUR_GEN3_SERVICE_REPO_NAME}.git
 
 echo "finish branch cloning process but pushing local changes to our repo's branch."
