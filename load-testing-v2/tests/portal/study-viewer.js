a55bbc87559f48d9f11a35a7d2ce1cc9d5f7ba6a
dsmith <david@krum.io>
2024-10-17 14:25:37 -0400

    study-viewer.js

--- load-testing-v2/tests/portal/study-viewer.js
+++ load-testing-v2/tests/portal/study-viewer.js
@@ -1,48 +1,71 @@
-const { check, group, sleep } = require('k6'); // eslint-disable-line import/no-unresolved
-const http = require('k6/http'); // eslint-disable-line import/no-unresolved
-const { Rate } = require('k6/metrics'); // eslint-disable-line import/no-unresolved
+/* eslint-disable no-mixed-operators */
+/* eslint-disable no-bitwise */
+/* eslint-disable one-var */
 
-const myFailRate = new Rate('failed requests');
+import { sleep, group, check } from 'k6';
+import http from 'k6/http';
+import { Rate } from 'k6/metrics';
+import { getCommonVariables } from '../../utils/helpers.js';
+const myFailRate = new Rate('failed_requests');
 
-const {
-  ACCESS_TOKEN,
-  RELEASE_VERSION,
-  GEN3_HOST,
-  VIRTUAL_USERS,
-} = __ENV; // eslint-disable-line no-undef
+const credentials = JSON.parse(open('../../utils/credentials.json'));
+console.log(`credentials.key_id: ${credentials.key_id}`);
+
+//Default values:
+__ENV.RELEASE_VERSION = __ENV.RELEASE_VERSION || "v3.3.1";
+__ENV.VIRTUAL_USERS = __ENV.VIRTUAL_USERS || JSON.stringify([
+  { "duration": "1s", "target": 1 },
+  { "duration": "10s", "target": 10 },
+  // { "duration": "60s", "target": 10 },
+  // { "duration": "60s", "target": 50 },
+  // { "duration": "60s", "target": 100 },
+  // { "duration": "60s", "target": 150 },
+  // { "duration": "60s", "target": 200 },
+  // { "duration": "60s", "target": 200 },
+  // { "duration": "120s", "target": 0 }
+  ]);
+console.log(`VIRTUAL_USERS: ${__ENV.VIRTUAL_USERS}`);
 
 export const options = {
   tags: {
     test_scenario: 'Portal - Study Viewer',
-    release: RELEASE_VERSION,
+    release: __ENV.RELEASE_VERSION,
     test_run_id: (new Date()).toISOString().slice(0, 16),
   },
-  stages: JSON.parse(VIRTUAL_USERS.slice(1, -1)),
+  stages: JSON.parse(__ENV.VIRTUAL_USERS),
   thresholds: {
     http_req_duration: ['avg<250'],
-    'failed requests': ['rate<0.1'],
+    'failed_requests': ['rate<0.1'],
   },
   noConnectionReuse: true,
 };
 
-export default function () {
-  const url = `https://${GEN3_HOST}/study-viewer/clinical_trials`;
+export function setup() {
+  return getCommonVariables(__ENV, credentials);
+}
+
+export default function (env) {
+  const url = `${env.GEN3_HOST}/study-viewer/clinical_trials`;
 
   const params = {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${ACCESS_TOKEN}`,
+      Authorization: `Bearer ${env.ACCESS_TOKEN}`,
     },
+    tags: { name: 'NIAID Study Viewer' }
   };
   group('Visiting the study viewer page', () => {
     group('http get', () => {
       console.log(`Shooting requests against: ${url}`);
-      const res = http.get(url, params, { tags: { name: 'NIAID Study Viewer' } });
+      const res = http.get(url, params);
       // console.log(`Request performed: ${new Date()}`);
+      console.log(`Request response status: ${res.status}`);
+      console.log(`Request response body: ${res.body}`);
+
       myFailRate.add(res.status !== 200);
       if (res.status !== 200) {
-        console.log(`Request response: ${res.status}`);
-        console.log(`Request response: ${res.body}`);
+        console.log(`Request response status: ${res.status}`);
+        console.log(`Request response body: ${res.body}`);
       }
       check(res, {
         'is status 200': (r) => r.status === 200,
