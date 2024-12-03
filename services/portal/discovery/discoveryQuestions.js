3df3ef735ee25943ad446ce2780bcf740c2751e1
Hara Prasad <haraprasadj@uchicago.edu>
2024-12-03 09:50:44 -0600

    Fix/discoverypagetestdata (#1061)
    * Fix discovery page test data
    
    * wait longer for study to show up on page
    
    * try waiting for 2 minutes before launching discovery page
    
    * fix data
    
    * retrying study lookup
    
    * fix retry
    
    * fix retry
    
    * fix
    
    * fix
    
    * fix data

--- services/portal/discovery/discoveryQuestions.js
+++ services/portal/discovery/discoveryQuestions.js
@@ -1,4 +1,6 @@
+const { sleepMS } = require('../../../utils/apiUtil.js');
 const props = require('./discoveryProps.js');
+const tasks = require('./discoveryTasks.js');
 
 const I = actor();
 
@@ -11,7 +13,26 @@ module.exports = {
     I.seeElement(props.readyCue, 30);
   },
 
-  isStudyFound(studyId) {
-    I.seeElement(props.studyLocator(studyId), 30);
+  async isStudyFound(studyId) {
+    const retries = 10; // Number of retries
+    const delayInMs = 30000; // Delay between retries in milliseconds
+
+    for (let attempt = 1; attempt <= retries; attempt++) {
+      const visibleElements = await I.grabNumberOfVisibleElements(props.studyLocator(studyId));
+
+      if (visibleElements > 0) {
+        console.log(`Study found on attempt ${attempt}`);
+        return true; // Exit the loop if the element is found
+      } else {
+        console.log(`Attempt ${attempt} failed. Retrying in ${delayInMs / 1000} seconds...`);
+        if (attempt < retries) {
+          await sleepMS(delayInMs); // Wait before retrying
+          tasks.goToPage(); // Refresh discovery page
+        } else {
+          console.error('Study not found after maximum retries.');
+          throw new Error('Study not found.');
+        }
+      }
+    }
   },
 };
